d6c76fefcbdcf03a274a5e756195947ffc74596d
cynic <daniel@szymanski.cc>
2019-03-14 23:55:50 +0100

    Restructed project and added draft for instance ui management
    Restructured project in a way that namespaces now match the paths and also
    splitted classes for operator management mostly into own files.
    Added concept of InstanceUi class which holds the ui relevant information
    for an instance. This is kept in a global registry and can be accessed via
    the id of the symbol that contains the instance and via the instance id
    itself. See comments in InstanceUiRegistry.

---
+++ T3/Core/Operator/Instance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T3.Core.Operator
+{
+    public class Instance : IDisposable
+    {
+        public Instance Parent { get; internal set; }
+        public Symbol Definition { get; internal set; }
+
+        public void Dispose()
+        {
+        }
+
+        public List<Slot> Outputs { get; } = new List<Slot>();
+        public List<Instance> Children { get; } = new List<Instance>();
+        public List<Slot> Inputs { get; } = new List<Slot>();
+    }
+}
--- T3/Properties/AssemblyInfo.cs
+++ T3/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("T3Tests")]
+[assembly: AssemblyTitle("T3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("T3Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("T3")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
