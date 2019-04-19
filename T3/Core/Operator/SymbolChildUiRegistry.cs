5ef1839bfb9267cb3719de8f63e95a06604aef38
cynic <daniel@szymanski.cc>
2019-04-19 11:20:04 +0200

    Made SymbolChildUiRegistry a static class and the dict a static property

--- T3/Core/Operator/SymbolChildUiRegistry.cs
+++ T3/Core/Operator/SymbolChildUiRegistry.cs
@@ -3,19 +3,9 @@ using System.Collections.Generic;
 
 namespace T3.Core.Operator
 {
-    public class SymbolChildUiRegistry
+    public static class SymbolChildUiRegistry
     {
-        /*
-         * Usage: 
-         * var allUiEntries = InstanceUiRegistry.Instance.UiEntries;
-         * var uiEntriesForSymbol = allUiEntries[instance.Symbol.Id];
-         * var instanceUi = uiEntriesForSymbol[instance.Id];
-         * imgui directly modifies the values in instanceUi -> all instances are updated automatically
-         */
-        public static SymbolChildUiRegistry Instance => _instance ?? (_instance = new SymbolChildUiRegistry());
-        // symbol id -> (instance id -> instance ui entry)
-        public Dictionary<Guid, Dictionary<Guid, SymbolChildUi>> UiEntries { get; } = new Dictionary<Guid, Dictionary<Guid, SymbolChildUi>>();
-
-        private static SymbolChildUiRegistry _instance;
+        // symbol id -> (symbol child id -> symbol child ui entry)
+        public static Dictionary<Guid, Dictionary<Guid, SymbolChildUi>> Entries { get; } = new Dictionary<Guid, Dictionary<Guid, SymbolChildUi>>();
     }
 }
