4b302559c8eac8b3bf9a8588fe5c501943de5403
pixtur <thomas.mann@framefield.com>
2019-06-09 13:27:43 +0200

    Updated to ImGUI 1.7

---
+++ T3/Gui/Graph/GraphWidgets.cs
@@ -0,0 +1,26 @@
+﻿using ImGuiNET;
+using System.Numerics;
+
+namespace T3.Gui.Graph
+{
+    /// <summary>
+    /// Renders a small representation of the operator's parameters and other data
+    /// </summary>
+    public class GraphWidgets
+    {
+        static public void Draw(SymbolChildUi ui)
+        {
+            ImGui.SetCursorPos(Canvas.ChildPosFromCanvas(ui.Position + ui.Size));
+
+            ImGui.BeginChildFrame((uint)ui.SymbolChild.Id.GetHashCode(), new Vector2(230, 100));
+            {
+                for (int i = 0; i < 10; i++)
+                {
+
+                    ImGui.Text("hallo" + i);
+                }
+            }
+            ImGui.EndChildFrame();
+        }
+    }
+}
