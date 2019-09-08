b37d5d252b8636cfdbc6ef1c20e19908dafb02c0
pixtur <thomas.mann@framefield.com>
2019-09-08 14:33:29 +0200

    Minor code formatting

---
+++ T3/Gui/CustomComponents.cs
@@ -0,0 +1,118 @@
+﻿using ImGuiNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using UiHelpers;
+
+namespace T3.Gui
+{
+    static class CustomComponents
+    {
+        public static bool JogDial(string label, ref double delta, Vector2 size)
+        {
+            var hot = ImGui.Button(label + "###dummy", size);
+            var io = ImGui.GetIO();
+            if (ImGui.IsItemActive())
+            {
+                var center = (ImGui.GetItemRectMin() + ImGui.GetItemRectMax()) * 0.5f;
+                ImGui.SetMouseCursor(ImGuiMouseCursor.Hand);
+                ImGui.GetForegroundDrawList().AddCircle(center, 100, Color.Gray, 50);
+                hot = true;
+
+                var pLast = io.MousePos - io.MouseDelta - center;
+                var pNow = io.MousePos - center;
+                var aLast = Math.Atan2(pLast.X, pLast.Y);
+                var aNow = Math.Atan2(pNow.X, pNow.Y);
+                delta = aNow - aLast;
+                if (delta > 1.5)
+                {
+                    delta -= 2 * Math.PI;
+                }
+                else if (delta < -1.5)
+                {
+                    delta += 2 * Math.PI;
+                }
+            }
+            return hot;
+        }
+
+
+        /// <summary>Draw a splitter</summary>
+        /// <remarks>
+        /// Take from https://github.com/ocornut/imgui/issues/319#issuecomment-147364392
+        /// </remarks>
+
+
+
+
+
+        /// <summary>Draw a splitter</summary>
+        /// <remarks>
+        /// Take from https://github.com/ocornut/imgui/issues/319#issuecomment-147364392
+        /// </remarks>
+        public static void SplitFromBottom(ref float offsetFromBottom)
+        {
+            const float thickness = 5; ;
+
+            var backup_pos = ImGui.GetCursorPos();
+
+            var size = ImGui.GetWindowContentRegionMax() - ImGui.GetWindowContentRegionMin();
+            var contentMin = ImGui.GetWindowContentRegionMin() + ImGui.GetWindowPos();
+
+            var pos = new Vector2(contentMin.X, contentMin.Y + size.Y - offsetFromBottom - thickness);
+            ImGui.SetCursorScreenPos(pos);
+
+            ImGui.PushStyleColor(ImGuiCol.Button, new Vector4(0, 0, 0, 0));
+            ImGui.PushStyleColor(ImGuiCol.ButtonActive, new Vector4(0, 0, 0, 1));
+            ImGui.PushStyleColor(ImGuiCol.ButtonHovered, new Vector4(0, 0, 0, 1));
+
+            ImGui.Button("##Splitter", new Vector2(-1, thickness));
+
+            ImGui.PopStyleColor(3);
+
+            if (ImGui.IsItemHovered())
+            {
+                ImGui.SetMouseCursor(ImGuiMouseCursor.ResizeNS);
+            }
+
+            if (ImGui.IsItemActive())
+            {
+                offsetFromBottom = Im.Clamp(
+                    offsetFromBottom - ImGui.GetIO().MouseDelta.Y,
+                    0,
+                    size.Y - thickness);
+            }
+
+            ImGui.SetCursorPos(backup_pos);
+        }
+
+
+        public static bool ToggleButton(string label, ref bool isSelected, Vector2 size, bool trigger = false)
+        {
+            var wasSelected = isSelected;
+            var clicked = false;
+            if (isSelected)
+            {
+                ImGui.PushStyleColor(ImGuiCol.Button, Color.Red.Rgba);
+                ImGui.PushStyleColor(ImGuiCol.ButtonHovered, Color.Red.Rgba);
+                ImGui.PushStyleColor(ImGuiCol.ButtonActive, Color.Red.Rgba);
+            }
+            if (ImGui.Button(label, size) || trigger)
+            {
+                isSelected = !isSelected;
+                clicked = true;
+            }
+
+            if (wasSelected)
+            {
+                ImGui.PopStyleColor(3);
+            }
+            return clicked;
+        }
+
+
+    }
+}
--- T3/Gui/Graph/GraphCanvasWindow.cs
+++ T3/Gui/Graph/GraphCanvasWindow.cs
@@ -6,6 +6,8 @@ using System.Numerics;
 using T3.Core.Operator;
 using T3.Gui.Animation;
 using UiHelpers;
+using static ImGuiNET.ImGui;
+using static T3.Gui.CustomComponents;
 
 namespace T3.Gui.Graph
 {
@@ -14,18 +16,14 @@ namespace T3.Gui.Graph
     /// </summary>
     public class GraphCanvasWindow
     {
+        public GraphCanvas Canvas { get; private set; }
+
+
         public GraphCanvasWindow(Instance opInstance, string windowTitle = "Graph windows")
         {
             _windowTitle = windowTitle;
             Canvas = new GraphCanvas(opInstance);
             _curveEditor.ClipTime = _clipTime;
-        }
-
-        private ClipTime _clipTime = new ClipTime();
-
-        private float GetGraphHeight()
-        {
-            return ImGui.GetWindowHeight() - _heightTimeLine - 30;
         }
 
 
@@ -34,16 +32,15 @@ namespace T3.Gui.Graph
             bool opened = true;
             _clipTime.Update();
 
-            ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, new Vector2(0, 0));
-            if (ImGui.Begin(_windowTitle, ref opened, ImGuiWindowFlags.NoScrollbar))
+            PushStyleVar(ImGuiStyleVar.WindowPadding, new Vector2(0, 0));
+            if (Begin(_windowTitle, ref opened, ImGuiWindowFlags.NoScrollbar))
             {
                 var dl = ImGui.GetWindowDrawList();
-                //dl.ChannelsSplit(2);
-                //dl.ChannelsSetCurrent(0);
 
                 SplitFromBottom(ref _heightTimeLine);
-                //dl.ChannelsSetCurrent(1);
-                ImGui.BeginChild("##graph", new Vector2(0, GetGraphHeight()), false, ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoMove);
+                var graphHeight = ImGui.GetWindowHeight() - _heightTimeLine - 30;
+
+                ImGui.BeginChild("##graph", new Vector2(0, graphHeight), false, ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoMove);
                 {
                     //Im.DrawContentRegion();                    
                     dl.ChannelsSplit(2);
@@ -73,7 +70,6 @@ namespace T3.Gui.Graph
         }
 
 
-        public static Vector2 _timeControlsSize = new Vector2(40, 0);
         private void DrawTimeControls()
         {
             ImGui.SetCursorPos(
@@ -84,7 +80,7 @@ namespace T3.Gui.Graph
             TimeSpan timespan = TimeSpan.FromSeconds(_clipTime.Time);
 
             var delta = 0.0;
-            if (JogDial(timespan.ToString(@"hh\:mm\:ss\:ff"), ref delta, new Vector2(80, 0)))
+            if (CustomComponents.JogDial(timespan.ToString(@"hh\:mm\:ss\:ff"), ref delta, new Vector2(80, 0)))
             {
                 _clipTime.PlaybackSpeed = 0;
                 _clipTime.Time += delta;
@@ -97,7 +93,7 @@ namespace T3.Gui.Graph
             ImGui.SameLine();
 
             var isPlayingBackwards = _clipTime.PlaybackSpeed < 0;
-            if (ToggleButton(
+            if (CustomComponents.ToggleButton(
                     label: isPlayingBackwards ? $"[{(int)_clipTime.PlaybackSpeed}x]" : "<",
                     ref isPlayingBackwards,
                     _timeControlsSize))
@@ -116,7 +112,7 @@ namespace T3.Gui.Graph
 
             // Play forward
             var isPlaying = _clipTime.PlaybackSpeed > 0;
-            if (ToggleButton(
+            if (CustomComponents.ToggleButton(
                     label: isPlaying ? $"[{(int)_clipTime.PlaybackSpeed}x]" : ">",
                     ref isPlaying,
                     _timeControlsSize,
@@ -156,8 +152,6 @@ namespace T3.Gui.Graph
                 }
             }
 
-
-
             if (KeyboardBinding.Triggered(UserAction.PlaybackStop))
             {
                 _clipTime.PlaybackSpeed = 0;
@@ -168,9 +162,8 @@ namespace T3.Gui.Graph
             ImGui.SameLine();
             ImGui.Button(">]", _timeControlsSize);
             ImGui.SameLine();
-            ToggleButton("Loop", ref _clipTime.IsLooping, _timeControlsSize);
+            CustomComponents.ToggleButton("Loop", ref _clipTime.IsLooping, _timeControlsSize);
         }
-
 
         private void DrawTimeline()
         {
@@ -192,109 +185,6 @@ namespace T3.Gui.Graph
 
             _curveEditor.SetCurves(curvesForSelection);
         }
-
-        public static bool JogDial(string label, ref double delta, Vector2 size)
-        {
-            var hot = ImGui.Button(label + "###dummy", size);
-            var io = ImGui.GetIO();
-            if (ImGui.IsItemActive())
-            {
-                var center = (ImGui.GetItemRectMin() + ImGui.GetItemRectMax()) * 0.5f;
-                ImGui.SetMouseCursor(ImGuiMouseCursor.Hand);
-                ImGui.GetForegroundDrawList().AddCircle(center, 100, Color.Gray, 50);
-                hot = true;
-
-                var pLast = io.MousePos - io.MouseDelta - center;
-                var pNow = io.MousePos - center;
-                var aLast = Math.Atan2(pLast.X, pLast.Y);
-                var aNow = Math.Atan2(pNow.X, pNow.Y);
-                delta = aNow - aLast;
-                if (delta > 1.5)
-                {
-                    delta -= 2 * Math.PI;
-                }
-                else if (delta < -1.5)
-                {
-                    delta += 2 * Math.PI;
-                }
-            }
-            return hot;
-        }
-
-
-        /// <summary>Draw a splitter</summary>
-        /// <remarks>
-        /// Take from https://github.com/ocornut/imgui/issues/319#issuecomment-147364392
-        /// </remarks>
-
-        private static float _heightTimeLine = 100;
-        private CurveEditCanvas _curveEditor = new CurveEditCanvas();
-
-
-        /// <summary>Draw a splitter</summary>
-        /// <remarks>
-        /// Take from https://github.com/ocornut/imgui/issues/319#issuecomment-147364392
-        /// </remarks>
-        void SplitFromBottom(ref float offsetFromBottom)
-        {
-            const float thickness = 5; ;
-
-            var backup_pos = ImGui.GetCursorPos();
-
-            var size = ImGui.GetWindowContentRegionMax() - ImGui.GetWindowContentRegionMin();
-            var contentMin = ImGui.GetWindowContentRegionMin() + ImGui.GetWindowPos();
-
-            var pos = new Vector2(contentMin.X, contentMin.Y + size.Y - offsetFromBottom - thickness);
-            ImGui.SetCursorScreenPos(pos);
-
-            ImGui.PushStyleColor(ImGuiCol.Button, new Vector4(0, 0, 0, 0));
-            ImGui.PushStyleColor(ImGuiCol.ButtonActive, new Vector4(0, 0, 0, 1));
-            ImGui.PushStyleColor(ImGuiCol.ButtonHovered, new Vector4(0, 0, 0, 1));
-
-            ImGui.Button("##Splitter", new Vector2(-1, thickness));
-
-            ImGui.PopStyleColor(3);
-
-            if (ImGui.IsItemHovered())
-            {
-                ImGui.SetMouseCursor(ImGuiMouseCursor.ResizeNS);
-            }
-
-            if (ImGui.IsItemActive())
-            {
-                offsetFromBottom = Im.Clamp(
-                    offsetFromBottom - ImGui.GetIO().MouseDelta.Y,
-                    0,
-                    size.Y - thickness);
-            }
-
-            ImGui.SetCursorPos(backup_pos);
-        }
-
-
-        private bool ToggleButton(string label, ref bool isSelected, Vector2 size, bool trigger = false)
-        {
-            var wasSelected = isSelected;
-            var clicked = false;
-            if (isSelected)
-            {
-                ImGui.PushStyleColor(ImGuiCol.Button, Color.Red.Rgba);
-                ImGui.PushStyleColor(ImGuiCol.ButtonHovered, Color.Red.Rgba);
-                ImGui.PushStyleColor(ImGuiCol.ButtonActive, Color.Red.Rgba);
-            }
-            if (ImGui.Button(label, size) || trigger)
-            {
-                isSelected = !isSelected;
-                clicked = true;
-            }
-
-            if (wasSelected)
-            {
-                ImGui.PopStyleColor(3);
-            }
-            return clicked;
-        }
-
 
 
         private void DrawBreadcrumbs()
@@ -323,6 +213,12 @@ namespace T3.Gui.Graph
         }
 
         private string _windowTitle;
-        public GraphCanvas Canvas { get; private set; }
+
+        private ClipTime _clipTime = new ClipTime();
+        private static float _heightTimeLine = 100;
+        private CurveEditCanvas _curveEditor = new CurveEditCanvas();
+
+        // Styling properties
+        public static Vector2 _timeControlsSize = new Vector2(40, 0);
     }
 }
