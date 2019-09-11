b0d5cdea8259c6b3959fec496c7b537d8bf36e1b
cynic <daniel@szymanski.cc>
2019-09-11 18:08:42 +0200

    Added shader resource input to compute shader stage
    And enhanced test shader to use input texture.

--- T3/Resources/compute-shader.hlsl
+++ T3/Resources/compute-shader.hlsl
@@ -1,5 +1,7 @@
 RWTexture2D<float4> outputTexture : register(u0);
 
+Texture2D<float4> inputTexture : register(t0);
+
 cbuffer TimeConstants : register(b0)
 {
     float globalTime;
@@ -10,7 +12,12 @@ cbuffer TimeConstants : register(b0)
 [numthreads(16,16,1)]
 void main(uint3 i : SV_DispatchThreadID)
 {
-    float2 rg = (float2)i.xy / float2(300, 300);
+    uint width, height;
+    outputTexture.GetDimensions(width, height);
+    float2 rg = (float2)i.xy / float2(width, height);
     float b = sin(time)*0.5 + 0.5;
-    outputTexture[i.xy] = float4(rg.x, rg.y, b, 1.0);
+    float4 calcColor = float4(rg, b, 1);
+    float4 inputColor = inputTexture[i.xy*3];
+    float4 outputColor = lerp(calcColor, inputColor, 0.5);
+    outputTexture[i.xy] = outputColor;
 }
