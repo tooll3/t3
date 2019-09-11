4f6f0b8067864aa6dfd9fb1e26f6c2ca1b3d3d52
cynic <daniel@szymanski.cc>
2019-09-11 15:04:59 +0200

    Cleanup of Texture2d and LoadTexture2d ops
    Removed the shader resource view outputs

--- Operators/Types/LoadTexture2d.cs
+++ Operators/Types/LoadTexture2d.cs
@@ -7,20 +7,14 @@ namespace T3.Operators.Types
 {
     public class LoadTexture2d : Instance<LoadTexture2d>
     {
-        //[Output(Guid = "{E0C4FEDD-5C2F-46C8-B67D-5667435FB037}")]
-        //public readonly Slot<Texture2D> Texture = new Slot<Texture2D>();
+        [Output(Guid = "{E0C4FEDD-5C2F-46C8-B67D-5667435FB037}")]
+        public readonly Slot<Texture2D> Texture = new Slot<Texture2D>();
 
-        [Output(Guid = "{D45D32B6-E588-4723-A071-6E02406B831A}")]
-        public readonly Slot<ShaderResourceView> ShaderResourceView = new Slot<ShaderResourceView>();
-
-        private Texture2D _texture;
         private uint _textureResId;
-        private uint _textureSrvResId;
 
         public LoadTexture2d()
         {
-            //Texture.UpdateAction = UpdateTexture;
-            ShaderResourceView.UpdateAction = UpdateShaderResourceView;
+            Texture.UpdateAction = UpdateTexture;
         }
 
         private void UpdateTexture(EvaluationContext context)
@@ -28,18 +22,12 @@ namespace T3.Operators.Types
             if (Path.DirtyFlag.IsDirty)
             {
                 string imagePath = Path.GetValue(context);
-                (uint textureResId, uint textureSrvResId) = ResourceManager.Instance().CreateTextureFromFile(imagePath);
+                (uint textureResId, _) = ResourceManager.Instance().CreateTextureFromFile(imagePath);
                 _textureResId = textureResId;
-                _textureSrvResId = textureSrvResId;
+
+                if (ResourceManager.Instance().Resources[_textureResId] is TextureResource textureResource)
+                    Texture.Value = textureResource.Texture;
             }
-        }
-
-        private void UpdateShaderResourceView(EvaluationContext context)
-        {
-            UpdateTexture(context);
-
-            if (ResourceManager.Instance().Resources[_textureSrvResId] is ShaderResourceViewResource srvr)
-                ShaderResourceView.Value = srvr.ShaderResourceView;
         }
 
         [Input(Guid = "{76CC3811-4AE0-48B2-A119-890DB5A4EEB2}")]
