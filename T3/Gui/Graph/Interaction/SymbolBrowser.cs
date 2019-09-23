e5b4669f9b270a0e2f00394e5fb4a3f1e80c4dfc
pixtur <thomas.mann@framefield.com>
2019-09-23 09:18:58 +0200

    Added some cursor navigation to SymbolBrowser (left, right, esc, return)

--- T3/Gui/Graph/Interaction/SymbolBrowser.cs
+++ T3/Gui/Graph/Interaction/SymbolBrowser.cs
@@ -29,6 +29,14 @@ namespace T3.Gui.Graph
             PosOnCanvas = positionOnCanvas;
             _focusInputNextTime = true;
             _filterType = type;
+            _searchString = "";
+            _selectedSymbol = null;
+            _filter.Update();
+
+            if (_selectedSymbol == null && _filter.MatchingSymbols.Count > 0)
+            {
+                _selectedSymbol = _filter.MatchingSymbols[0];
+            }
         }
 
 
@@ -39,14 +47,16 @@ namespace T3.Gui.Graph
 
             Current = this;
 
+            _filter.Update();
+
             ImGui.PushID(_uiId);
             {
                 _posInWindow = GraphCanvas.Current.ChildPosFromCanvas(PosOnCanvas);
                 _posInScreen = GraphCanvas.Current.TransformPosition(PosOnCanvas);
                 _drawList = ImGui.GetWindowDrawList();
 
-                DrawSearchInput();
                 DrawMatchesList();
+                DrawSearchInput();
             }
             ImGui.PopID();
         }
@@ -71,12 +81,52 @@ namespace T3.Gui.Graph
                 ImGui.SetKeyboardFocusHere();
                 _focusInputNextTime = false;
             }
+
             ImGui.SetCursorPos(_posInWindow + new Vector2(1, 1));
             ImGui.PushStyleVar(ImGuiStyleVar.FramePadding, new Vector2(7, 6));
-            if (ImGui.InputText("##filter", ref _searchString, 10))
+            ImGui.InputText("##filter", ref _searchString, 10);
+
+            if (ImGui.IsKeyReleased((int)Key.CursorRight))
             {
-                //Log.Debug("completed search input");
+                if (_filter.MatchingSymbols.Count > 0)
+                {
+                    var index = _filter.MatchingSymbols.IndexOf(_selectedSymbol);
+                    index++;
+                    index %= _filter.MatchingSymbols.Count;
+                    _selectedSymbol = _filter.MatchingSymbols[index];
+                }
             }
+            else if (ImGui.IsKeyReleased((int)Key.CursorLeft))
+            {
+                if (_filter.MatchingSymbols.Count > 0)
+                {
+                    var index = _filter.MatchingSymbols.IndexOf(_selectedSymbol);
+                    index--;
+                    if (index < 0)
+                        index = _filter.MatchingSymbols.Count - 1;
+
+                    _selectedSymbol = _filter.MatchingSymbols[index];
+                }
+
+            }
+            else if (ImGui.IsKeyPressed((int)Key.Return))
+            {
+                if (_selectedSymbol != null)
+                {
+                    CreateInstance(_selectedSymbol);
+                }
+            }
+
+            if (ImGui.IsItemDeactivated() || ImGui.GetIO().KeysDownDuration[(int)Key.Esc] > 0)
+            {
+                IsOpen = false;
+                ConnectionMaker.Cancel();
+            }
+
+
+
+
+
             ImGui.PopStyleVar();
 
             if (ImGui.IsItemActive())
@@ -88,18 +138,6 @@ namespace T3.Gui.Graph
             }
         }
 
-        private Symbol.InputDefinition GetInputMatchingType(Symbol symbol, Type type)
-        {
-            foreach (var inputDefinition in symbol.InputDefinitions)
-            {
-                if (inputDefinition.DefaultValue.ValueType == type)
-                    return inputDefinition;
-            }
-            return null;
-        }
-
-
-
 
         private void DrawMatchesList()
         {
@@ -108,7 +146,7 @@ namespace T3.Gui.Graph
             ImGui.PushStyleVar(ImGuiStyleVar.ItemSpacing, new Vector2(10, 10));
 
             var typeUi = TypeUiRegistry.Entries[_filterType];
-            _filter.Update(_searchString);
+
 
             if (ImGui.BeginChildFrame(234, new Vector2(150, 200)))
             {
@@ -116,46 +154,19 @@ namespace T3.Gui.Graph
                 ImGui.TextDisabled(_filterType.Name);
                 ImGui.PopFont();
 
-                var parentSymbols = new List<Symbol>(GraphCanvas.Current.GetParentSymbols());
+
                 if (_selectedSymbol == null && SymbolRegistry.Entries.Values.Any())
                     _selectedSymbol = SymbolRegistry.Entries.Values.FirstOrDefault();
 
-
-                foreach (var symbol in SymbolRegistry.Entries.Values)
+                foreach (var symbol in _filter.MatchingSymbols)
                 {
-                    if (parentSymbols.Contains(symbol))
-                        continue;
-
-                    var matchingInputDef = GetInputMatchingType(symbol, _filterType);
-                    if (matchingInputDef == null)
-                        continue;
-
-                    if (!_filter.Match(symbol))
-                        continue;
-
                     ImGui.PushID(symbol.Id.GetHashCode());
                     {
-                        if (ImGui.Selectable("", symbol == _selectedSymbol))
+                        ImGui.Selectable("", symbol == _selectedSymbol);
+
+                        if (ImGui.IsItemActivated())
                         {
-                            var parent = GraphCanvas.Current.CompositionOp.Symbol;
-                            var addCommand = new AddSymbolChildCommand(parent, symbol.Id) { PosOnCanvas = PosOnCanvas };
-                            UndoRedoStack.AddAndExecute(addCommand);
-                            var newSymbolChild = parent.Children.Single(entry => entry.Id == addCommand.AddedChildId);
-
-                            if (symbol.InputDefinitions.Any())
-                            {
-                                var temp = ConnectionMaker.TempConnection;
-                                ConnectionMaker.CompleteConnectionIntoBuiltNode(
-                                    parent,
-                                    newSymbolChild,
-                                    matchingInputDef);
-                            }
-                            else
-                            {
-                                ConnectionMaker.Cancel();
-                            }
-
-                            IsOpen = false;
+                            CreateInstance(symbol);
                         }
                         ImGui.SameLine();
                         ImGui.Text(symbol.Name);
@@ -172,34 +183,108 @@ namespace T3.Gui.Graph
             ImGui.PopStyleVar(2);
         }
 
+
+        private void CreateInstance(Symbol symbol)
+        {
+            var parent = GraphCanvas.Current.CompositionOp.Symbol;
+            var addCommand = new AddSymbolChildCommand(parent, symbol.Id) { PosOnCanvas = PosOnCanvas };
+            UndoRedoStack.AddAndExecute(addCommand);
+            var newSymbolChild = parent.Children.Single(entry => entry.Id == addCommand.AddedChildId);
+
+            if (symbol.InputDefinitions.Any())
+            {
+                var temp = ConnectionMaker.TempConnection;
+                ConnectionMaker.CompleteConnectionIntoBuiltNode(
+                    parent,
+                    newSymbolChild,
+                    _filter.GetInputMatchingType(symbol, _filterType));
+            }
+            else
+            {
+                ConnectionMaker.Cancel();
+            }
+
+            IsOpen = false;
+        }
+
         /// <summary>
         /// Provides a regular expression to filter for matching <see cref="Symbol"/>s
         /// </summary>
-        private class NameFilter
+        private class Filter
         {
-            public void Update(string newSearchString)
+            public void Update()
             {
-                if (_currentFilter == newSearchString)
-                    return;
+                bool needsUpdate = false;
 
-                _currentFilter = newSearchString;
-                var pattern = string.Join(".*", newSearchString.ToCharArray());
-                _regex = new Regex(pattern, RegexOptions.IgnoreCase);
+                if (_currentSearchString != _searchString)
+                {
+                    _currentSearchString = _searchString;
+                    var pattern = string.Join(".*", _currentSearchString.ToCharArray());
+                    _currentRegex = new Regex(pattern, RegexOptions.IgnoreCase);
+                    needsUpdate = true;
+                }
+
+                if (_currentType != _filterType)
+                {
+                    _currentType = _filterType;
+                    needsUpdate = true;
+                }
+                if (needsUpdate)
+                {
+                    UpdateMatchingSymbols();
+                }
             }
 
-            private Regex _regex;
-            private string _currentFilter = null;
+            private Type _currentType;
+            public Symbol SelectedSymbol = null;
+
+            private void UpdateMatchingSymbols()
+            {
+                var parentSymbols = new List<Symbol>(GraphCanvas.Current.GetParentSymbols());
+
+                MatchingSymbols = new List<Symbol>();
+
+                foreach (var symbol in SymbolRegistry.Entries.Values)
+                {
+                    if (parentSymbols.Contains(symbol))
+                        continue;
+
+                    var matchingInputDef = GetInputMatchingType(symbol, _filterType);
+                    if (matchingInputDef == null)
+                        continue;
+
+                    if (!_currentRegex.IsMatch(symbol.Name))
+                        continue;
+
+                    MatchingSymbols.Add(symbol);
+                }
+            }
+
+            public Symbol.InputDefinition GetInputMatchingType(Symbol symbol, Type type)
+            {
+                foreach (var inputDefinition in symbol.InputDefinitions)
+                {
+                    if (inputDefinition.DefaultValue.ValueType == type)
+                        return inputDefinition;
+                }
+                return null;
+            }
+
+            public List<Symbol> MatchingSymbols { private set; get; } = null;
+
+            private Regex _currentRegex;
+            private string _currentSearchString = null;
 
             internal bool Match(Symbol symbol)
             {
-                return _regex.IsMatch(symbol.Name);
+                return _currentRegex.IsMatch(symbol.Name);
             }
         }
-        private NameFilter _filter = new NameFilter();
+        private Filter _filter = new Filter();
 
         #endregion
 
-        private Type _filterType;
+        private static Type _filterType;
 
         public List<Symbol.Connection> ConnectionsIn = new List<Symbol.Connection>();
         public Symbol.Connection ConnectionOut = null;
@@ -216,7 +301,7 @@ namespace T3.Gui.Graph
 
         private Symbol _selectedSymbol = null;
         private readonly static int _uiId = "DraftNode".GetHashCode();
-        private string _searchString = "";
+        private static string _searchString = "";
 
         public static SymbolBrowser Current;
     }
