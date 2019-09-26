ebc699a0ee594f551e65c6d6b36e6b93de5d7199
cynic <daniel@szymanski.cc>
2019-09-27 00:30:27 +0200

    Create own ComputeWobble op
    Only uses what really needed and also has some input parameters.

---
+++ Operators/Types/ComputeWobble.cs
@@ -0,0 +1,17 @@
+﻿using SharpDX.Direct3D11;
+using T3.Core.Operator;
+
+namespace T3.Operators.Types
+{
+    public class ComputeWobble : Instance<ComputeWobble>
+    {
+        [Output(Guid = "{F4E9BC98-6ED2-48A2-A2B6-A3372A74EA1B}")]
+        public readonly Slot<Texture2D> Output = new Slot<Texture2D>();
+
+        [Input(Guid = "{0214FD61-536B-4A58-9236-00466698269F}")]
+        public readonly InputSlot<float> Speed = new InputSlot<float>();
+
+        [Input(Guid = "{83771C7C-F861-4533-B4D2-3D9782A4C973}")]
+        public readonly InputSlot<float> Strength = new InputSlot<float>();
+    }
+}
--- T3/Resources/compute-wobble.hlsl
+++ T3/Resources/compute-wobble.hlsl
@@ -1,7 +1,5 @@
 RWTexture2D<float4> outputTexture : register(u0);
-
 Texture2D<float4> inputTexture : register(t0);
-Texture2D<float4> inputTexture2 : register(t1);
 sampler texSampler : register(s0);
 
 cbuffer TimeConstants : register(b0)
@@ -13,8 +11,8 @@ cbuffer TimeConstants : register(b0)
 
 cbuffer ParamConstants : register(b1)
 {
-    float param1;
-    float param2;
+    float speed;
+    float strength;
     float param3;
     float param4;
 }
@@ -26,12 +24,9 @@ void main(uint3 i : SV_DispatchThreadID)
     outputTexture.GetDimensions(width, height);
 
     float2 uv = (float2)i.xy / float2(width - 1, height - 1);
-    float b = sin(time)*0.5 + 0.5;
-    float4 calcColor = float4(uv, b, 1);
-    float4 inputColor = inputTexture.SampleLevel(texSampler, uv, 0.0);
-    inputColor *= 3*inputTexture2.SampleLevel(texSampler, uv, 0);
-    float4 outputColor = lerp(calcColor, inputColor, b);
-    outputColor.r = param1;
-
-    outputTexture[i.xy] = outputColor;
+    uv = uv*2.0 - 1.0;
+    float l = length(uv);
+    uv *= strength * sin(l*time*speed);
+    uv = uv*0.5 + 0.5;
+    outputTexture[i.xy] = inputTexture.SampleLevel(texSampler, uv, 0.0);
 }
