391969fb4ba3cd5410d62dfaff1077a82a716dc8
pixtur <thomas.mann@framefield.com>
2019-11-27 17:58:42 +0100

    Slightly refactored parameter window to prepare select input

--- T3/Gui/Windows/ParameterWindow.cs
+++ T3/Gui/Windows/ParameterWindow.cs
@@ -1,7 +1,5 @@
 ﻿using ImGuiNET;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Numerics;
 using T3.Core.Logging;
 using T3.Core.Operator;
@@ -16,21 +14,20 @@ namespace T3.Gui.Windows
     /// </summary>
     class ParameterWindow : Window
     {
-        public ParameterWindow() : base()
+        public ParameterWindow()
         {
             _instanceCounter++;
             Config.Title = "Parameters##" + _instanceCounter;
             AllowMultipleInstances = true;
             Config.Visible = true;
 
-            _parameterWindowInstances.Add(this);
+            ParameterWindowInstances.Add(this);
         }
 
-        private static List<Window> _parameterWindowInstances = new List<Window>();
 
         public override List<Window> GetInstances()
         {
-            return _parameterWindowInstances;
+            return ParameterWindowInstances;
         }
 
         protected override void UpdateBeforeDraw()
@@ -40,7 +37,7 @@ namespace T3.Gui.Windows
 
         protected override void DrawAllInstances()
         {
-            foreach (var w in _parameterWindowInstances)
+            foreach (var w in ParameterWindowInstances)
             {
                 w.DrawOneInstance();
             }
@@ -48,12 +45,13 @@ namespace T3.Gui.Windows
 
         protected override void Close()
         {
-            _parameterWindowInstances.Remove(this);
+            ParameterWindowInstances.Remove(this);
         }
 
         protected override void AddAnotherInstance()
         {
-            new ParameterWindow();
+            // ReSharper disable once ObjectCreationAsStatement
+            new ParameterWindow();    // Required to call constructor
         }
 
         protected override void DrawContent()
@@ -66,7 +64,67 @@ namespace T3.Gui.Windows
                 return;
             }
 
-            var opNamespace = op.Symbol.Namespace ?? "undefined";
+            DrawSelectedSymbolHeader(op);
+
+            var compositionSymbolUi = SymbolUiRegistry.Entries[op.Parent.Symbol.Id];
+            var selectedChildSymbolUi = SymbolUiRegistry.Entries[op.Symbol.Id];
+
+            foreach (var input in op.Inputs)
+            {
+                if (!selectedChildSymbolUi.InputUis.TryGetValue(input.Id, out IInputUi inputUi))
+                {
+                    Log.Warning("Trying to access an non existing input, probably the op instance is not the actual one.");
+                    continue;
+                }
+
+                ImGui.PushID(input.Id.GetHashCode());
+
+                if (_inputOptionsEditingReference.SymbolHash == op.Symbol.Id.GetHashCode())
+                {
+                    if (_inputOptionsEditingReference.InputHash == input.Id.GetHashCode())
+                    {
+                        inputUi.DrawParameterEdits();
+                        if (ImGui.Button("Back"))
+                        {
+                            _inputOptionsEditingReference = InputOptionsEditingReference.Inactive;
+                        }
+                    }
+                }
+                else
+                {
+                    var editState = inputUi.DrawInputEdit(input, compositionSymbolUi, _pinning.SelectedChildUi);
+
+                    if ((editState & InputEditState.Started) != InputEditState.Nothing)
+                    {
+                        _inputValueCommandInFlight = new ChangeInputValueCommand(op.Parent.Symbol, op.Id, input.Input);
+                    }
+
+                    if ((editState & InputEditState.Modified) != InputEditState.Nothing)
+                    {
+                        if (_inputValueCommandInFlight == null || _inputValueCommandInFlight.Value.ValueType != input.Input.Value.ValueType)
+                            _inputValueCommandInFlight = new ChangeInputValueCommand(op.Parent.Symbol, op.Id, input.Input);
+                        _inputValueCommandInFlight.Value.Assign(input.Input.Value);
+                    }
+
+                    if ((editState & InputEditState.Finished) != InputEditState.Nothing)
+                    {
+                        if (_inputValueCommandInFlight != null && _inputValueCommandInFlight.Value.ValueType == input.Input.Value.ValueType)
+                            UndoRedoStack.Add(_inputValueCommandInFlight);
+                    }
+
+                    if (editState == InputEditState.ShowOptions)
+                    {
+                        ShowParameterSettings(op, input);
+                    }
+                }
+
+                ImGui.PopID();
+            }
+        }
+
+        private void DrawSelectedSymbolHeader(Instance op)
+        {
+            //var opNamespace = op.Symbol.Namespace ?? "undefined";
 
             // Namespace
             {
@@ -99,7 +157,7 @@ namespace T3.Gui.Windows
                 ImGui.SameLine();
                 ImGui.SetNextItemWidth(-1);
                 var nameForEdit = op.Symbol.Name;
-                if (ImGui.InputText("##symbolname", ref nameForEdit, 128, ImGuiInputTextFlags.ReadOnly))
+                if (ImGui.InputText("##symbolName", ref nameForEdit, 128, ImGuiInputTextFlags.ReadOnly))
                 {
                     _symbolNameCommandInFlight.NewName = nameForEdit;
                     _symbolNameCommandInFlight.Do();
@@ -121,7 +179,7 @@ namespace T3.Gui.Windows
             {
                 ImGui.SetNextItemWidth(-1);
                 var nameForEdit = _pinning.SelectedChildUi.SymbolChild.Name;
-                if (ImGui.InputText("##symbolchildname", ref nameForEdit, 128))
+                if (ImGui.InputText("##symbolChildName", ref nameForEdit, 128))
                 {
                     _symbolChildNameCommand.NewName = nameForEdit;
                     _pinning.SelectedChildUi.SymbolChild.Name = nameForEdit;
@@ -139,88 +197,40 @@ namespace T3.Gui.Windows
                 }
             }
 
-            //ImGui.Spacing();
             ImGui.Dummy(new Vector2(0.0f, 5.0f));
-
-            var compositionSymbolUi = SymbolUiRegistry.Entries[op.Parent.Symbol.Id];
-            var selectedChildSymbolUi = SymbolUiRegistry.Entries[op.Symbol.Id];
-
-            foreach (var input in op.Inputs)
-            {
-                if (!selectedChildSymbolUi.InputUis.TryGetValue(input.Id, out IInputUi inputUi))
-                {
-                    Log.Warning("Trying to access an non existing input, probably the op instance is not the actual one.");
-                    continue;
-                }
-
-                ImGui.PushID(input.Id.GetHashCode());
-
-                if (_showInputParameterEdits.SymbolHash == op.Symbol.Id.GetHashCode())
-                {
-                    if (_showInputParameterEdits.InputHash == input.Id.GetHashCode())
-                    {
-                        inputUi.DrawParameterEdits();
-                        if (ImGui.Button("Back"))
-                        {
-                            _showInputParameterEdits = ShownInputParameterEdit.None;
-                        }
-                    }
-                }
-                else
-                {
-                    var editState = inputUi.DrawInputEdit(input, compositionSymbolUi, _pinning.SelectedChildUi);
-
-                    if ((editState & InputEditState.Started) != InputEditState.Nothing)
-                    {
-                        _inputValueCommandInFlight = new ChangeInputValueCommand(op.Parent.Symbol, op.Id, input.Input);
-                    }
-
-                    if ((editState & InputEditState.Modified) != InputEditState.Nothing)
-                    {
-                        if (_inputValueCommandInFlight == null || _inputValueCommandInFlight.Value.ValueType != input.Input.Value.ValueType)
-                            _inputValueCommandInFlight = new ChangeInputValueCommand(op.Parent.Symbol, op.Id, input.Input);
-                        _inputValueCommandInFlight.Value.Assign(input.Input.Value);
-                    }
-
-                    if ((editState & InputEditState.Finished) != InputEditState.Nothing)
-                    {
-                        if (_inputValueCommandInFlight != null && _inputValueCommandInFlight.Value.ValueType == input.Input.Value.ValueType)
-                            UndoRedoStack.Add(_inputValueCommandInFlight);
-                    }
-
-                    if (editState == InputEditState.ShowOptions)
-                    {
-                        _showInputParameterEdits = new ShownInputParameterEdit(op.Symbol.Id.GetHashCode(), input.Id.GetHashCode());
-                    }
-                }
-
-                ImGui.PopID();
-            }
         }
 
-        struct ShownInputParameterEdit
+        
+        private void ShowParameterSettings(Instance op, IInputSlot input)
         {
-            public static readonly ShownInputParameterEdit None = new ShownInputParameterEdit(0, 0);
+            _inputOptionsEditingReference = new InputOptionsEditingReference(op.Symbol.Id.GetHashCode(), input.Id.GetHashCode());
+        }
 
-            public ShownInputParameterEdit(int symbolHash, int inputHash)
+        
+        struct InputOptionsEditingReference
+        {
+            public static readonly InputOptionsEditingReference Inactive = new InputOptionsEditingReference(0, 0);
+
+            public InputOptionsEditingReference(int symbolHash, int inputHash)
             {
                 SymbolHash = symbolHash;
                 InputHash = inputHash;
             }
 
-            public int SymbolHash;
-            public int InputHash;
+            public bool IsInactive => (SymbolHash == Inactive.SymbolHash && InputHash == Inactive.SymbolHash); 
+            public readonly int SymbolHash;
+            public readonly int InputHash;
         }
+        
+        private static readonly List<Window> ParameterWindowInstances = new List<Window>();
 
-        private ShownInputParameterEdit _showInputParameterEdits = ShownInputParameterEdit.None;
-        private ChangeSymbolNameCommand _symbolNameCommandInFlight = null;
-        private ChangeSymbolNamespaceCommand _symbolNamespaceCommandInFlight = null;
-        private ChangeSymbolChildNameCommand _symbolChildNameCommand = null;
-        private ChangeInputValueCommand _inputValueCommandInFlight = null;
-
+        private InputOptionsEditingReference _inputOptionsEditingReference = InputOptionsEditingReference.Inactive;
+        private ChangeSymbolNameCommand _symbolNameCommandInFlight;
+        private ChangeSymbolNamespaceCommand _symbolNamespaceCommandInFlight;
+        private ChangeSymbolChildNameCommand _symbolChildNameCommand;
+        private ChangeInputValueCommand _inputValueCommandInFlight;
+        
         private readonly SelectionPinning _pinning = new SelectionPinning();
-
-        //private static List<ParameterWindow> WindowInstances = new List<ParameterWindow>();
-        static int _instanceCounter = 0;
+        private static int _instanceCounter;
     }
 }
