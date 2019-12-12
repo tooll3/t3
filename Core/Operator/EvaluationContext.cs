182fb1b8e399f1d707c633dfb42efad789484430
pixtur <thomas.mann@framefield.com>
2019-12-13 00:12:35 +0100

    Moved EvaluationContext to separate file

---
+++ Core/Operator/EvaluationContext.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using SharpDX;
+
+namespace T3.Core.Operator
+{
+    public class EvaluationContext
+    {
+        public EvaluationContext()
+        {
+            Time = GlobalTime;
+        }
+
+        public void Reset()
+        {
+            Time = GlobalTime;
+            ClipSpaceTcamera = Matrix.Identity;
+            CameraTworld = Matrix.Identity;
+            WorldTobject = Matrix.Identity;
+        }
+
+        private static readonly Stopwatch _runTimeWatch = Stopwatch.StartNew();
+        public static double RunTime => _runTimeWatch.ElapsedMilliseconds / 1000.0;
+        public static double GlobalTime { get; set; }
+        public static double BeatTime { get; set; }
+        public double Time { get; set; }
+
+        public Matrix ClipSpaceTcamera { get; set; } = Matrix.Identity;
+        public Matrix CameraTworld { get; set; } = Matrix.Identity;
+        public Matrix WorldTobject { get; set; } = Matrix.Identity;
+    }
+}
--- Core/Operator/Slot.cs
+++ Core/Operator/Slot.cs
@@ -8,30 +8,7 @@ using T3.Core.Logging;
 
 namespace T3.Core.Operator
 {
-    public class EvaluationContext
-    {
-        public EvaluationContext()
-        {
-            Time = GlobalTime;
-        }
 
-        public void Reset()
-        {
-            Time = GlobalTime;
-            ClipSpaceTcamera = Matrix.Identity;
-            CameraTworld = Matrix.Identity;
-            WorldTobject = Matrix.Identity;
-        }
-
-        private static readonly Stopwatch _runTimeWatch = Stopwatch.StartNew();
-        public static double RunTime => _runTimeWatch.ElapsedMilliseconds / 1000.0;
-        public static double GlobalTime { get; set; }
-        public double Time { get; set; }
-
-        public Matrix ClipSpaceTcamera { get; set; } = Matrix.Identity;
-        public Matrix CameraTworld { get; set; } = Matrix.Identity;
-        public Matrix WorldTobject { get; set; } = Matrix.Identity;
-    }
 
     public class OperatorAttribute : Attribute
     {
