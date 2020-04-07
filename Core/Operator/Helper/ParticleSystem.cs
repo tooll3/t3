cb3c2f664d9c52ab83238c6242d80b36ff1bf62f
cynic <daniel@szymanski.cc>
2020-04-07 16:00:47 +0200

    Particle system is now doing the UAV counter init by itself
    Also fixed the particle render pipeline example to render particles
    again.

--- Core/Operator/Helper/ParticleSystem.cs
+++ Core/Operator/Helper/ParticleSystem.cs
@@ -1,7 +1,9 @@
 ﻿using System.Linq;
 using SharpDX;
+using SharpDX.D3DCompiler;
 using SharpDX.Direct3D11;
 using SharpDX.DXGI;
+using T3.Core.Logging;
 
 namespace T3.Core.Operator.Helper
 {
@@ -25,10 +27,20 @@ namespace T3.Core.Operator.Helper
 
         public int MaxCount { get; set; } = 20480;
         public readonly int ParticleSizeInBytes = 48;
+        private uint _initDeadListShaderResId = ResourceManager.NullResource;
         public int ParticleSystemSizeInBytes => MaxCount * ParticleSizeInBytes;
 
         public void Init()
         {
+            if (_initDeadListShaderResId == ResourceManager.NullResource)
+            {
+                string sourcePath = @"Resources\lib\particles\particle-dead-list-init.hlsl";
+                string entryPoint = "main";
+                string debugName = "particle-dead-list-init";
+                var resourceManager = ResourceManager.Instance();
+                _initDeadListShaderResId = resourceManager.CreateComputeShaderFromFile(sourcePath, entryPoint, debugName, null);
+            }
+
             InitParticleBufferAndViews();
             InitDeadParticleIndices();
             InitAliveParticleIndices();
@@ -48,9 +60,32 @@ namespace T3.Core.Operator.Helper
 
         private void InitDeadParticleIndices()
         {
+            // init the buffer 
             var resourceManager = ResourceManager.Instance();
             resourceManager.SetupStructuredBuffer(4*MaxCount, 4, ref DeadParticleIndices);
             resourceManager.CreateStructuredBufferUav(DeadParticleIndices, UnorderedAccessViewBufferFlags.Append, ref DeadParticleIndicesUav);
+            
+            // init counter of the dead list buffer (must be done due to uav binding)
+            ComputeShader deadListInitShader = resourceManager.GetComputeShader(_initDeadListShaderResId);
+            var device = resourceManager.Device;
+            var deviceContext = device.ImmediateContext;
+            var csStage = deviceContext.ComputeShader;
+            var prevShader = csStage.Get();
+            var prevUavs = csStage.GetUnorderedAccessViews(0, 1);
+            
+            // set and call the init shader
+            var reflectedShader = new ShaderReflection(resourceManager.GetComputeShaderBytecode(_initDeadListShaderResId));
+            reflectedShader.GetThreadGroupSize(out int x, out int y, out int z);
+            
+            csStage.Set(deadListInitShader);
+            csStage.SetUnorderedAccessView(0, DeadParticleIndicesUav, 0);
+            int dispatchCount = MaxCount / (x > 0 ? x : 1);
+            Log.Info($"particle system: maxcount {{MaxCount}}  dispatchCount: {dispatchCount} *64: {dispatchCount*64}");
+            deviceContext.Dispatch(dispatchCount, 1, 1);
+            
+            // restore prev setup
+            csStage.SetUnorderedAccessView(0, prevUavs[0]);
+            csStage.Set(prevShader);
         }
 
         private void InitAliveParticleIndices()
