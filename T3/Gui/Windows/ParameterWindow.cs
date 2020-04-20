ccffd9822268325b0769ea09d542005c99c9d851
pixtur <thomas.mann@framefield.com>
2020-04-20 22:54:39 +0200

    Namespace and symbol name are no longer editable
    also added placeholder label to rename instance

--- T3/Gui/Windows/ParameterWindow.cs
+++ T3/Gui/Windows/ParameterWindow.cs
@@ -1,4 +1,5 @@
-﻿using ImGuiNET;
+﻿using System;
+using ImGuiNET;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
@@ -30,7 +31,6 @@ namespace T3.Gui.Windows
 
         protected override void UpdateBeforeDraw()
         {
-            
         }
 
         protected override void DrawAllInstances()
@@ -60,10 +60,10 @@ namespace T3.Gui.Windows
             {
                 if (instance.Parent == null)
                     return;
-                
+
                 var parentUi = SymbolUiRegistry.Entries[instance.Parent.Symbol.Id];
                 var symbolChildUi = parentUi.ChildUis.Single(childUi => childUi.Id == instance.SymbolChildId);
-                
+
                 DrawSelectedSymbolHeader(instance, symbolChildUi);
 
                 var compositionSymbolUi = SymbolUiRegistry.Entries[instance.Parent.Symbol.Id];
@@ -127,60 +127,26 @@ namespace T3.Gui.Windows
 
         private void DrawSelectedSymbolHeader(Instance op, SymbolChildUi symbolChildUi)
         {
-            // Namespace
+            // namespace and symbol
             {
+                ImGui.SetCursorPos( ImGui.GetCursorPos()+ Vector2.One * 5);
                 ImGui.PushStyleColor(ImGuiCol.Text, new Color(0.5f).Rgba);
-                ImGui.SetNextItemWidth(150);
-                var namespaceForEdit = op.Symbol.Namespace ?? "";
-
-                if (ImGui.InputText("##namespace", ref namespaceForEdit, 128))
-                {
-                    _symbolNamespaceCommandInFlight.NewNamespace = namespaceForEdit;
-                    _symbolNamespaceCommandInFlight.Do();
-                }
-
-                if (ImGui.IsItemActivated())
-                {
-                    _symbolNamespaceCommandInFlight = new ChangeSymbolNamespaceCommand(op.Symbol);
-                }
-
-                if (ImGui.IsItemDeactivatedAfterEdit())
-                {
-                    UndoRedoStack.Add(_symbolNamespaceCommandInFlight);
-                    _symbolNamespaceCommandInFlight = null;
-                }
-
+                ImGui.Text(op.Symbol.Namespace ?? "");
                 ImGui.PopStyleColor();
-            }
-
-            // Symbol Name
-            {
                 ImGui.SameLine();
-                ImGui.SetNextItemWidth(-1);
-                var nameForEdit = op.Symbol.Name;
-                if (ImGui.InputText("##symbolName", ref nameForEdit, 128, ImGuiInputTextFlags.ReadOnly))
-                {
-                    _symbolNameCommandInFlight.NewName = nameForEdit;
-                    _symbolNameCommandInFlight.Do();
-                }
-
-                if (ImGui.IsItemActivated())
-                {
-                    _symbolNameCommandInFlight = new ChangeSymbolNameCommand(op.Symbol);
-                }
-
-                if (ImGui.IsItemDeactivatedAfterEdit())
-                {
-                    UndoRedoStack.Add(_symbolNameCommandInFlight);
-                    _symbolNameCommandInFlight = null;
-                }
+                ImGui.Dummy(new Vector2(10,0));
+                ImGui.SameLine();
+                ImGui.Text(op.Symbol.Name);
+                ImGui.Dummy(Vector2.One * 5);
             }
 
             // SymbolChild Name
             {
+                ImGui.PushFont(Fonts.FontLarge);
                 ImGui.SetNextItemWidth(-1);
-                
+
                 var nameForEdit = symbolChildUi.SymbolChild.Name;
+
                 if (ImGui.InputText("##symbolChildName", ref nameForEdit, 128))
                 {
                     _symbolChildNameCommand.NewName = nameForEdit;
@@ -197,14 +163,26 @@ namespace T3.Gui.Windows
                     UndoRedoStack.Add(_symbolChildNameCommand);
                     _symbolChildNameCommand = null;
                 }
+
+                // Fake placeholder text
+                if (string.IsNullOrEmpty(nameForEdit))
+                {
+                    ImGui.PushStyleVar(ImGuiStyleVar.ItemSpacing, new Vector2(6, 6));
+                    ImGui.SetCursorScreenPos(ImGui.GetItemRectMin());
+                    ImGui.PushStyleColor(ImGuiCol.Text, new Color(0.2f).Rgba);
+                    ImGui.SetCursorPos( ImGui.GetCursorPos()+ Vector2.One * 5);
+                    ImGui.Text("Untitled instance");
+                    ImGui.PopStyleColor();
+                    ImGui.PopStyleVar();
+                }
+
+                ImGui.PopFont();
             }
 
             ImGui.Dummy(new Vector2(0.0f, 5.0f));
         }
 
         private static readonly List<Window> ParameterWindowInstances = new List<Window>();
-        private ChangeSymbolNameCommand _symbolNameCommandInFlight;
-        private ChangeSymbolNamespaceCommand _symbolNamespaceCommandInFlight;
         private ChangeSymbolChildNameCommand _symbolChildNameCommand;
         private ChangeInputValueCommand _inputValueCommandInFlight;
         private static int _instanceCounter;
