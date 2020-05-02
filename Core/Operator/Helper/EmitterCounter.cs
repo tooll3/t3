026cdcc54c61afd7d7103f0cc111b1401437c8f5
cynic <daniel@szymanski.cc>
2020-05-03 00:47:20 +0200

    Added emitter helper for ID distribution among emitters

---
+++ Core/Operator/Helper/EmitterCounter.cs
@@ -0,0 +1,12 @@
+﻿namespace T3.Operators
+{
+    public static class EmitterCounter
+    {
+        public static int GetId()
+        {
+            return ++_id;
+        }
+
+        private static int _id = 0;
+    }
+}
