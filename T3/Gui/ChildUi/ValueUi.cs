d1643489d03f42edbd6ab4178e9e72953aad9905
Thomas Mann <thomas.mann@framefield.com>
2020-06-12 00:40:02 +0200

    Fixed customui for ValueUi

--- T3/Gui/ChildUi/ValueUi.cs
+++ T3/Gui/ChildUi/ValueUi.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Linq;
 using System.Numerics;
 using ImGuiNET;
 using T3.Core.Logging;
 using T3.Core.Operator;
 using T3.Gui.InputUi;
+using T3.Gui.Styling;
 using T3.Operators.Types.Id_5d7d61ae_0a41_4ffa_a51d_93bab665e7fe;
 using UiHelpers;
 
@@ -16,36 +18,27 @@ namespace T3.Gui.ChildUi
             if (!(instance is Value valueInstance))
                 return SymbolChildUi.CustomUiResult.None;
 
-            // var valueSymbolUi = SymbolUiRegistry.Entries[instance.Symbol.Id];
-            // var inputUi = (FloatInputUi)valueSymbolUi.InputUis[valueInstance.Float.Id];
-            // ImGui.SetCursorScreenPos(selectableScreenRect.Min + Vector2.One * 10);
-            // ImGui.SetNextWindowSize(new Vector2(selectableScreenRect.GetWidth(), selectableScreenRect.GetHeight()));
-            // bool edited = ImGui.SliderFloat("value", ref valueInstance.Float.TypedInputValue.Value, inputUi.Min, inputUi.Max);
-            // if (edited)
-            // {
-            //     // todo: trigger for all instances
-            //     valueInstance.Float.DirtyFlag.Invalidate();
-            // }
-
+            var symbolChild = valueInstance.Parent.Symbol.Children.Single(c => c.Id == valueInstance.SymbolChildId);
             ImGui.PushClipRect(selectableScreenRect.Min, selectableScreenRect.Max, true);
-            var opacity = (float)Math.Sin(ImGui.GetTime());
-            drawList.AddRectFilled(selectableScreenRect.Min, selectableScreenRect.Max, new Color(1, 1, 0, 0.2f * opacity));
-            ImGui.SetCursorScreenPos(selectableScreenRect.Min + Vector2.One * 10);
-            ImGui.PushStyleColor(ImGuiCol.ButtonActive, Color.Red.Rgba);
-            if(ImGui.Button("X", new Vector2(10,10)))
+            
+            var h = selectableScreenRect.GetHeight();
+            var font = h > 50
+                           ? Fonts.FontLarge
+                           : (h > 25
+                                  ? Fonts.FontNormal
+                                  : Fonts.FontSmall);
+
+            ImGui.PushFont(font);
+            ImGui.SetCursorScreenPos(selectableScreenRect.Min + new Vector2(10,0));
+            ImGui.BeginGroup();
+            if (!string.IsNullOrEmpty(symbolChild.Name))
             {
-                valueInstance.Float.Value++;
-                valueInstance.Float.DirtyFlag.Invalidate();
-                Log.Debug("clicked!");
+                ImGui.Text(symbolChild.Name);
             }
 
-            if (ImGui.IsItemActive() && ImGui.IsMouseDragging(0))
-            {
-                valueInstance.Float.Value+= ImGui.GetMouseDragDelta(ImGuiMouseButton.Left).X;
-            }
-            ImGui.PopStyleColor();
-            ImGui.SameLine();
-            ImGui.Text($"{valueInstance.Result.Value:0.00}");
+            ImGui.Text($"{valueInstance.Float.TypedInputValue.Value:0.00}");
+            ImGui.EndGroup();
+            ImGui.PopFont();
             
             ImGui.PopClipRect();
             return SymbolChildUi.CustomUiResult.Rendered;
