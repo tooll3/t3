21887fe134693e4fbccb860523e8437eb81bb302
cynic <daniel@szymanski.cc>
2020-06-29 23:31:14 +0200

    Some more experiments with possible foreach solutions

---
+++ Core/Operator/Slots/Research/IterationOutputSlot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using T3.Core.Logging;
+
+namespace T3.Core.Operator.Slots
+{
+    public class IterationOutputSlot<T> : Slot<T>
+    {
+        private void UpdateForIteration(EvaluationContext context)
+        {
+            // get corresponding iteration input
+
+            // get all ops in corresponding composition op
+            // foreach (var child in Parent.Children)
+            // {
+            // Log.Debug($"{child.Symbol.Name}");
+            // }
+
+            _baseUpdateAction(context);
+        }
+
+        private Action<EvaluationContext> _baseUpdateAction;
+
+        public override Action<EvaluationContext> UpdateAction
+        {
+            set
+            {
+                _baseUpdateAction = value;
+                base.UpdateAction = UpdateForIteration;
+            }
+        }
+    }
+
+    public class IterationInputSlot<T> : InputSlot<T>
+    {
+    }
+}
