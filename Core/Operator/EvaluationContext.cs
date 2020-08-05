a21c54c30548a029adf797443b394d0ce931044c
cynic <daniel@szymanski.cc>
2020-08-05 10:34:05 +0200

    Small code cleanup

--- Core/Operator/EvaluationContext.cs
+++ Core/Operator/EvaluationContext.cs
@@ -1,7 +1,5 @@
 ﻿using System.Diagnostics;
-using System.Numerics;
 using SharpDX;
-using SharpDX.DirectWrite;
 using Vector3 = SharpDX.Vector3;
 
 namespace T3.Core.Operator
@@ -16,19 +14,18 @@ namespace T3.Core.Operator
         public void Reset()
         {
             TimeInBars = GlobalTimeInBars;
-            
+
             var fov = MathUtil.DegreesToRadians(45);
-            float aspectRatio = (float)RequestedResolution.Width/RequestedResolution.Height;
+            float aspectRatio = (float)RequestedResolution.Width / RequestedResolution.Height;
             CameraToClipSpace = Matrix.PerspectiveFovRH(fov, aspectRatio, 0.01f, 1000);
-            
-            Vector3 eye = new Vector3(0,0, 2.416f);
+
+            Vector3 eye = new Vector3(0, 0, 2.416f);
             Vector3 target = Vector3.Zero;
             Vector3 up = Vector3.Up;
             WorldToCamera = Matrix.LookAtRH(eye, target, up);
-            
+
             ObjectToWorld = Matrix.Identity;
         }
-        
 
         private static readonly Stopwatch _runTimeWatch = Stopwatch.StartNew();
         public static double RunTimeInSecs => _runTimeWatch.ElapsedMilliseconds / 1000.0;
