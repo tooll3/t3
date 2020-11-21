20541e049b20f6d0927990bee213ef6b9e9291aa
Thomas Mann <thomas.mann@framefield.com>
2020-11-21 18:23:40 +0100

    Update point structure

--- Core/DataTypes/Point.cs
+++ Core/DataTypes/Point.cs
@@ -2,12 +2,12 @@
 
 namespace T3.Core.DataTypes
 {
-    [StructLayout(LayoutKind.Explicit, Size = 16)]
+    [StructLayout(LayoutKind.Explicit, Size = 2 * 16)]
     public struct Point
     {
         [FieldOffset(0)]
         public SharpDX.Vector4 Position;
-        // [FieldOffset(16)]
-        // public SharpDX.Vector4 Orientation;
+        [FieldOffset(16)]
+        public SharpDX.Quaternion Orientation;    
     }
 }
