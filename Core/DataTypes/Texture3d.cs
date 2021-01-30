a28183e781317494d0482bb93b68d16091a08e62
cynic <daniel@szymanski.cc>
2021-01-30 14:52:41 +0100

    Fixed Texture3d RTV type

--- Core/DataTypes/Texture3d.cs
+++ Core/DataTypes/Texture3d.cs
@@ -7,6 +7,6 @@ namespace T3.Core.DataTypes
         public SharpDX.Direct3D11.Texture3D Texture;
         public SharpDX.Direct3D11.ShaderResourceView Srv;
         public SharpDX.Direct3D11.UnorderedAccessView Uav;
-        public SharpDX.Direct3D11.UnorderedAccessView Rtv;
+        public SharpDX.Direct3D11.RenderTargetView Rtv;
     }
 }
