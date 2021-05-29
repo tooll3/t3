8898c544a5616fe7899d8b91f0eef3dc71d51295
Thomas Mann <thomas.mann@framefield.com>
2021-05-29 21:44:49 +0200

    Add cimgui to dependencies installer

--- Install/install.bat
+++ Install/install.bat
@@ -1,9 +1,7 @@
 mkdir ..\T3\bin\Debug\
-copy dependencies\bass.dll ..\T3\bin\Debug\
-copy dependencies\basswasapi.dll ..\T3\bin\Debug\
+copy dependencies\*.dll ..\T3\bin\Debug\
 
 mkdir ..\T3\bin\Release\
-copy dependencies\bass.dll ..\T3\bin\Release\
-copy dependencies\basswasapi.dll ..\T3\bin\Release\
+copy dependencies\*.dll ..\T3\bin\Release\
 
 Xcopy /E /I default-layouts ..\.t3
