148c0264c9656926665293ac999ea62a8c17d3ac
Thomas Mann <thomas.mann@framefield.com>
2021-06-01 01:03:49 +0200

    First draft of _PrefilterSpecular

---
+++ Core/Rendering/DefaultRenderingStates.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using SharpDX;
+using SharpDX.Direct3D11;
+
+namespace T3.Core.Rendering
+{
+    /// <summary>
+    /// A helper class to generate and provide reusable default states for various DX rendering states
+    /// </summary>
+    public static class DefaultRenderingStates
+    {
+        private static DepthStencilState _defaultDepthStencilState;
+
+        public static DepthStencilState DefaultDepthStencilState
+        {
+            get
+            {
+                if (_defaultDepthStencilState == null && ResourceManager.Instance().Device != null)
+                {
+                    var depthStencilDescription = new DepthStencilStateDescription
+                                                      {
+                                                          IsDepthEnabled = true,
+                                                          DepthWriteMask = DepthWriteMask.All,
+                                                          DepthComparison = Comparison.Less,
+                                                          StencilReadMask = 255,
+                                                          StencilWriteMask = 255
+                                                      };
+                    _defaultDepthStencilState = new DepthStencilState(ResourceManager.Instance().Device, depthStencilDescription);
+                }
+
+                return _defaultDepthStencilState;
+            }
+        }
+
+        private static DepthStencilState _disabledDepthStencilState;
+
+        public static DepthStencilState DisabledDepthStencilState
+        {
+            get
+            {
+                if (_disabledDepthStencilState == null && ResourceManager.Instance().Device != null)
+                {
+                    var depthStencilDescription = new DepthStencilStateDescription
+                                                      {
+                                                          IsDepthEnabled = false
+                                                      };
+                    _disabledDepthStencilState = new DepthStencilState(ResourceManager.Instance().Device, depthStencilDescription);
+                }
+
+                return _disabledDepthStencilState;
+            }
+        }
+
+        private static BlendState _defaultBlendState;
+
+        public static BlendState DefaultBlendState
+        {
+            get
+            {
+                if (_defaultBlendState == null && ResourceManager.Instance().Device != null)
+                {
+                    var blendStateDescription = new BlendStateDescription();
+                    blendStateDescription.RenderTarget[0].IsBlendEnabled = true;
+                    blendStateDescription.RenderTarget[0].SourceBlend = BlendOption.SourceAlpha;
+                    blendStateDescription.RenderTarget[0].DestinationBlend = BlendOption.InverseSourceAlpha;
+                    blendStateDescription.RenderTarget[0].BlendOperation = BlendOperation.Add;
+                    blendStateDescription.RenderTarget[0].SourceAlphaBlend = BlendOption.One;
+                    blendStateDescription.RenderTarget[0].DestinationAlphaBlend = BlendOption.InverseSourceAlpha;
+                    blendStateDescription.RenderTarget[0].AlphaBlendOperation = BlendOperation.Add;
+                    blendStateDescription.RenderTarget[0].RenderTargetWriteMask = ColorWriteMaskFlags.All;
+                    blendStateDescription.AlphaToCoverageEnable = false;
+                    _defaultBlendState = new BlendState(ResourceManager.Instance().Device, blendStateDescription);
+                }
+
+                return _defaultBlendState;
+            }
+        }
+
+        private static BlendState _disabledBlendState;
+
+        public static BlendState DisabledBlendState
+        {
+            get
+            {
+                if (_disabledBlendState == null && ResourceManager.Instance().Device != null)
+                {
+                    var blendStateDescription = new BlendStateDescription();
+                    blendStateDescription.RenderTarget[0].IsBlendEnabled = false;
+                    blendStateDescription.RenderTarget[0].RenderTargetWriteMask = ColorWriteMaskFlags.All;
+                    _disabledBlendState = new BlendState(ResourceManager.Instance().Device, blendStateDescription);
+                }
+
+                return _disabledBlendState;
+            }
+        }
+
+        public static Color4 DefaultBlendFactor { get { return new Color4(1, 1, 1, 1); } }
+
+        private static RasterizerState _defaultRasterizerState;
+
+        public static RasterizerState DefaultRasterizerState
+        {
+            get
+            {
+                if (_defaultRasterizerState == null && ResourceManager.Instance().Device != null)
+                {
+                    var desc = new RasterizerStateDescription
+                                   {
+                                       FillMode = FillMode.Solid,
+                                       CullMode = CullMode.Back,
+                                       IsDepthClipEnabled = true
+                                   };
+                    _defaultRasterizerState = new RasterizerState(ResourceManager.Instance().Device, desc);
+                }
+
+                return _defaultRasterizerState;
+            }
+        }
+
+        private static SamplerState _defaultSamplerState;
+
+        public static SamplerState DefaultSamplerState
+        {
+            get
+            {
+                if (_defaultSamplerState == null && ResourceManager.Instance().Device != null)
+                {
+                    var desc = new SamplerStateDescription
+                                   {
+                                       Filter = Filter.MinMagMipPoint,
+                                       AddressU = TextureAddressMode.Clamp,
+                                       AddressV = TextureAddressMode.Clamp,
+                                       AddressW = TextureAddressMode.Clamp,
+                                       MaximumAnisotropy = 16,
+                                       ComparisonFunction = Comparison.Never,
+                                       MaximumLod = Single.MaxValue
+                                   };
+
+                    _defaultSamplerState = new SamplerState(ResourceManager.Instance().Device, desc);
+                }
+
+                return _defaultSamplerState;
+            }
+        }
+    }
+}
