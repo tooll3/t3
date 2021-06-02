97445c1bb863fc559d75956566c0ec531958ea86
Thomas Mann <thomas.mann@framefield.com>
2021-06-03 00:32:29 +0200

    Updated install bat to only copy dashboard layout

--- Install/install.bat
+++ Install/install.bat
@@ -5,4 +5,6 @@ mkdir ..\T3\bin\Release\
 copy dependencies\*.dll ..\T3\bin\Release\
 
 Xcopy /E /I default-layouts ..\.t3
-Xcopy /E /I default-dashboard ..\Operators\Types
+
+copy default-dashboard\Dashboard_dab61a12-9996-401e-9aa6-328dd6292beb.t3 ..\Operators\Types\
+copy default-dashboard\Dashboard_dab61a12-9996-401e-9aa6-328dd6292beb.t3ui ..\Operators\Types\
