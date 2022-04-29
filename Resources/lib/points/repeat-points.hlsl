aa684ed2c2672f29fece1671a58f7598e25a4e37
Thomas Mann <thomas.mann@framefield.com>
2022-04-29 10:45:37 +0200

    Update [RepeatAGPoints]

--- Resources/lib/points/repeat-points.hlsl
+++ Resources/lib/points/repeat-points.hlsl
@@ -19,28 +19,57 @@ void main(uint3 i : SV_DispatchThreadID)
     SourcePoints.GetDimensions(sourcePointCount,stride);
     TargetPoints.GetDimensions(targetPointCount,stride);
 
-
-    //uint sourcePointsBatch = (uint)(CountA + 0.1);  
-    uint pointIndex = i.x % sourcePointCount;
-    
-    if(pointIndex == sourcePointCount-1) {
-        ResultPoints[i.x].position =  0;
-        ResultPoints[i.x].w = sqrt(-1);
-    }
-    else {
+    if(ConnectPointsMode < 0.5) {
+        uint sourceIndex = i.x % sourcePointCount;
         uint targetIndex = (i.x / sourcePointCount )  % targetPointCount;
         
-        Point A = SourcePoints[pointIndex];
-        Point B = TargetPoints[targetIndex];
-        float s = ApplyTargetScaleW > 0.5 ? B.w : 1;
-        float3  pLocal = ApplyTargetOrietnation  > 0.5
-                        ? rotate_vector(A.position, B.rotation)
-                        : A.position;
+        if(sourceIndex == sourcePointCount-1) {
+            ResultPoints[i.x].position =  0;
+            ResultPoints[i.x].w = sqrt(-1);
+        }
+        else {
+            
+            Point A = SourcePoints[sourceIndex];
+            Point B = TargetPoints[targetIndex];
+            float s = ApplyTargetScaleW > 0.5 ? B.w : 1;
+            float3  pLocal = ApplyTargetOrietnation  > 0.5
+                            ? rotate_vector(A.position, B.rotation)
+                            : A.position;
 
-        ResultPoints[i.x].position = pLocal  * s + B.position;
-        ResultPoints[i.x].w = MultiplyTargetW > 0.5 ? A.w * B.w : A.w;
-        ResultPoints[i.x].rotation = ApplyTargetOrietnation  > 0.5 ? qmul(B.rotation, A.rotation)
-                                                                : A.rotation;
+            ResultPoints[i.x].position = pLocal  * s + B.position;
+            ResultPoints[i.x].w = MultiplyTargetW > 0.5 ? A.w * B.w : A.w;
+            ResultPoints[i.x].rotation = ApplyTargetOrietnation  > 0.5 ? qmul(B.rotation, A.rotation)
+                                                                    : A.rotation;
+        }
+    }
+    else {
+        uint loopLength = targetPointCount + 1;
+        uint sourceIndex = i.x / loopLength;
+        uint targetIndex = i.x % loopLength;
+        
+        if(targetIndex == loopLength - 1) {
+            ResultPoints[i.x].position =  0;
+            ResultPoints[i.x].w = sqrt(-1);
+        }
+        else {
+            //uint targetIndex = (i.x / sourcePointCount )  % targetPointCount;
+            //uint sourceIndex = i.x % loopLength;
+        
+            Point sourceP = SourcePoints[sourceIndex];
+            Point targetP = TargetPoints[targetIndex];
+
+            float s = ApplyTargetScaleW > 0.5 ? targetP.w : 1;
+            
+            float3  pLocal = ApplyTargetOrietnation  > 0.5
+                            ? rotate_vector(sourceP.position, targetP.rotation)
+                            : sourceP.position;
+
+            ResultPoints[i.x].position = pLocal  * s  + targetP.position;
+            ResultPoints[i.x].w = MultiplyTargetW > 0.5 ? sourceP.w * targetP.w : sourceP.w;
+            ResultPoints[i.x].rotation = ApplyTargetOrietnation  > 0.5 ? qmul(targetP.rotation, sourceP.rotation)
+                                                                    : sourceP.rotation;
+        }
+
     }
 
 }
