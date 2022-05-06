bc481dc64dd1a2e21bb9ed26259d8ad1e28b8176
Thomas Mann <thomas.mann@framefield.com>
2022-05-06 12:50:23 +0200

    Add [ColorGrade.Vignette] parameters to make the Operator more powerful

--- Operators/Types/ColorGrade.cs
+++ Operators/Types/ColorGrade.cs
@@ -11,21 +11,32 @@ namespace T3.Operators.Types.Id_42d86738_d644_47c8_ab92_cc426d958e51
         [Output(Guid = "1680781d-af5e-4b77-beb6-3e4a12d73d59")]
         public readonly Slot<Texture2D> Output = new Slot<Texture2D>();
 
-        
-        [Input(Guid = "777B2C27-A3C8-40D0-A196-80A08AF51296")]
-        public readonly InputSlot<Texture2D> Texture2d = new InputSlot<Texture2D>();
+        [Input(Guid = "777b2c27-a3c8-40d0-a196-80a08af51296")]
+        public readonly InputSlot<SharpDX.Direct3D11.Texture2D> Texture2d = new InputSlot<SharpDX.Direct3D11.Texture2D>();
 
-        [Input(Guid = "4DC44A7B-FE7C-4807-AAAA-53FB553DE017")]
+        [Input(Guid = "16231de9-2e85-4a9a-a2d1-99dfac18a0f6")]
+        public readonly InputSlot<float> PreSaturate = new InputSlot<float>();
+
+        [Input(Guid = "4dc44a7b-fe7c-4807-aaaa-53fb553de017")]
         public readonly InputSlot<System.Numerics.Vector4> Gain = new InputSlot<System.Numerics.Vector4>();
 
-        [Input(Guid = "BE4DC864-A5F9-4356-91F9-58DE8056A3A8")]
+        [Input(Guid = "be4dc864-a5f9-4356-91f9-58de8056a3a8")]
         public readonly InputSlot<System.Numerics.Vector4> Gamma = new InputSlot<System.Numerics.Vector4>();
 
-        [Input(Guid = "E8CC8A26-313E-4399-B800-901019BBAA78")]
+        [Input(Guid = "e8cc8a26-313e-4399-b800-901019bbaa78")]
         public readonly InputSlot<System.Numerics.Vector4> Lift = new InputSlot<System.Numerics.Vector4>();
-        
-        [Input(Guid = "16231DE9-2E85-4A9A-A2D1-99DFAC18A0F6")]
-        public readonly InputSlot<float> PreSaturate = new InputSlot<float>();
+
+        [Input(Guid = "423eb712-f7e3-4402-b841-324a9fc91c54")]
+        public readonly InputSlot<System.Numerics.Vector4> VignetteColor = new InputSlot<System.Numerics.Vector4>();
+
+        [Input(Guid = "a0aaadb8-3b39-4a29-b04b-5043ec8bbf42")]
+        public readonly InputSlot<float> VignetteRadius = new InputSlot<float>();
+
+        [Input(Guid = "e94da387-2c81-4ae0-a37e-3141e16c345d")]
+        public readonly InputSlot<float> VignetteFeather = new InputSlot<float>();
+
+        [Input(Guid = "f493d824-0f3e-4b30-838b-59ee0fba755b")]
+        public readonly InputSlot<System.Numerics.Vector2> VignetteCenter = new InputSlot<System.Numerics.Vector2>();
 
     }
 }
--- Resources/lib/img/img-fx-ColorGrade.hlsl
+++ Resources/lib/img/img-fx-ColorGrade.hlsl
@@ -7,6 +7,10 @@ cbuffer ParamConstants : register(b0)
     float4 Gain;
     float4 Gamma;
     float4 Lift;
+    float4 VignetteColor;
+    float2 VignetteCenter;
+    float VignetteRadius;
+    float VignetteBias;
     float PreSaturate;
 }
 
@@ -32,38 +36,86 @@ float4 psMain(vsOutput psInput) : SV_TARGET
     //outputTexture.GetDimensions(width, height);
 
     //float2 uv = (float2)i.xy/ float2(width - 1, height - 1);
-    float2 uv = psInput.texCoord;
-    float4 c = inputTexture.SampleLevel(texSampler, uv, 0.0);
+    // float2 uv = psInput.texCoord;
+    // float4 c = inputTexture.SampleLevel(texSampler, uv, 0.0);
 
-    float a = c.a;
+    // float a = c.a;
+    // c.rgb = clamp( c.rgb, 0.000001,1000);
+
+    // // Saturation
+    // float gray = (c.r *0.22 + c.g*0.707 + c.b*0.071);
+    // c.rgb = lerp( float3(gray, gray,gray), c.rgb, PreSaturate); 
+         
+    // // Grade        
+    // float3 liftScaled =   Lift * 2*Lift.a + (0.5-Lift.a);
+    // float3 gammaScaled =   Gamma * 2*Gamma.a + (0.5-Gamma.a);
+    // float3 gainScaled =   Gain * 2*Gain.a + (0.5-Gain.a);
+    
+    // c.rgb=  pow( 
+    //                ( c.rgb+ (liftScaled * 2-1)*(1-c))*      // Lift
+    //                ( gainScaled * 2 )  // Gain
+    //                ,    
+    //                1/((gammaScaled*2)));        
+    
+   
+    // c.rgb = clamp(c.rgb, 0.000001,1000);
+
+    // c.a = clamp(a,0,1);
+    
+    // return c; 
+
+
+    float2 uv = psInput.texCoord;
+    float4 c=inputTexture.Sample(texSampler, uv);
     c.rgb = clamp( c.rgb, 0.000001,1000);
 
     // Saturation
     float gray = (c.r *0.22 + c.g*0.707 + c.b*0.071);
-    c.rgb = lerp( float3(gray, gray,gray), c.rgb, PreSaturate); 
-         
+    c.rgb = lerp( float3(gray, gray, gray), c.rgb, PreSaturate); 
+
+    // Vignette
+    float flipEdge =  VignetteRadius < 0 ? -1 : 1;
+
+    float v = length( (uv - 0.5 - VignetteCenter * float2(1,-1)));
+    v /= VignetteRadius * flipEdge/2;
+    v -= 0.5;
+    v = smoothstep(0,1,(v- 0.5) / (VignetteBias* flipEdge*2) + 0.5);
+
     // Grade        
-    float3 liftScaled =   Lift * 2*Lift.a + (0.5-Lift.a);
-    float3 gammaScaled =   Gamma * 2*Gamma.a + (0.5-Gamma.a);
-    float3 gainScaled =   Gain * 2*Gain.a + (0.5-Gain.a);
+    float3 liftScaled =   Lift.rgb * 2*Lift.a + (0.5-Lift.a);
+    float3 gammaScaled =  Gamma.rgb * 2*Gamma.a + (0.5-Gamma.a);
+    float3 gainScaled =   Gain.rgb * 2*Gain.a + (0.5-Gain.a);
+    gainScaled += (VignetteColor.rgb-0.5) * v * (VignetteColor.a*2+1);
     
-    c.rgb=  pow( 
-                   ( c.rgb+ (liftScaled * 2-1)*(1-c))*      // Lift
-                   ( gainScaled * 2 )  // Gain
-                   ,    
-                   1/((gammaScaled*2)));        
-    
-   
-    c.rgb = clamp(c.rgb, 0.000001,1000);
+    c.rgb=  pow(   ( c.rgb + (liftScaled.rgb * 2 - 1 ) * (1-c.rgb))      // Lift
+                 * gainScaled * 2                        // Gain
+                 ,    
+                   1/(gammaScaled * 2));
+       
+    c.rgb = clamp(c.rgb, 0.000001,1000);        
+    c.a = clamp(c.a,0,1);
 
-    // PostSaturate (Deprecated because confusing)
-    //float gray2 = (c.r *0.22 + c.g*0.707 + c.b*0.071);
-    //c.rgb = lerp( float3(gray2, gray2,gray2), c.rgb, PostSaturate); 
-    //c.rgb = clamp(c.rgb, 0.000001,1000);
-        
 
-    c.a = clamp(a,0,1);
+    // // Vignette
+    // float r = pow( length( (uv - 0.5 - VignetteCenter * float2(1,-1)) / -VignetteRadius), VignetteBias);
+    // if(VignetteRadius < 0 )
+    //     r= 1-r;
+    // float v=  smoothstep( 0,1, r);
+
+
+    // // Grade        
+    // float3 liftScaled =   Lift.rgb * 4*Lift.a + (0.5-Lift.a);
+    // float3 gammaScaled =  Gamma.rgb * 4*Gamma.a + (0.5-Gamma.a);
+    // float3 gainScaled =   Gain.rgb * 4*Gain.a + (0.5-Gain.a);
+    // gainScaled += (VignetteColor.rgb-0.5) * v * (VignetteColor.a*2+1);
     
-	//outputTexture[i.xy] = c;
-    return c; 
+    
+    // c.rgb=  pow(   ( c.rgb + (liftScaled.rgb - 1 ) * (1-c.rgb))      // Lift
+    //              * gainScaled                        // Gain
+    //              ,    
+    //                1/gammaScaled);
+       
+
+
+    return c;
 }
