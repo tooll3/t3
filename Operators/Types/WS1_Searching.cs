8da484dc0efaf1e0344d749098e113eeb215e5fb
Thomas Mann <thomas.mann@framefield.com>
2022-06-15 00:13:06 +0200

    Build some VJ-Content for WAKE summerset

---
+++ Operators/Types/WS1_Searching.cs
@@ -0,0 +1,16 @@
+using T3.Core;
+using T3.Core.Operator;
+using T3.Core.Operator.Attributes;
+using T3.Core.Operator.Slots;
+
+namespace T3.Operators.Types.Id_90b20942_810b_480c_a19e_a41296cac9e6
+{
+    public class WS1_Searching : Instance<WS1_Searching>
+    {
+        [Output(Guid = "8902091c-9eee-4a4e-a55e-67768ba3465a")]
+        public readonly Slot<Command> Output = new Slot<Command>();
+
+
+    }
+}
+
