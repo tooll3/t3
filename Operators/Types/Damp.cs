5f00ede6bc04b64a575d22f9b5f9133a6f0c019a
Thomas Mann <thomas.mann@framefield.com>
2022-06-20 18:48:42 +0200

    Add spring damping method [Damp] and for blending presets with low precision midi faders

--- Operators/Types/Damp.cs
+++ Operators/Types/Damp.cs
@@ -28,32 +28,54 @@ namespace T3.Operators.Types.Id_af9c5db8_7144_4164_b605_b287aaf71bf6
                 return;
             
             _lastEvalTime = t;
-            const float FrameRate = 60;
+            const float FrameRate = 60f;
 
-            var framesPassed = (int)((Playback.LastFrameDuration * FrameRate) - 0.5f).Clamp(0,5) + 1 ;
-            
-            for (int stepIndex = 0; stepIndex < framesPassed; stepIndex++)
+            var method = (Methods)Method.GetValue(context).Clamp(0, 1);
+            switch (method)
             {
-                _dampedValue = MathUtils.Lerp(v,_dampedValue, damping);
-            }
+                case Methods.LinearInterpolation:
+                    var framesPassed = (int)((Playback.LastFrameDuration * FrameRate) - 0.5f).Clamp(0,5) + 1 ;
+                    
+                    for (int stepIndex = 0; stepIndex < framesPassed; stepIndex++)
+                    {
+                        _dampedValue = MathUtils.Lerp(v,_dampedValue, damping);
+                    }
 
+                    break;
+                
+                case Methods.DampedSpring:
+                    _dampedValue = MathUtils.SpringDamp(v,_dampedValue , ref _velocity, 0.5f/(damping + 0.001f),  (float)Playback.LastFrameDuration);
+                    break;
+            }
+            
             // Prevent NaN
-            if (!float.IsNormal(_dampedValue))
+            if (float.IsNaN(_dampedValue) || float.IsNaN(_velocity))
             {
                 _dampedValue = 0;
+                _velocity = 0;
             }
             
             Result.Value = _dampedValue;
         }
 
+        private enum Methods
+        {
+            LinearInterpolation,
+            DampedSpring
+        }
+        
         private float _dampedValue;
-        private double _lastEvalTime; 
+        private float _velocity;
+        private double _lastEvalTime;
         
         [Input(Guid = "795aca79-dd10-4f28-a290-a30e7b27b436")]
-        public readonly InputSlot<float> Value = new InputSlot<float>();
+        public readonly InputSlot<float> Value = new();
 
         [Input(Guid = "F29D5426-5E31-4C7C-BE77-5E45BFB9DAA9")]
-        public readonly InputSlot<float> Damping = new InputSlot<float>();
+        public readonly InputSlot<float> Damping = new();
+        
+        [Input(Guid = "76D52DF1-597E-4429-9916-13E6E0D93248", MappedType = typeof(Methods))]
+        public readonly InputSlot<int> Method = new();
         
     }
 }
