fc2abf11b340c57c841848efe106f8f4eb8505fb
Julien Vulliet <julien@mrvux.com>
2022-06-26 18:34:55 +0300

    Fix reflection not using the assembly parameter.
    Add More utils for reflection.

--- Core/Resource/ReflectionUtilities.cs
+++ Core/Resource/ReflectionUtilities.cs
@@ -11,7 +11,7 @@ namespace T3.Core
     {
         public static IEnumerable<Type> GetAssemblyTypes(this Assembly assembly, Predicate<Type> typePredicate)
         {
-            return Assembly.GetExecutingAssembly().GetExportedTypes().Where(t => typePredicate(t));
+            return assembly.GetExportedTypes().Where(t => typePredicate(t));
         }
 
         public static IReadOnlyList<Type> GetDomainTypes(this AppDomain appDomain, Predicate<Type> typePredicate)
@@ -27,6 +27,27 @@ namespace T3.Core
         public static IReadOnlyList<Type> GetCurrentDomainTypes(Predicate<Type> typePredicate)
         {
             return AppDomain.CurrentDomain.GetDomainTypes(typePredicate);
+        }
+
+        public static IEnumerable<Type> GetConcreteClassTypeOf<TInterface>(this Assembly assembly)
+        {
+            return assembly.GetAssemblyTypes(t => typeof(TInterface).IsAssignableFrom(t) && !t.IsAbstract && t.IsClass);
+        }
+
+
+        public static IReadOnlyList<Type> GetConcreteClassTypeOf<TInterface>(this AppDomain appDomain)
+        {
+            List<Type> types = new List<Type>();
+            foreach (var assembly in appDomain.GetAssemblies())
+            {
+                types.AddRange(assembly.GetConcreteClassTypeOf<TInterface>());
+            }
+            return types;
+        }
+
+        public static IReadOnlyList<Type> GetConcreteClassTypeOfFromDomain<TInterface>()
+        {
+            return AppDomain.CurrentDomain.GetConcreteClassTypeOf<TInterface>();
         }
     }
 }
