2a10342f514729f8d47f695a886a306f8563b99c
Thomas Mann <thomas.mann@framefield.com>
2022-07-09 17:30:13 +0200

    Add AlphaCutOff to [DrawMesh]

--- Operators/Types/DrawMesh.cs
+++ Operators/Types/DrawMesh.cs
@@ -29,6 +29,9 @@ namespace T3.Operators.Types.Id_a3c5471e_079b_4d4b_886a_ec02d6428ff6
         [Input(Guid = "dfad3400-885a-4f83-8c39-ec6520f4e2aa")]
         public readonly InputSlot<bool> EnableZWrite = new InputSlot<bool>();
 
+        [Input(Guid = "4748d9ab-58a4-41d7-a2ee-6f7dfed86211")]
+        public readonly InputSlot<float> AlphaCutOff = new InputSlot<float>();
+
     }
 }
 
