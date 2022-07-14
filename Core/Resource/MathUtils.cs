cb3a1e9edd9b7ca1bae217da388f935863db6b3b
Thomas Mann <thomas.mann@framefield.com>
2022-07-14 23:11:04 +0200

    Add random output for [LFO]

--- Core/Resource/MathUtils.cs
+++ Core/Resource/MathUtils.cs
@@ -35,6 +35,19 @@ namespace T3.Core
             return (float)(1.0 - ((n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0);
         }
 
+        public static uint XxHash(uint p)
+        {
+            const uint PRIME32_2 = 2246822519U, PRIME32_3 = 3266489917U;
+            const uint PRIME32_4 = 668265263U,  PRIME32_5 = 374761393U;
+
+            uint h32 = p + PRIME32_5;
+            h32 = PRIME32_4 * ((h32 << 17) | (h32 >> (32 - 17)));
+            h32 = PRIME32_2 * (h32 ^ (h32 >> 15));
+            h32 = PRIME32_3 * (h32 ^ (h32 >> 13));
+
+            return h32 ^ (h32 >> 16);
+        }
+        
         private static float Fade(float t)
         {
             return t * t * t * (t * (t * 6 - 15) + 10);
