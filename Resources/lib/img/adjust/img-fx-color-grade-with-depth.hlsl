d3507e0a2d183ffd12aaca86795c3ac1a36ef2b9
Thomas Mann <thomas.mann@framefield.com>
2022-07-30 21:48:14 +0200

    Add [ColorGradeDepth]

--- Resources/lib/img/adjust/img-fx-color-grade-with-depth.hlsl
+++ Resources/lib/img/adjust/img-fx-color-grade-with-depth.hlsl
@@ -1,17 +1,22 @@
-//RWTexture2D<float4> outputTexture : register(u0);
-Texture2D<float4> inputTexture : register(t0);
+Texture2D<float4> InputTexture : register(t0);
+Texture2D<float> DepthBuffer : register(t1);
+Texture2D<float4> Gradient : register(t2);
+
 sampler texSampler : register(s0);
 
 cbuffer ParamConstants : register(b0)
 {
     float4 Gain;
-    float4 Gamma;
-    float4 Lift;
-    float4 VignetteColor;
-    float2 VignetteCenter;
-    float VignetteRadius;
-    float VignetteBias;
-    float PreSaturate;
+    float4 Gamma;               // 4
+    float4 Lift;                // 8
+    float4 VignetteColor;       // 12
+    float2 VignetteCenter;      // 16
+    float VignetteRadius;       // 18
+    float VignetteBias;         // 19
+    float2 GradientDepthRange;  // 20
+    float NearClip;             // 22
+    float FarClip;              // 23
+    float PreSaturate;          // 24
 }
 
 
@@ -21,12 +26,28 @@ struct vsOutput
     float2 texCoord : TEXCOORD;
 };
 
+float DepthToSceneZ(float depth) 
+{
+    float n = NearClip;
+    float f = FarClip;
+    return (2.0 * n) / (f + n - depth * (f - n)) * (FarClip-NearClip) + NearClip;    
+}
+
 
 float4 psMain(vsOutput psInput) : SV_TARGET
 {
     float2 uv = psInput.texCoord;
-    float4 c=inputTexture.Sample(texSampler, uv);
+    float4 c=InputTexture.Sample(texSampler, uv);
     c.rgb = clamp( c.rgb, 0.000001,1000);
+
+    float depth = DepthBuffer.SampleLevel(texSampler, uv,0 ).r;
+    float z = DepthToSceneZ(depth);
+    float normalizedZ = saturate((z - GradientDepthRange.x) / (GradientDepthRange.y - GradientDepthRange.x));
+
+    //return float4(normalizedZ.xxx ,1);
+
+    float4 gradientColor = Gradient.SampleLevel(texSampler, float2(normalizedZ, 0.5),0);
+
 
     // Saturation
     float gray = (c.r *0.22 + c.g*0.707 + c.b*0.071);
@@ -44,7 +65,9 @@ float4 psMain(vsOutput psInput) : SV_TARGET
     float3 liftScaled =   Lift.rgb * 2*Lift.a + (0.5-Lift.a);
     float3 gammaScaled =  Gamma.rgb * 2*Gamma.a + (0.5-Gamma.a);
     float3 gainScaled =   Gain.rgb * 2*Gain.a + (0.5-Gain.a);
-    gainScaled += (VignetteColor.rgb-0.5) * v * (VignetteColor.a*2+1);
+    gainScaled += (VignetteColor.rgb-0.5) * v * (VignetteColor.a*2+1)
+               + (gradientColor.rgb - 0.5) * (gradientColor.a*2+1);
+    
     
     c.rgb=  pow((c.rgb + (liftScaled.rgb * 2 - 1 ) * (1-c.rgb))  // Lift
                  * gainScaled * 2,                               // Gain                     
@@ -52,5 +75,7 @@ float4 psMain(vsOutput psInput) : SV_TARGET
        
     c.rgb = clamp(c.rgb, 0.000001,1000);        
     c.a = clamp(c.a,0,1);
+
+
     return c;
 }
