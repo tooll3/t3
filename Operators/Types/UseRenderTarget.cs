c5ba8c4cf1bff39809a68ce6b31ecd35831193d0
Thomas Mann <thomas.mann@framefield.com>
2022-08-06 19:46:26 +0200

    Sonmi: Tweaking

--- Operators/Types/UseRenderTarget.cs
+++ Operators/Types/UseRenderTarget.cs
@@ -14,15 +14,20 @@ namespace T3.Operators.Types.Id_fd5ac393_02a4_43ae_8cdf_64a947abbafa
 {
     public class UseRenderTarget : Instance<UseRenderTarget>
     {
-        [Output(Guid = "7e846b2f-1d1a-4bfd-bbc8-d71aaaf9b5d4", DirtyFlagTrigger = DirtyFlagTrigger.Animated)]
-        public readonly Slot<Texture2D> Texture = new Slot<Texture2D>();
-
         [Output(Guid = "CB9850B3-119A-4839-B79F-A882112503C6")]
-        public readonly Slot<RenderTargetReference> Reference = new Slot<RenderTargetReference>();
+        public readonly Slot<RenderTargetReference> Reference = new();
+        
+        [Output(Guid = "7e846b2f-1d1a-4bfd-bbc8-d71aaaf9b5d4", DirtyFlagTrigger = DirtyFlagTrigger.Animated)]
+        public readonly Slot<Texture2D> Texture = new();
+
+        [Output(Guid = "FB232E1D-A09B-4B02-8E2F-408CD9AC1FFF", DirtyFlagTrigger = DirtyFlagTrigger.Animated)]
+        public readonly Slot<Texture2D> DepthTexture = new();
+
         
         public UseRenderTarget()
         {
             Texture.UpdateAction = UpdateTexture;
+            DepthTexture.UpdateAction = UpdateTexture;
             Reference.UpdateAction = UpdateTexture;
         }
         
@@ -30,8 +35,12 @@ namespace T3.Operators.Types.Id_fd5ac393_02a4_43ae_8cdf_64a947abbafa
         {
             Reference.Value = _renderTargetReference;
             Texture.Value = _renderTargetReference.ColorTexture;
+            DepthTexture.Value = _renderTargetReference.DepthTexture;
+            
+            DepthTexture.DirtyFlag.Clear();
+            Texture.DirtyFlag.Clear();
         }
 
-        private readonly RenderTargetReference _renderTargetReference = new RenderTargetReference();
+        private readonly RenderTargetReference _renderTargetReference = new();
     }
 }
