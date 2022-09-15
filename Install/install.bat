27c9d2baae32ab68f6304a3315166b7f0d3fec03
Thomas Mann <thomas.mann@framefield.com>
2022-09-15 23:16:39 +0200

    Updated gitignore fix Dashboard issue

--- Install/install.bat
+++ Install/install.bat
@@ -9,5 +9,6 @@ copy dependencies\*.dll ..\Player\bin\Release\net5.0-windows\
 
 Xcopy /E /I default-layouts ..\.t3\layouts\
 
+copy default-dashboard\Dashboard.cs ..\Operators\Types\
 copy default-dashboard\Dashboard_dab61a12-9996-401e-9aa6-328dd6292beb.t3 ..\Operators\Types\
 copy default-dashboard\Dashboard_dab61a12-9996-401e-9aa6-328dd6292beb.t3ui ..\Operators\Types\
