64e4d2191389f492188cc5fd16c2dc9a705b6746
Thomas Mann <thomas.mann@framefield.com>
2022-09-15 23:05:04 +0200

    Switching to .net 6

--- Install/install.bat
+++ Install/install.bat
@@ -1,11 +1,11 @@
-mkdir ..\T3\bin\Debug\net5.0-windows
-copy dependencies\*.dll ..\T3\bin\Debug\net5.0-windows\
+mkdir ..\T3\bin\Debug\net6.0-windows
+copy dependencies\*.dll ..\T3\bin\Debug\net6.0-windows\
 
-mkdir ..\T3\bin\Release\net5.0-windows
-copy dependencies\*.dll ..\T3\bin\Release\net5.0-windows\
+mkdir ..\T3\bin\Release\net6.0-windows
+copy dependencies\*.dll ..\T3\bin\Release\net6.0-windows\
 
-mkdir ..\Player\bin\Release\net5.0-windows
-copy dependencies\*.dll ..\Player\bin\Release\net5.0-windows\
+mkdir ..\Player\bin\Release\net6.0-windows
+copy dependencies\*.dll ..\Player\bin\Release\net6.0-windows\
 
 Xcopy /E /I default-layouts ..\.t3\layouts\
 
