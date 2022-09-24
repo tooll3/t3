c947f759a5c5dceb151c315a9436ebe710948114
Thomas Mann <thomas.mann@framefield.com>
2022-09-24 03:04:16 +0200

    Add [DetectPulse] and improve [HasValueChange] with some timing parameters

--- Operators/Types/lib/math/bool/HasValueChanged.cs
+++ Operators/Types/lib/math/bool/HasValueChanged.cs
@@ -16,7 +16,10 @@ namespace T3.Operators.Types.Id_146fae64_18da_4183_9794_a322f47c669e
         [Output(Guid = "ab818835-77a1-4cd9-b2ad-c503034e49f9")]
         public readonly Slot<float> Delta = new Slot<float>();
 
+        [Output(Guid = "5E37DA80-5D1F-4E17-A0A1-1E386B3A2561")]
+        public readonly Slot<float> DeltaOnHit = new Slot<float>();
 
+        
         public HasValueChanged()
         {
             HasChanged.UpdateAction = Update;
@@ -27,13 +30,16 @@ namespace T3.Operators.Types.Id_146fae64_18da_4183_9794_a322f47c669e
         {
             var newValue = Value.GetValue(context);
             var threshold = Threshold.GetValue(context);
+            var minTimeBetweenHits = MinTimeBetweenHits.GetValue(context);
 
             var hasChanged = false;
+            
+            float delta = Math.Abs(newValue - _lastValue);
 
             switch ((Modes)Mode.GetValue(context).Clamp(0, Enum.GetNames(typeof(Modes)).Length -1))
             {
                 case Modes.Changed:
-                    var increase = Math.Abs(newValue - _lastValue) > threshold;
+                    var increase = delta > threshold;
                     hasChanged = increase;
                     break;
                 
@@ -48,31 +54,56 @@ namespace T3.Operators.Types.Id_146fae64_18da_4183_9794_a322f47c669e
                 default:
                     throw new ArgumentOutOfRangeException();
             }
+
+            var wasTriggered = MathUtils.WasTriggered(hasChanged, ref _wasHit);
+            
+            if (hasChanged && (PreventContinuedChanges.GetValue(context) || wasTriggered))
+            {
+                var timeSinceLastHit = context.LocalFxTime - _lastHitTime;
+                if (timeSinceLastHit >= minTimeBetweenHits)
+                {
+                    _lastHitTime = context.LocalFxTime;
+                    _lastHitDelta = delta;
+                }
+                else
+                {
+                    hasChanged = false;
+                }
+            }
             
             HasChanged.Value = hasChanged;
 
             Delta.Value = newValue - _lastValue;
             _lastValue = newValue;
+            DeltaOnHit.Value = (float)_lastHitDelta;
         }
 
         private float _lastValue;
+        private double _lastHitTime;
+        private float _lastHitDelta;
+        private bool _wasHit;
 
         [Input(Guid = "7f5fb125-8aca-4344-8b30-e7d4e7873c1c")]
-        public readonly InputSlot<float> Value = new InputSlot<float>();
+        public readonly InputSlot<float> Value = new();
 
         [Input(Guid = "1e03f11b-f6cc-497e-9528-ed9490e878b5")]
-        public readonly InputSlot<float> Threshold = new InputSlot<float>();
+        public readonly InputSlot<float> Threshold = new();
 
         [Input(Guid = "031ef11b-f6cc-497e-9528-ed9490e878b5", MappedType = typeof(Modes))]
-        public readonly InputSlot<int> Mode = new InputSlot<int>();
+        public readonly InputSlot<int> Mode = new();
+
+        [Input(Guid = "B994249D-C101-41D9-8142-A4F675FCD70C")]
+        public readonly InputSlot<float> MinTimeBetweenHits = new();
+
+        [Input(Guid = "8EBF4715-0B4B-4CDD-A079-9F91C2DF0476")]
+        public readonly InputSlot<bool> PreventContinuedChanges = new();
+
 
         private enum Modes
         {
             Changed,
             Increased,
-            Decreased
+            Decreased,
         }
-
-
     }
 }
---
+++ Operators/Types/lib/math/float/DetectPulse.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Diagnostics;
+using T3.Core;
+using T3.Core.Logging;
+using T3.Core.Operator;
+using T3.Core.Operator.Attributes;
+using T3.Core.Operator.Slots;
+using T3.Operators.Types.Id_af9c5db8_7144_4164_b605_b287aaf71bf6;
+
+namespace T3.Operators.Types.Id_7ff47023_622e_4834_8de5_2438d56c09bd
+{
+    public class DetectPulse : Instance<DetectPulse>
+    {
+        [Output(Guid = "b3254af8-03c6-41af-a775-5724aaaa91ef")]
+        public readonly Slot<bool> HasChanged = new();
+        
+        public DetectPulse()
+        {
+            HasChanged.UpdateAction = Update;
+        }
+
+        private void Update(EvaluationContext context)
+        {
+            var newValue = Value.GetValue(context);
+            var threshold = Threshold.GetValue(context);
+            var minTimeBetweenHits = MinTimeBetweenHits.GetValue(context);
+
+            var hasTimeDecreased = context.LocalFxTime < _lastHitTime;
+            if (hasTimeDecreased)
+                _lastHitTime = 0;
+            
+            var deltaToDamped = MathF.Abs(_dampedValue - newValue);
+            
+            var dampFactor = Damping.GetValue(context).Clamp(0,1);
+            _dampedValue = MathUtils.Lerp(newValue, _dampedValue, dampFactor);
+
+            var exceedsThreshold = deltaToDamped > threshold;
+            var wasTriggered = MathUtils.WasTriggered(exceedsThreshold, ref _wasHit);
+
+            var isHit = false;
+            var timeSinceLastHit = context.LocalFxTime - _lastHitTime;
+            if (wasTriggered && timeSinceLastHit >= minTimeBetweenHits)
+            {
+                if (timeSinceLastHit >= minTimeBetweenHits)
+                {
+                    _lastHitTime = context.LocalFxTime;
+                    isHit = true;
+                }
+            }
+            
+            HasChanged.Value = isHit;
+        }
+
+        private double _lastHitTime;
+        private bool _wasHit;
+        private float _dampedValue;
+        private double _updateTime;
+
+        [Input(Guid = "c0b97bc3-1f93-4678-b93f-d364374fd40a")]
+        public readonly InputSlot<float> Value = new();
+
+        [Input(Guid = "4424863B-B25C-449A-8AC9-16796617AF7B")]
+        public readonly InputSlot<float> Damping = new();
+
+        [Input(Guid = "b9a322a3-b85d-4e51-ad1c-17d61a6f5f5e")]
+        public readonly InputSlot<float> Threshold = new();
+
+        [Input(Guid = "2cf8d9e3-a984-4760-88cd-5dc2c8b66d7b")]
+        public readonly InputSlot<float> MinTimeBetweenHits = new();
+    }
+}
