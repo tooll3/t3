f657444ea6b7700040679249b24e239c8f526b25
Thomas Mann <thomas.mann@framefield.com>
2022-10-04 20:08:55 +0200

    Updated some animation projects

---
+++ Operators/Types/user/ff/pc/elements/PCProfileTransition.cs
@@ -0,0 +1,22 @@
+using T3.Core;
+using T3.Core.Operator;
+using T3.Core.Operator.Attributes;
+using T3.Core.Operator.Slots;
+
+namespace T3.Operators.Types.Id_a85870e1_6b18_40db_96e2_076536d5a521
+{
+    public class PCProfileTransition : Instance<PCProfileTransition>
+    {
+        [Output(Guid = "df1297ac-fd7a-44ae-bf18-c37b6a0c8c4d")]
+        public readonly Slot<Command> Output = new Slot<Command>();
+
+        [Input(Guid = "1e9a70d7-a85a-4b3d-bea1-c0c7c5e8f09d")]
+        public readonly InputSlot<string> ProfileImage = new InputSlot<string>();
+
+        [Input(Guid = "6d1dd4e7-560e-4374-adf1-8e8245070cda")]
+        public readonly InputSlot<float> Transition = new InputSlot<float>();
+
+
+    }
+}
+
