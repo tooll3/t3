a9664227d47b0c7b0d8114457c2a2cd47873144d
Thomas Mann <thomas.mann@framefield.com>
2022-10-20 01:42:30 +0200

    [ValuesToTextures] supports auto sizing from input value list length

--- Operators/Types/lib/math/floats/ValuesToTexture.cs
+++ Operators/Types/lib/math/floats/ValuesToTexture.cs
@@ -20,50 +20,48 @@ namespace T3.Operators.Types.Id_55cc0f79_96c9_482e_9794_934dc0f87708
         [Output(Guid = "f01099a0-a196-4689-9900-edac07908714")]
         public readonly Slot<Texture2D> CurveTexture = new Slot<Texture2D>();
 
-        
-
         public ValuesToTexture()
         {
             CurveTexture.UpdateAction = Update;
         }
 
         private float[] _floatBuffer = new float[0];
-        
+
         private void Update(EvaluationContext context)
         {
-            
             if (!Values.IsConnected)
                 return;
 
             var values = Values.GetValue(context);
             if (values == null || values.Count == 0)
                 return;
-            
 
-            int rangeStart = RangeStart.GetValue(context).Clamp(0, values.Count-1);
-            int rangeEnd = RangeEnd.GetValue(context).Clamp(0, values.Count-1);
+            var rangeStart = RangeStart.GetValue(context).Clamp(0, values.Count - 1);
+            var rangeEnd = RangeEnd.GetValue(context).Clamp(0, values.Count - 1);
+
+            if (UseFullList.GetValue(context))
+            {
+                rangeStart = 0;
+                rangeEnd = values.Count - 1;
+            }
             
-            float gain = Gain.GetValue(context);
-            float pow = Pow.GetValue(context);
+            var gain = Gain.GetValue(context);
+            var pow = Pow.GetValue(context);
 
             if (Math.Abs(pow) < 0.001f)
             {
                 return;
             }
             
-
             if (rangeEnd < rangeStart)
             {
-                var tmp = rangeEnd;
-                rangeEnd = rangeStart;
-                rangeStart = tmp;
+                (rangeEnd, rangeStart) = (rangeStart, rangeEnd);
             }
-            
-            int sampleCount = (rangeEnd - rangeStart) + 1;
-            int entrySizeInBytes = sizeof(float);
-            int listSizeInBytes = sampleCount * entrySizeInBytes;
-            int bufferSizeInBytes = 1 * listSizeInBytes;
-            
+
+            var sampleCount = (rangeEnd - rangeStart) + 1;
+            var entrySizeInBytes = sizeof(float);
+            var listSizeInBytes = sampleCount * entrySizeInBytes;
+            var bufferSizeInBytes = 1 * listSizeInBytes;
 
             using (var dataStream = new DataStream(bufferSizeInBytes, true, true))
             {
@@ -80,23 +78,11 @@ namespace T3.Operators.Types.Id_55cc0f79_96c9_482e_9794_934dc0f87708
                                       SampleDescription = new SharpDX.DXGI.SampleDescription(1, 0),
                                   };
 
-                //foreach (var curveInput in Curves.CollectedInputs)
-                //{
-                    // var curve = curveInput.GetValue(context);
-                    // if (curve == null)
-                    // {
-                    //     dataStream.Seek(curveSizeInBytes, SeekOrigin.Current);
-                    //     continue;
-                    // }
-
-                    for (var sampleIndex = rangeStart; sampleIndex <= rangeEnd; sampleIndex++)
-                    {
-                        //dataStream.Write((float)curve.GetSampledValue((float)sampleIndex / sampleCount));
-                        float v = (float)Math.Pow(values[sampleIndex] * gain, pow);
-                        dataStream.Write(v );
-                    }
-                //}
-                //Curves.DirtyFlag.Clear();
+                for (var sampleIndex = rangeStart; sampleIndex <= rangeEnd; sampleIndex++)
+                {
+                    float v = (float)Math.Pow(values[sampleIndex] * gain, pow);
+                    dataStream.Write(v);
+                }
 
                 dataStream.Position = 0;
                 var dataRectangles = new DataRectangle[] { new DataRectangle(dataStream.DataPointer, listSizeInBytes) };
@@ -104,25 +90,25 @@ namespace T3.Operators.Types.Id_55cc0f79_96c9_482e_9794_934dc0f87708
                 CurveTexture.Value = new Texture2D(ResourceManager.Device, texDesc, dataRectangles);
             }
         }
-
-
-        // [Input(Guid = "3a443955-69d4-4e96-a819-a221c71ca2cd")]
-        // public readonly MultiInputSlot<Curve> Curves = new MultiInputSlot<T3.Core.Animation.Curve>();
         
         [Input(Guid = "092C8D1F-A70E-4298-B5DF-52C9D62F8E04")]
-        public readonly InputSlot<List<float>> Values = new InputSlot<List<float>>();
+        public readonly InputSlot<List<float>> Values = new();
 
+        [Input(Guid = "165F7E0E-6EF0-4BE1-8ED3-61ED0DB752ED")]
+        public readonly InputSlot<bool> UseFullList = new();
+        
         [Input(Guid = "CA67BFAF-EDE7-43BA-B279-FC1DDFBE2FFA")]
-        public readonly InputSlot<int> RangeStart = new InputSlot<int>();
+        public readonly InputSlot<int> RangeStart = new();
 
         [Input(Guid = "DB868176-D51C-41AA-BAFE-68C3E50E725E")]
-        public readonly InputSlot<int> RangeEnd = new InputSlot<int>();
-        
+        public readonly InputSlot<int> RangeEnd = new();
+
         [Input(Guid = "CC812393-F080-4E17-A525-15B09F8ACDD0")]
-        public readonly InputSlot<float> Gain = new InputSlot<float>();
+        public readonly InputSlot<float> Gain = new();
 
         [Input(Guid = "51545316-69FC-441F-B59F-44979E32972C")]
-        public readonly InputSlot<float> Pow = new InputSlot<float>();
+        public readonly InputSlot<float> Pow = new();
+        
         
     }
 }
