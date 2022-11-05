17ff65e362223329308f3029706323bcc83ab772
Thomas Mann <thomas.mann@framefield.com>
2022-11-05 02:04:00 +0100

    Add first stub for fc2 demo

---
+++ Operators/Types/user/still/fc2/FC2.cs
@@ -0,0 +1,16 @@
+using SharpDX.Direct3D11;
+using T3.Core.Operator;
+using T3.Core.Operator.Attributes;
+using T3.Core.Operator.Slots;
+
+namespace T3.Operators.Types.Id_ace28a0c_b71a_41eb_bd50_b57da38a23ce
+{
+    public class FC2 : Instance<FC2>
+    {
+        [Output(Guid = "c072b400-ff32-4840-ba91-d10622330a21")]
+        public readonly Slot<Texture2D> ColorBuffer = new Slot<Texture2D>();
+
+
+    }
+}
+
