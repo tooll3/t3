79a175f40f486642361fbffc378f474a7f1bb9e7
Thomas Mann <thomas.mann@framefield.com>
2022-11-05 16:02:07 +0100

    Clean up FC2

--- Operators/Types/user/still/fc2/FC2.cs
+++ Operators/Types/user/still/fc2/FC2.cs
@@ -7,8 +7,9 @@ namespace T3.Operators.Types.Id_ace28a0c_b71a_41eb_bd50_b57da38a23ce
 {
     public class FC2 : Instance<FC2>
     {
-        [Output(Guid = "c072b400-ff32-4840-ba91-d10622330a21")]
-        public readonly Slot<Texture2D> ColorBuffer = new Slot<Texture2D>();
+
+        [Output(Guid = "6cb5826d-6999-4d5c-bf10-badc690666e8")]
+        public readonly Slot<SharpDX.Direct3D11.Texture2D> Output2 = new Slot<SharpDX.Direct3D11.Texture2D>();
 
 
     }
