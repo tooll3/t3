fd9e668ab314eff859ad820c23f874bd3daff30e
Thomas Mann <thomas.mann@framefield.com>
2023-02-06 21:45:45 +0100

    Fixed implementation for [PairPointsForSplines]

--- Resources/lib/points/combine/pair-points-for-splines.hlsl
+++ Resources/lib/points/combine/pair-points-for-splines.hlsl
@@ -48,7 +48,6 @@ void main(uint3 i : SV_DispatchThreadID)
     uint indexA = pairIndex % countA;
     uint indexB = pairIndex % countB;
 
-
     float3 pA1 = PointsA[indexA].position;
     float3 pB1 = PointsB[indexB].position;
     float3 forward = TangentDirection;
@@ -56,15 +55,38 @@ void main(uint3 i : SV_DispatchThreadID)
     float3 tA = rotate_vector( forward, PointsA[indexA].rotation) * (TangentA + PointsA[indexA].w * TangentA_WFactor);
     float3 tB = rotate_vector( forward, PointsB[indexB].rotation) * (TangentB + PointsB[indexB].w * TangentB_WFactor);
 
-    float3 pAA = lerp(pA1, pA1+tA, 1-(1-f)* (1-f));
-    float3 pBB = lerp(pB1, pB1+tB,  1-(f*f));
+    // float3 pAA = lerp(pA1, pA1+tA, 1-(1-f)* (1-f));
+    // float3 pBB = lerp(pB1, pB1+tB,  1-(f*f));
 
-    ResultPoints[i.x].position = lerp(
-                                    pAA, 
-                                    pBB,
-                                    f);
+    // ResultPoints[i.x].position = lerp(
+    //                                 pAA, 
+    //                                 pBB,
+    //                                 f);
+
+    float3 v0 = pA1;
+    float3 v1 = pA1 + tA;
+    float3 v2 = pB1 + tB;
+    float3 v3 = pB1;
     
+    // float3 A = (v3-v2)-(v0-v1);
+    // float3 B = (v0-v1)-A;
+    // float3 C = v2-v0;
+    // float3 D = v1;    
+
+    // float3 pF = D + f * (C + f * (B + f * A));
+
+
+    float t =f;
+    float t2 = t * t;
+    float t3 = t2 * t;
     
+    float3 pF= (2 * t3 - 3 * t2 + 1) * v0 +
+           (t3 - 2 * t2 + t) * tA +
+           (-2 * t3 + 3 * t2) * v3 +
+           (t3 - t2) * -tB;
+    ResultPoints[i.x].position = pF;
+
+
     ResultPoints[i.x].rotation = float4(1,0,0,1);
 
     //ResultPoints[i.x].position = float3(1,0,0);
