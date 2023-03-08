bc6c35cba2c58ad12167ae1b9f9a9669df94bd35
Thomas Mann <thomas.mann@framefield.com>
2023-03-08 22:05:36 +0100

    Add [RgbTV] glitch effect

--- Operators/Types/user/still/worksforeverybody/fx/RgbTV.cs
+++ Operators/Types/user/still/worksforeverybody/fx/RgbTV.cs
@@ -13,11 +13,29 @@ namespace T3.Operators.Types.Id_5972a57b_73cd_49b2_8b24_96636a4c294b
         [Input(Guid = "2dbfdd5d-8b4b-447c-bd19-326d46657ea1")]
         public readonly InputSlot<SharpDX.Direct3D11.Texture2D> Image = new InputSlot<SharpDX.Direct3D11.Texture2D>();
 
+        [Input(Guid = "402ec136-4727-41b7-8bfb-344d1ed83a48")]
+        public readonly InputSlot<float> Visibility = new InputSlot<float>();
+
         [Input(Guid = "38529a44-4622-4c87-886e-72f4400ec468")]
         public readonly InputSlot<float> PatternAmount = new InputSlot<float>();
 
-        [Input(Guid = "7ba6f801-3d73-4f67-a6bc-40b409f9d116")]
-        public readonly InputSlot<float> PatternAspect = new InputSlot<float>();
+        [Input(Guid = "3ef09b89-b3e8-432b-bed3-f7c9033acefa")]
+        public readonly InputSlot<float> ImageBrightess = new InputSlot<float>();
+
+        [Input(Guid = "50cab12f-535c-4651-98d2-5c8b3c18cc81")]
+        public readonly InputSlot<float> BlackLevel = new InputSlot<float>();
+
+        [Input(Guid = "88258a10-d5e4-4a4e-80b3-eacaebe75abf")]
+        public readonly InputSlot<float> ImageContrast = new InputSlot<float>();
+
+        [Input(Guid = "fc4e5ca3-edb3-4ebc-8210-b339d7914741")]
+        public readonly InputSlot<float> BlurImage = new InputSlot<float>();
+
+        [Input(Guid = "e29b1dad-d3ef-405f-99c0-4552caedaf7c")]
+        public readonly InputSlot<float> GlowIntensity = new InputSlot<float>();
+
+        [Input(Guid = "51f05bd9-6f7e-4774-a05d-acaa1b8cba35")]
+        public readonly InputSlot<float> GlowBlur = new InputSlot<float>();
 
         [Input(Guid = "866493b5-50ec-4587-861f-87d5d3a698e5")]
         public readonly InputSlot<float> PatternSize = new InputSlot<float>();
@@ -28,62 +46,44 @@ namespace T3.Operators.Types.Id_5972a57b_73cd_49b2_8b24_96636a4c294b
         [Input(Guid = "f58da234-b925-40ca-b449-1ac882bd2e96")]
         public readonly InputSlot<float> Gaps = new InputSlot<float>();
 
-        [Input(Guid = "9b3a821b-3bad-4411-99ba-669436e240fd")]
-        public readonly InputSlot<System.Numerics.Vector2> PatternOffset = new InputSlot<System.Numerics.Vector2>();
-
-        [Input(Guid = "e3e3f393-0c43-4c71-b134-adc094ca2965")]
-        public readonly InputSlot<float> Buldge = new InputSlot<float>();
-
         [Input(Guid = "5f536f1f-49da-435d-a02b-002a3673c240")]
         public readonly InputSlot<System.Numerics.Vector2> PatternBlur = new InputSlot<System.Numerics.Vector2>();
-
-        [Input(Guid = "b03dd009-99b7-4846-8d81-9c67f57fbd91")]
-        public readonly InputSlot<float> GlitchTime = new InputSlot<float>();
 
         [Input(Guid = "f76c6202-34dc-4c10-adab-c10cb7665fed")]
         public readonly InputSlot<float> GlitchAmount = new InputSlot<float>();
 
-        [Input(Guid = "9964e5dc-071a-4dce-bca4-994fbda19906")]
-        public readonly InputSlot<float> GlitchVignette = new InputSlot<float>();
-
-        [Input(Guid = "13bceb18-aa27-480a-bf34-c93c758dabca")]
-        public readonly InputSlot<float> GlitchOffset = new InputSlot<float>();
-
         [Input(Guid = "5cb6a27b-db0d-4d85-83aa-87316ba6f5a0")]
         public readonly InputSlot<float> GlitchFlicker = new InputSlot<float>();
+
+        [Input(Guid = "895e4da6-fd6e-436f-b414-14c42cbb3259")]
+        public readonly InputSlot<float> GlitchTimeOverride = new InputSlot<float>();
+
+        [Input(Guid = "026b6679-10e9-41e5-84b0-2c4b3f86b155")]
+        public readonly InputSlot<float> GlitchDistort = new InputSlot<float>();
+
+        [Input(Guid = "c71c9adc-eae6-4acb-b1f0-4eea873a5bce")]
+        public readonly InputSlot<float> ShadeDistortion = new InputSlot<float>();
+
+        [Input(Guid = "749a9cb1-9dd7-43c7-af91-d5624e26423c")]
+        public readonly InputSlot<float> NoiseForDistortion = new InputSlot<float>();
 
         [Input(Guid = "a13b757c-62ed-478b-b0fe-70cceb43586e")]
         public readonly InputSlot<float> Noise = new InputSlot<float>();
 
+        [Input(Guid = "795f9829-7dbf-4e07-82e5-d03eead8c527")]
+        public readonly InputSlot<float> NoiseSpeed = new InputSlot<float>();
+
+        [Input(Guid = "69b4b2d0-cede-4479-a976-855b69107b8d")]
+        public readonly InputSlot<float> NoiseExponent = new InputSlot<float>();
+
         [Input(Guid = "06781f30-4be8-4eb6-95fd-7e8b16081a64")]
         public readonly InputSlot<float> NoiseColorize = new InputSlot<float>();
 
-        [Input(Guid = "86233c09-0ca4-4393-9480-b8275a5bd9e8")]
-        public readonly InputSlot<float> BlurBackdrop = new InputSlot<float>();
+        [Input(Guid = "e3e3f393-0c43-4c71-b134-adc094ca2965")]
+        public readonly InputSlot<float> Buldge = new InputSlot<float>();
 
-        [Input(Guid = "be2bf88f-870e-4dd9-a0a7-e10e38b9a8d1")]
-        public readonly InputSlot<float> OverdrawSteps = new InputSlot<float>();
-
-        [Input(Guid = "c96bcad4-c73e-4731-9aa0-16dfaa91ba57")]
-        public readonly InputSlot<float> OverdrawScale = new InputSlot<float>();
-
-        [Input(Guid = "f8358e13-a769-45e9-b345-f172fabd9c74")]
-        public readonly InputSlot<System.Numerics.Vector2> OverdrawOffset = new InputSlot<System.Numerics.Vector2>();
-
-        [Input(Guid = "fc4e5ca3-edb3-4ebc-8210-b339d7914741")]
-        public readonly InputSlot<float> BlurImage = new InputSlot<float>();
-
-        [Input(Guid = "1d2328ce-d0c2-4b0b-9318-4a7c5dcfd411")]
-        public readonly InputSlot<float> P1 = new InputSlot<float>();
-
-        [Input(Guid = "95c2f7b3-52e9-4d22-bf1f-c2ad2d0fb8da")]
-        public readonly InputSlot<float> P2 = new InputSlot<float>();
-
-        [Input(Guid = "6d4c28fd-2bea-48a6-a87a-df0b46f76d7c")]
-        public readonly InputSlot<float> P3 = new InputSlot<float>();
-
-        [Input(Guid = "ac6c18b6-8896-43e7-b063-00cd67fb3545")]
-        public readonly InputSlot<float> P4 = new InputSlot<float>();
+        [Input(Guid = "a24de125-eb69-4c44-afa6-69dfdbf16087")]
+        public readonly InputSlot<float> Vignette = new InputSlot<float>();
 
         [Input(Guid = "e3f06019-97a4-44b3-b9b4-36d07cbd53ac")]
         public readonly InputSlot<SharpDX.Size2> Resolution = new InputSlot<SharpDX.Size2>();
--- Resources/lib/img/fx/img-fx-RgbTV.hlsl
+++ Resources/lib/img/fx/img-fx-RgbTV.hlsl
@@ -1,65 +1,44 @@
-/*
-
-PatternAmount
-PatternStretch
-PatternSize
-PatternBlurX,Y
-PatternBlurAmount
-
-Distort
-
-GlitchTime
-GlitchAmount
-GlitchVignette
-Offset
-Flicker
-Noise
-NoiseColorize
-
-BlurBackdrop
-OverdrawSteps
-Scale
-OffsetX
-OffsetY
-
-*/
-
-Texture2D<float4> inputTexture : register(t0);
-sampler texSampler : register(s0);
+#include "lib/shared/hash-functions.hlsl"
 
 cbuffer ParamConstants : register(b0)
 {
+    float Visibility;
+
     float PatternAmount;
-    float PatternAspect;
+    float ImageBrightness;
+    float BlackLevel;
+    float Contrast;
+
+    float BlurImage;
+    float GlowIntensity;
+    float GlowBlur;
     float PatternSize;
+
     float ShiftColums;
     float Gaps;
-    float2 PatternOffset;
-
     float PatternBlurX;
     float PatternBlurY;
 
-    float Buldge;
-    float GlitchTime;
     float GlitchAmount;
-    float GlitchVignette;
-    float GlitchOffset;
-    float GlitchFlicker;
+    float GlitchTime;
+    float GlitchDistort;
+    float ShadeDistortion;
+
+    float NoiseForDistortion;
     float Noise;
+    float NoiseSpeed;
+    float NoiseExponent;
+
     float NoiseColorize;
-    float BlurBackdrop;
-    float OverdrawSteps;
-    float Scale;
-    float OffsetX;
-    float OffsetY;
-
-    float BlurImage;
-
-    float P1;
-    float P2;
-    float P3;
-    float P4;
+    float Buldge;
+    float Vignette;
 }
+
+Texture2D<float4> inputTexture : register(t0);
+Texture2D<float4> noiseTexture : register(t1);
+sampler clampingSampler : register(s0);
+sampler wrappingSampler : register(s1);
+// sampler clampingSampler : register(s0);
 
 cbuffer Resolution : register(b1)
 {
@@ -83,10 +62,39 @@ static const float3 RgbColors[] = {
 
 float GetColor(int rgbIndex, float x, float py, float sourceImageChannel)
 {
-    x += (rgbIndex - 1) / 3.0;
-    float xx = pow((0.5 - abs(x - 0.5) + P3 + sourceImageChannel * P1), P2);
+    float offset = (rgbIndex - 1) / 3.0;
+    x += offset;
+    x = mod(x, 1.03);
+    // return x;
+    float center = (0.5 - abs(x - 0.5)) * 2;
+
+    // return center;
+    //  float xx = center ; // pow((center + Contrast + sourceImageChannel * ImageBrightness), BlackLevel);
+    float xx = center + pow(sourceImageChannel * ImageBrightness, Contrast) + BlackLevel;
     float s = smoothstep(1 - PatternBlurX, 1 + PatternBlurY, xx);
     return s;
+}
+
+float4 GetNoiseFromRandom(float2 uv)
+{
+    // Animation
+    float pxHash = hash12(uv * 431 + 111);
+    float t = GlitchTime * NoiseSpeed + pxHash;
+
+    // Color Noise
+    float4 hash1 = hash42((uv * 431 + (int)t));
+    float4 hash2 = hash42((uv * 431 + (int)t + 1));
+    float4 hash = lerp(hash1, hash2, t % 1);
+
+    float4 grayScale = (hash.r + hash.g + hash.b) / 3;
+    float4 noise = (lerp(grayScale, hash, NoiseColorize) - 0.5) * 2;
+
+    noise = noise < 0
+                ? -pow(-noise, NoiseExponent)
+                : pow(noise, NoiseExponent);
+
+    // noise += Brightness;
+    return noise;
 }
 
 float4 psMain(vsOutput psInput) : SV_TARGET
@@ -98,23 +106,61 @@ float4 psMain(vsOutput psInput) : SV_TARGET
     p -= 0.5;
 
     // Bulge distort
-    p -= p * Buldge * (0.5 - dot(p, p));
+    p -= p * Buldge * Visibility * (0.5 - dot(p, p));
 
     float2 uv2 = p + 0.5;
-    float4 imgCol1 = inputTexture.SampleLevel(texSampler, uv2, 0);
-    float4 imgColBlurred =
-        imgCol1 * 0.2 +
-        inputTexture.SampleLevel(texSampler, uv2, 1) * 0.2 +
-        inputTexture.SampleLevel(texSampler, uv2, 2) * 0.2 +
-        inputTexture.SampleLevel(texSampler, uv2, 3) * 0.2 +
-        inputTexture.SampleLevel(texSampler, uv2, 4) * 0.2 +
-        inputTexture.SampleLevel(texSampler, uv2, 5) * 1;
 
-    float4 imgCol = lerp(imgCol1, imgColBlurred, BlurImage);
+    // Distortion
+    float2 noiseOffset = float2(.1, 0.1) * GlitchTime;
+    float2 noiseUv = p * float2(0.001, 1) + noiseOffset;
+    float4 noiseColor = abs(noiseTexture.SampleLevel(wrappingSampler, noiseUv, 0) - 0.5) * GlitchAmount * Visibility;
+
+    // Amplify noise on upper edge
+    noiseColor *= (0.4 + pow(1 - uv2.y, 6) * 3);
+    noiseColor += 0.03;
+
+    float2 glichOffset = pow(noiseColor.r, 4) * GlitchDistort * float2(1, 0.1);
+    uv2 -= glichOffset;
+
+    int sourceWidth, sourceHeight, mipLevelCount;
+    inputTexture.GetDimensions(0, sourceWidth, sourceHeight, mipLevelCount);
+    mipLevelCount = 7;
+
+    float4 blurredCol = 0;
+    float blurredSum = 0;
+
+    float4 glowCol = 0;
+    float glowSum = 0;
+    for (int i = 0; i < mipLevelCount + 1; i++)
+    {
+        float4 mipColor = inputTexture.SampleLevel(clampingSampler, uv2, i);
+
+        // Accumulate blur color
+        float f = i / ((float)mipLevelCount);
+        float level = saturate((pow(f + 1 - saturate(BlurImage), 20) + 0.001));
+        blurredSum += level;
+        blurredCol += mipColor * level;
+
+        // Accumulate glow color
+
+        level = saturate((pow(f + 1 - saturate(GlowBlur), 20) + 0.001));
+        glowSum += level;
+        glowCol += mipColor * level;
+    }
+    blurredCol /= blurredSum;
+    float4 imgCol1 = inputTexture.SampleLevel(clampingSampler, uv2, 0);
+    blurredCol = lerp(imgCol1, blurredCol, saturate(BlurImage + 1) * Visibility);
+
+    glowCol /= glowSum;
+    float4 imgCol = blurredCol + clamp((glowCol)*GlowIntensity, 0, 10) * Visibility;
+
+    imgCol.rgb *= clamp(1 - pow(noiseColor.r, 1.4) * ShadeDistortion * GlitchDistort * GlitchAmount, 0, 10);
+
     imgCol.a = saturate(imgCol.a);
 
-    float2 divisions = float2(aspectRatio, 1) * 4 / (PatternSize * float2(PatternAspect, 1));
-    float2 pCentered = (p + PatternOffset / divisions * float2(-1, 1));
+    float2 divisions = float2(aspectRatio, 1) * 4 / PatternSize;
+    // float2 pCentered = (p + PatternOffset / divisions * float2(-1, 1));
+    float2 pCentered = p;
     float2 pScaled = pCentered * divisions;
 
     float pInCellX = mod(pScaled.x, 1);
@@ -124,24 +170,28 @@ float4 psMain(vsOutput psInput) : SV_TARGET
 
     float2 pInCell = float2(pInCellX, pInCellY);
 
+    float4 noise = GetNoiseFromRandom(float2(cellIdX, cellIdY));
+    float4 noiseDelta = abs(noise) * ((pow(noiseColor.r, 2)) * GlitchDistort * NoiseForDistortion + 1) * Noise * GlitchAmount;
+
     int rgbStripeIndex = int(pInCellX * 3);
 
     float xInStripe = (pInCellX - 0.5) / (1 - Gaps * 2) + 0.5;
 
+    float3 noisyImage = imgCol.rgb + noiseDelta.rgb;
     float3 cc = float3(
-        GetColor(0, xInStripe, pInCellY, imgCol.r),
-        GetColor(1, xInStripe, pInCellY, imgCol.g),
-        GetColor(2, xInStripe, pInCellY, imgCol.b));
+        GetColor(0, xInStripe, pInCellY, noisyImage.r),
+        GetColor(1, xInStripe, pInCellY, noisyImage.g),
+        GetColor(2, xInStripe, pInCellY, noisyImage.b));
 
     float padding = Gaps;
     float yBlur = smoothstep(padding - PatternBlurY, padding + PatternBlurY, abs(pInCellY - 0.5));
 
     float4 pattern = float4(cc * yBlur, 1);
 
-    float4 r = lerp(imgCol, pattern, PatternAmount);
+    float4 r = lerp(imgCol, pattern, PatternAmount * Visibility);
 
     // Vignette
-    r.rgb *= -GlitchVignette * (1 - (0.5 - dot(p, p)) - 0.5) + 1.5;
+    r.rgb *= lerp(1.5 - Vignette * (1 - (0.5 - dot(p, p)) - 0.5), 1, Visibility);
 
     return float4(r);
 }
