f3c52ec2f355b1ee9ecd1f9e401021e46eed5b2e
Thomas Mann <thomas.mann@framefield.com>
2023-03-11 23:37:03 +0100

    Inverted [RadialGradient] offset direction

--- Resources/lib/img/generate/img-generate-radial-gradient-hlsl.hlsl
+++ Resources/lib/img/generate/img-generate-radial-gradient-hlsl.hlsl
@@ -51,27 +51,19 @@ float4 psMain(vsOutput psInput) : SV_TARGET
 
     if (PolarOrientation < 0.5)
     {
-        c = distance(p, Center) * 2 + Offset;
+        c = distance(p, Center) * 2 - Offset;
     }
     else
     {
         p += Center;
         float Radius = 1;
         float l = 2 * length(p) / Radius;
-        // l = pow(l, RadialBias);
 
         float2 polar = float2(atan2(p.x, p.y) / 3.141578 / 2 + 0.5, l) + Center - Center.x;
-        // polar.y += Offset + Center.x * 2;
-        //  polar.x += polar.y * Twist;
-        //  polar *= Stretch;
-        //  p = polar;
         c = polar.x + Offset;
-        // return float4(p, 0, 1);
     }
 
     float4 orgColor = ImageA.Sample(texSampler, psInput.texCoord);
-
-    // float c=  dot(p-Center, angle);
 
     c = PingPong > 0.5
             ? (Repeat < 0.5 ? (abs(c) / Width)
@@ -82,21 +74,13 @@ float4 psMain(vsOutput psInput) : SV_TARGET
             ? fmod(c, 1)
             : saturate(c);
 
-    // if(Smooth > 0.5) {
-    //     c= smoothstep(0,1,c);
-    // }
-
     float dBiased = Bias >= 0
                         ? pow(c, Bias + 1)
                         : 1 - pow(clamp(1 - c, 0, 10), -Bias + 1);
 
     dBiased = clamp(dBiased, 0.001, 0.999);
-    // return float4(dBiased,0,0,1);
-    // float4 cOut= lerp(Fill, Background, dBiased);
-
     float4 gradient = Gradient.Sample(texSampler, float2(dBiased, 0));
     float a = orgColor.a + gradient.a - orgColor.a * gradient.a;
     float3 rgb = (1.0 - gradient.a) * orgColor.rgb + gradient.a * gradient.rgb;
-
     return float4(rgb, a);
 }
