ec2079a70c59cee9bb500d1d3655cd567c0d1fbe
Thomas Mann <thomas.mann@framefield.com>
2023-03-11 14:02:13 +0100

    Add [RadialGradients] polar direction (e.g. in a circular shape)

--- Resources/lib/img/generate/img-generate-radial-gradient-hlsl.hlsl
+++ Resources/lib/img/generate/img-generate-radial-gradient-hlsl.hlsl
@@ -1,12 +1,11 @@
 cbuffer ParamConstants : register(b0)
 {
-    float4 Fill;
-    float4 Background;
     float2 Center;
     float Width;
-    float Rotation;
+    float Offset;
     float PingPong;
     float Repeat;
+    float PolarOrientation;
     float Bias;
 }
 
@@ -24,7 +23,6 @@ cbuffer Resolution : register(b2)
     float TargetHeight;
 }
 
-
 struct vsOutput
 {
     float4 position : SV_POSITION;
@@ -35,55 +33,70 @@ Texture2D<float4> ImageA : register(t0);
 Texture2D<float4> Gradient : register(t1);
 sampler texSampler : register(s0);
 
-float fmod(float x, float y) {
+float fmod(float x, float y)
+{
     return (x - y * floor(x / y));
-} 
+}
 
 float4 psMain(vsOutput psInput) : SV_TARGET
-{    
+{
     float2 uv = psInput.texCoord;
-    //float4 orgColor = inputTexture.SampleLevel(texSampler, uv, 0.0);
 
-    float aspectRation = TargetWidth/TargetHeight;
+    float aspectRation = TargetWidth / TargetHeight;
     float2 p = uv;
-    p-= 0.5;
-    p.x *=aspectRation;
+    p -= 0.5;
+    p.x *= aspectRation;
 
-    //float2 p = psInput.texCoord;
+    float c = 0;
 
-    // float radians = Rotation / 180 *3.141578;
-    // float2 angle =  float2(sin(radians),cos(radians));
+    if (PolarOrientation < 0.5)
+    {
+        c = distance(p, Center) * 2 + Offset;
+    }
+    else
+    {
+        p += Center;
+        float Radius = 1;
+        float l = 2 * length(p) / Radius;
+        // l = pow(l, RadialBias);
+
+        float2 polar = float2(atan2(p.x, p.y) / 3.141578 / 2 + 0.5, l) + Center - Center.x;
+        // polar.y += Offset + Center.x * 2;
+        //  polar.x += polar.y * Twist;
+        //  polar *= Stretch;
+        //  p = polar;
+        c = polar.x + Offset;
+        // return float4(p, 0, 1);
+    }
 
     float4 orgColor = ImageA.Sample(texSampler, psInput.texCoord);
 
     // float c=  dot(p-Center, angle);
-    float c = distance(p, Center) * 2;
 
-    c = PingPong > 0.5 
-        ? (Repeat < 0.5 ? (abs(c) / Width)
-                        : 1-abs( fmod(c,Width *2) -Width)  / Width)
-        : c / Width;
+    c = PingPong > 0.5
+            ? (Repeat < 0.5 ? (abs(c) / Width)
+                            : 1 - abs(fmod(c, Width * 2) - Width) / Width)
+            : c / Width;
 
-    c = Repeat > 0.5 
-        ? fmod(c,1)
-        : saturate(c);
+    c = Repeat > 0.5
+            ? fmod(c, 1)
+            : saturate(c);
 
     // if(Smooth > 0.5) {
     //     c= smoothstep(0,1,c);
     // }
 
-    float dBiased = Bias>= 0
-        ? pow( c, Bias+1)
-        : 1-pow( clamp(1-c,0,10), -Bias+1);
-    
-    dBiased= clamp(dBiased,0.001, 0.999);
-    //return float4(dBiased,0,0,1);
-    //float4 cOut= lerp(Fill, Background, dBiased);
+    float dBiased = Bias >= 0
+                        ? pow(c, Bias + 1)
+                        : 1 - pow(clamp(1 - c, 0, 10), -Bias + 1);
 
+    dBiased = clamp(dBiased, 0.001, 0.999);
+    // return float4(dBiased,0,0,1);
+    // float4 cOut= lerp(Fill, Background, dBiased);
 
     float4 gradient = Gradient.Sample(texSampler, float2(dBiased, 0));
-    float a = orgColor.a + gradient.a - orgColor.a*gradient.a;
-    float3 rgb = (1.0 - gradient.a)*orgColor.rgb + gradient.a*gradient.rgb;   
+    float a = orgColor.a + gradient.a - orgColor.a * gradient.a;
+    float3 rgb = (1.0 - gradient.a) * orgColor.rgb + gradient.a * gradient.rgb;
 
-    return float4(rgb,a);
+    return float4(rgb, a);
 }
