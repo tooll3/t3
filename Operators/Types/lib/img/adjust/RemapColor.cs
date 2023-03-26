9cbc991676c0e4080abc27e46ea399ab90b01375
Thomas Mann <thomas.mann@framefield.com>
2023-03-26 13:52:37 +0200

    Improve blending in RemapColors

--- Operators/Types/lib/img/adjust/RemapColor.cs
+++ Operators/Types/lib/img/adjust/RemapColor.cs
@@ -16,9 +16,6 @@ namespace T3.Operators.Types.Id_da93f7d1_ef91_4b4a_9708_2d9b1baa4c14
         [Input(Guid = "cb52ff49-17de-4e36-b918-5de6973a234a")]
         public readonly InputSlot<SharpDX.Size2> Resolution = new InputSlot<SharpDX.Size2>();
 
-        [Input(Guid = "f10896b4-b8fa-4e16-a7ec-4a641ddda926")]
-        public readonly InputSlot<float> Bias = new InputSlot<float>();
-
         [Input(Guid = "c45d487b-3221-44c7-bf9e-b982a65280f6")]
         public readonly InputSlot<T3.Core.DataTypes.Gradient> Gradient = new InputSlot<T3.Core.DataTypes.Gradient>();
 
