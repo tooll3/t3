3395e260a6669e45f1079b395407a010649085b2
Thomas Mann <thomas.mann@framefield.com>
2023-04-06 00:58:16 +0200

    WFE: Working on saving parameter selections

--- Operators/Types/user/still/worksforeverybody/fx/SaveParameterSet.cs
+++ Operators/Types/user/still/worksforeverybody/fx/SaveParameterSet.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using T3.Core.Logging;
 using T3.Core.Operator;
 using T3.Core.Operator.Attributes;
@@ -8,6 +10,9 @@ using System.Net.Http.Headers;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using T3.Core.DataTypes;
 using T3.Core.Utils;
 
 namespace T3.Operators.Types.Id_3246cf5a_3c9b_4765_89d1_68852a3dd7a1
@@ -17,55 +22,128 @@ namespace T3.Operators.Types.Id_3246cf5a_3c9b_4765_89d1_68852a3dd7a1
         [Output(Guid = "a75a50ac-9361-4cac-8208-3c79f329bad2")]
         public readonly Slot<string> Result = new();
 
+        
+        [Output(Guid = "90F4D983-685E-499B-B121-0D7F34669490")]
+        public readonly Slot<StructuredList> Points = new();
+        
         public SaveParameterSet()
         {
             Result.UpdateAction = Update;
+            Points.UpdateAction = Update;
+            UpdatePointLists();
         }
 
         private void Update(EvaluationContext context)
         {
-            var wasTriggered = MathUtils.WasTriggered(Trigger.GetValue(context), ref _trigger);
+            if (!_initialized)
+            {
+                _initialized = true;
+                FetchParameters();
+            }
 
-            var position = Position.GetValue(context);
-            var newSet = new ParameterSet
-                             {
-                                 X = position.X,
-                                 Y = position.Y,
-                                 SceneIndex = SceneIndex.GetValue(context),
-                                 UserHash = 0
-                             };
-            if(wasTriggered)
-                SetRequestAsync(newSet);
+            try
+            {
+                var saveNewTriggered = MathUtils.WasTriggered(TriggerSaveParameters.GetValue(context), ref _triggerSaveParameters);
+
+                var sceneIndex = SceneIndex.GetValue(context).Clamp(0, MaxSceneCount - 1);
+                if (saveNewTriggered)
+                {
+                    Log.Debug("Save triggered");
+                    var position = Position.GetValue(context);
+                    var newSet = new ParameterSet
+                                     {
+                                         X = position.X,
+                                         Y = position.Y,
+                                         SceneIndex = sceneIndex,
+                                         UserHash = 0
+                                     };
+                    SendDataAsync(newSet);
+                    _parameterSets.Add(newSet);
+
+                    UpdatePointLists();
+                }
+
+                var points = _parameterPoints[sceneIndex];
+                if (points.NumElements == 0)
+                    points = _emptyList;
+                
+                Points.Value = points;
+            }
+            catch (Exception e)
+            {
+                Log.Warning("Exception " + e.Message);
+            }
+            
+            // Avoid double evaluation
+            Points.DirtyFlag.Clear();
+            Result.DirtyFlag.Clear();
         }
 
-        private async void SetRequestAsync(ParameterSet parameterSet)
+        
+        private void UpdatePointLists()
         {
+            var paramsForScenes = new List<ParameterSet>[MaxSceneCount];
+            for (var i = 0; i < MaxSceneCount - 1; i++)
+            {
+                paramsForScenes[i] = new List<ParameterSet>();
+            }
+            
+            // Count parameters for all scenes
+            foreach (var p in _parameterSets)
+            {
+                if (p.SceneIndex < 0 && p.SceneIndex >= MaxSceneCount)
+                {
+                    Log.Warning($"Skipping parameter set for invalid scene index {p.SceneIndex}");
+                    continue;
+                }
+
+                paramsForScenes[p.SceneIndex].Add(p);
+            }
+            
+            // Initialize point buffers
+            for (var sceneIndex = 0; sceneIndex < MaxSceneCount -1; sceneIndex++)
+            {
+                var pointList = new StructuredList<Point>(paramsForScenes[sceneIndex].Count);
+                
+                var parameters = paramsForScenes[sceneIndex];
+                for (var paramIndex = 0; paramIndex < parameters.Count; paramIndex++)
+                {
+                    var parameter = parameters[paramIndex];
+                    pointList.TypedElements[paramIndex] = new Point()
+                                                {
+                                                    Position = new Vector3(parameter.X, -parameter.Y, 0),
+                                                    W = 1,
+                                                };
+                }
+
+                _parameterPoints[sceneIndex] = pointList;
+            }
+        }
+        
+        
+        private async void SendDataAsync(ParameterSet parameterSet)
+        {
+            Log.Debug("Saving parameterSet...");
             try
             {
                 var client = new HttpClient(new HttpClientHandler() { UseDefaultCredentials = true });
-                
-                
                 var request = new HttpRequestMessage
                                   {
                                       RequestUri = new Uri("https://oyjobypyozkufrbrmqnt.supabase.co/rest/v1/scores"),
                                       Method = HttpMethod.Post,
                                   };
                 request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                request.Headers.Add("apikey",
-                                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95am9ieXB5b3prdWZyYnJtcW50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODAwMDA4MTUsImV4cCI6MTk5NTU3NjgxNX0.9Km2SmPnOQre_obeKr_DSr2l33PECtMWIF1VuxE1zck");
-                request.Headers.Add("Authorization",
-                                    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95am9ieXB5b3prdWZyYnJtcW50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODAwMDA4MTUsImV4cCI6MTk5NTU3NjgxNX0.9Km2SmPnOQre_obeKr_DSr2l33PECtMWIF1VuxE1zck");
+                
+                request.Headers.Add("apikey", Token);
+                request.Headers.Add("Authorization", "Bearer " + Token);
                 request.Headers.Add("Prefer","return=minimal");
-                //var content = new StringContent("{'data': {'foo':'bar2'}}", Encoding.UTF8, "application/json");
 
                 var jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(parameterSet);
-                var s = "{\"data\":" + jsonString + "}";
-                Log.Debug("data:" + s);
-                //var s2 = "{\"data\":{\"foo\":\"bar\"}}";
-                var content = new StringContent(s, Encoding.UTF8, "application/json");
+                var content = new StringContent("{\"data\":" + jsonString + "}", Encoding.UTF8, "application/json");
                 request.Content = content;
                 
                 var httpTask = await client.SendAsync(request);
+                
                 Result.Value =await httpTask.Content.ReadAsStringAsync();;
                 Result.DirtyFlag.Invalidate();
             }
@@ -75,21 +153,85 @@ namespace T3.Operators.Types.Id_3246cf5a_3c9b_4765_89d1_68852a3dd7a1
             }
         }
 
-        private bool _trigger;
 
+        
+        
+        private async void FetchParameters()
+        {
+            try
+            {
+                var client = new HttpClient(new HttpClientHandler() { UseDefaultCredentials = true });
+                var request = new HttpRequestMessage
+                                  {
+                                      RequestUri = new Uri("https://oyjobypyozkufrbrmqnt.supabase.co/rest/v1/scores?select=data"),
+                                      Method = HttpMethod.Get,
+                                  };
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                request.Headers.Add("apikey", Token);
+                request.Headers.Add("Authorization", "Bearer " + Token);
+                var httpResponseTask = await client.SendAsync(request);
+                var jsonString = await httpResponseTask.Content.ReadAsStringAsync();
+                
+                ParseData(jsonString);
+                Result.Value = jsonString;
+                Result.DirtyFlag.Invalidate();
+            }
+            catch (Exception e)
+            {
+                Log.Warning("Download failed: " + e.Message);
+            }
+        }        
+        
+        private void ParseData(string jsonString)
+        {
+            _parameterSets.Clear();
+            using var streamReader = new StringReader(jsonString);
+            using var jsonReader = new JsonTextReader(streamReader);
+
+            try 
+            {
+                var o = JToken.ReadFrom(jsonReader);
+                foreach (var childJson in (JArray)o)
+                {
+                    var dataToken = childJson["data"];
+                    var s = dataToken?.ToObject<ParameterSet>();
+                    if (s == null)
+                        continue;
+
+                    Log.Debug("Found parameters! " + s.X);
+                    _parameterSets.Add(s);
+                }
+                
+                UpdatePointLists();
+            }
+            catch(Exception e)
+            {
+                Log.Warning("parsing data failed:" + e.Message);
+            }
+        }
+
+        private const int MaxSceneCount = 30;
+        
+        private const string Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95am9ieXB5b3prdWZyYnJtcW50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODAwMDA4MTUsImV4cCI6MTk5NTU3NjgxNX0.9Km2SmPnOQre_obeKr_DSr2l33PECtMWIF1VuxE1zck";
+        private bool _triggerSaveParameters;
+        private readonly StructuredList<Point> _emptyList = new StructuredList<Point>(1);
+        private readonly StructuredList<Point>[] _parameterPoints = new StructuredList<Point>[MaxSceneCount];
+        private readonly List<ParameterSet> _parameterSets = new();
+
+        private bool _initialized;
+        
+        [Serializable]
         private class ParameterSet
         {
             public float X;
             public float Y;
             public int SceneIndex;
-            public int UserHash;
+            public int UserHash;    // Todo
         }
-        
+
         
         [Input(Guid = "D6A017AF-DF4E-4532-8A4A-208DF224D60A")]
-        public readonly InputSlot<bool> Trigger = new();
-
-
+        public readonly InputSlot<bool> TriggerSaveParameters = new();
         
         [Input(Guid = "BDF7D9AB-0E73-4323-816D-AF5CA0A60989")]
         public readonly InputSlot<Vector2> Position = new();
