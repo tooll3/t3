b77b4b6460bfdb324a1edb281482b6067e21c5ed
Thomas Mann <thomas.mann@framefield.com>
2023-05-29 18:39:43 +0200

    Add output mapping to [MouseInput] to transform normalized screenspace to position coordinates

--- Operators/Types/lib/io/input/MouseInput.cs
+++ Operators/Types/lib/io/input/MouseInput.cs
@@ -1,6 +1,7 @@
 using T3.Core.Operator;
 using T3.Core.Operator.Attributes;
 using T3.Core.Operator.Slots;
+using T3.Core.Utils;
 using Vector2 = System.Numerics.Vector2;
 
 namespace T3.Operators.Types.Id_eff2ffff_dc39_4b90_9b1c_3c0a9a0108c6
@@ -21,11 +22,26 @@ namespace T3.Operators.Types.Id_eff2ffff_dc39_4b90_9b1c_3c0a9a0108c6
 
         private void Update(EvaluationContext context)
         {
-            Position.Value = Core.IO.MouseInput.LastPosition;
+            var mode = OutputMode.GetEnumValue<OutputModes>(context);
+            var aspectRatio = (float)context.RequestedResolution.Width / context.RequestedResolution.Height;
+                                         Position.Value = mode == OutputModes.Normalized 
+                                                              ? Core.IO.MouseInput.LastPosition
+                                                              : (Core.IO.MouseInput.LastPosition - new Vector2(0.5f,0.5f)) * new Vector2(aspectRatio,-1) * 2;
+                                         
             IsLeftButtonDown.Value = Core.IO.MouseInput.IsLeftButtonDown;
         }
-        
+
         [Input(Guid = "49775CC2-35B7-4C9F-A502-59FE8FBBE2A7")]
         public readonly InputSlot<bool> DoUpdate = new InputSlot<bool>();
+        
+        [Input(Guid = "1327525C-716C-43E4-A5D1-58CF35440462", MappedType = typeof(OutputModes))]
+        public readonly InputSlot<int> OutputMode = new ();
+        
+        private enum OutputModes
+        {
+            Normalized,
+            SignedPosition,
+        }
+        
     }
 }
