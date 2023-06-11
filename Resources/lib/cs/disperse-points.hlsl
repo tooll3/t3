04943b0585df03e7137fd2f2077b4929bfe9267b
Thomas Mann <thomas.mann@framefield.com>
2023-06-11 19:49:00 +0200

    Improve DispersePoints

--- Resources/lib/cs/disperse-points.hlsl
+++ Resources/lib/cs/disperse-points.hlsl
@@ -24,17 +24,19 @@ void DispersePoints(uint3 DTid : SV_DispatchThreadID, uint GI: SV_GroupIndex)
         return; // out of bounds
 
     float3 position = points[DTid.x].position;
-    float3 jitter = (hash33u( uint3(DTid.x, DTid.x + 134775813U, DTid.x + 1664525U) + position * 1000 + Time % 123 ) -0.5f)  * CellSize * 2;
-    position+= jitter;
+    //float3 jitter = (hash33u( uint3(DTid.x, DTid.x + 134775813U, DTid.x + 1664525U) + Time % 123.12 ) -0.5f)  * CellSize * 1;
+    float3 jitter = (hash33u( uint3(DTid.x, DTid.x + 134775813U, DTid.x + 1664525U + (int)(Time * 123.12)) ) -0.5f)  * CellSize * 2;
+    //float3 jitter = (hash31( Time * 1234.37) - 0.5f) * CellSize * 1;
+    float3 searchPos = position + jitter;
 
     uint startIndex, endIndex;
-    if(GridFind(position, startIndex, endIndex)) 
+    if(GridFind(searchPos, startIndex, endIndex)) 
     {
         const uint particleCount = endIndex - startIndex;
         int count =0;
-        float3 sumPosition = 0;
+        float3 sumForces = 0;
 
-        endIndex = max(startIndex + 256 , endIndex);
+        endIndex = max(startIndex + 128 , endIndex);
 
         for(uint i=startIndex; i < endIndex; ++i) 
         {
@@ -51,21 +53,29 @@ void DispersePoints(uint3 DTid : SV_DispatchThreadID, uint GI: SV_GroupIndex)
 
             if(distance > Threshold)
                 continue;
-            
-            float fallOff = max(pow(((distance)/Threshold), 0.5), 0.0001);
-            direction *= fallOff;
-            float l = length(direction);
-            direction /=l;
-            direction *= min(l, ClampAccelleration);
 
-            sumPosition += direction;
+            float force = (Threshold - distance) / Threshold;
+            force = pow(force,2);
+            
+            // float fallOff = max(pow(((distance)/Threshold), 0.5), 0.0001);
+            // direction *= fallOff;
+            // float l = length(direction);
+            // direction /=l;
+            // direction *= min(l, ClampAccelleration);
+
+            sumForces += direction * force;
             count++;
         }
 
-        if(count > 0) {
+        if(count > 0) 
+        {
+            float acceleration = length(sumForces);
+            float3 direction = sumForces / acceleration;
+            
 
-            sumPosition /= count;
-            points[DTid.x].position += sumPosition * Dispersion;
+            //sumForces /= count;
+            points[DTid.x].position += direction * (Dispersion * 0.01) * clamp(acceleration, 0, ClampAccelleration);
+            //points[DTid.x].w = min(count,3) * 1;
         }
     }
 }
