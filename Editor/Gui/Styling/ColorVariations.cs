a46944c123ab6817f91030add4aee174ff194d43
Thomas Mann <thomas.mann@framefield.com>
2023-08-12 11:31:39 +0200

    Decrease hover color impact to make text more readable

--- Editor/Gui/Styling/ColorVariations.cs
+++ Editor/Gui/Styling/ColorVariations.cs
@@ -11,7 +11,7 @@ public static class ColorVariations
     public static ColorVariation OperatorBackground = new(brightnessFactor: 0.5f, saturationFactor: 0.7f, opacityFactor: 1);
     public static ColorVariation OperatorLabel = new(brightnessFactor: 1.3f, saturationFactor: 0.4f, opacityFactor: 1);
     public static ColorVariation OperatorBackgroundIdle = new(brightnessFactor: 0.71f, saturationFactor: 1f, opacityFactor: 0.3f);
-    public static ColorVariation OperatorBackgroundHover = new(brightnessFactor: 0.75f, saturationFactor: 1, opacityFactor: 1);
+    public static ColorVariation OperatorBackgroundHover = new(brightnessFactor: 0.6f, saturationFactor: 1, opacityFactor: 1);
     public static ColorVariation OutputNodes = new(brightnessFactor: 0.35f, saturationFactor: 0.7f, opacityFactor: 0.4f);
     public static ColorVariation Highlight = new(brightnessFactor: 1.2f, saturationFactor: 1.2f, opacityFactor: 1);
     public static ColorVariation ConnectionLines = new(brightnessFactor: 1f, saturationFactor: 1, opacityFactor: 0.8f);
