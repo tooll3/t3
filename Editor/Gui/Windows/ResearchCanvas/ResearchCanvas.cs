274ae37ddfa7b2810bdcf22627322240aaf0ee1f
Thomas Mann <thomas.mann@framefield.com>
2023-09-09 18:38:40 +0200

    Vertical stacking: Basic dragging

--- Editor/Gui/Windows/ResearchCanvas/ResearchCanvas.cs
+++ Editor/Gui/Windows/ResearchCanvas/ResearchCanvas.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Windows.Forms;
 using ImGuiNET;
-using T3.Editor.Gui.Graph.Interaction;
+using T3.Core.Logging;
 using T3.Editor.Gui.InputUi;
 using T3.Editor.Gui.Interaction;
 using T3.Editor.Gui.Selection;
@@ -31,30 +32,28 @@ public class ResearchWindow : Window
 
     private void DrawWindowContent(bool hideHeader = false)
     {
-        if (true || !_initialized)
+        if (ImGui.Button("Reinit") || !_initialized)
         {
-            InitializeFromSymbol();
+            //InitializeFromSymbol();
             InitializeFromDefinition();
         }
 
         var drawList = ImGui.GetWindowDrawList();
-        var keepCursorPos = ImGui.GetCursorScreenPos();
 
         // move test block
         var posOnCanvas = _canvas.InverseTransformPositionFloat(ImGui.GetMousePos());
-        _movingTestBlock.PosOnCanvas = posOnCanvas;
+        //_movingTestBlock.PosOnCanvas = posOnCanvas;
 
         // snap test block
         var foundSnapPos = false;
         var bestSnapDistance = float.PositiveInfinity;
         var bestSnapPos = Vector2.Zero;
         var snapThreshold = 20;
-        
 
         foreach (var movingBlockSlot in _movingTestBlock.GetSlots())
         {
             var slotPosA = movingBlockSlot.Block.PosOnCanvas + movingBlockSlot.AnchorPos * BlockSize;
-            
+
             foreach (var other in _blocks)
             {
                 if (other == _movingTestBlock)
@@ -72,7 +71,7 @@ public class ResearchWindow : Window
                     if (distance < bestSnapDistance)
                     {
                         bestSnapDistance = distance;
-                        bestSnapPos = other.PosOnCanvas -  (movingBlockSlot.AnchorPos - otherSlot.AnchorPos) * BlockSize;
+                        bestSnapPos = other.PosOnCanvas - (movingBlockSlot.AnchorPos - otherSlot.AnchorPos) * BlockSize;
                         foundSnapPos = true;
                     }
                 }
@@ -81,53 +80,50 @@ public class ResearchWindow : Window
 
         if (foundSnapPos)
         {
-            _dampedMovePos = Vector2.Lerp(_dampedMovePos,bestSnapPos,0.5f);
+            _dampedMovePos = Vector2.Lerp(_dampedMovePos, bestSnapPos, 0.5f);
             _movingTestBlock.PosOnCanvas = _dampedMovePos;
         }
         else
         {
-            _dampedMovePos = _movingTestBlock.PosOnCanvas;
+            //_dampedMovePos = _movingTestBlock.PosOnCanvas;
             //_movingTestBlock.PosOnCanvas = posOnCanvas;
         }
 
-        drawList.ChannelsSplit(2);
-        drawList.ChannelsSetCurrent(1);
+        _canvas.UpdateCanvas();
+        HandleFenceSelection();
+
+        var anchorScale = _canvas.TransformDirection(BlockSize);
+        var slotSize = 3 * _canvas.Scale.X;
+
+        foreach (var b in _blocks)
         {
-            _canvas.UpdateCanvas();
-            HandleFenceSelection();
+            if (!TypeUiRegistry.Entries.TryGetValue(b.PrimaryType, out var typeUiProperties))
+                continue;
 
-            var anchorScale = _canvas.TransformDirection(BlockSize);
-            var slotSize = 3 * _canvas.Scale.X;
+            var c = typeUiProperties.Color;
+            var cLabel = ColorVariations.OperatorLabel.Apply(c);
 
-            foreach (var b in _blocks)
+            var pMin = _canvas.TransformPosition(b.PosOnCanvas);
+            var pMax = _canvas.TransformPosition(b.PosOnCanvas + BlockSize);
+            drawList.AddRectFilled(pMin, pMax, c.Fade(0.7f), 3);
+            
+            // Outline
+
+            var outlineColor = DragHandling.IsNodeSelected(b) ? UiColors.ForegroundFull 
+                                   : UiColors.BackgroundFull.Fade(0.6f);
+            drawList.AddRect(pMin, pMax, outlineColor, 3);
+            drawList.AddText(pMin + new Vector2(2, -2), cLabel, b.Name);
+            ImGui.SetCursorScreenPos(pMin);
+            ImGui.InvisibleButton(b.Id.ToString(), anchorScale);
+            DragHandling.HandleItemDragging(b, _canvas);
+
+            foreach (var slot in b.GetSlots())
             {
-                if (!TypeUiRegistry.Entries.TryGetValue(b.PrimaryType, out var typeUiProperties))
-                    continue;
-
-                var c = typeUiProperties.Color;
-                var cLabel = ColorVariations.OperatorLabel.Apply(c);
-
-                var pMin = _canvas.TransformPosition(b.PosOnCanvas);
-                var pMax = _canvas.TransformPosition(b.PosOnCanvas + BlockSize);
-                drawList.AddRectFilled(pMin, pMax, c.Fade(0.7f), 3);
-                drawList.AddRect(pMin, pMax, UiColors.BackgroundFull.Fade(0.6f), 3);
-                drawList.AddText(pMin + new Vector2(2, -2), cLabel, b.Name);
-
-                foreach (var slot in b.GetSlots())
-                {
-                    var xxx = slot.Connections.Count > 0 && slot.Connections[0].IsSnapped;
-                    var thickness = xxx ? 2 : 1;
-                    drawList.AddCircle(pMin + anchorScale * slot.AnchorPos, slotSize, c, 3, thickness);
-                }
+                var xxx = slot.Connections.Count > 0 && slot.Connections[0].IsSnapped;
+                var thickness = xxx ? 2 : 1;
+                drawList.AddCircle(pMin + anchorScale * slot.AnchorPos, slotSize, c, 3, thickness);
             }
         }
-
-        drawList.ChannelsSetCurrent(0);
-        {
-            ImGui.SetCursorScreenPos(keepCursorPos);
-        }
-
-        drawList.ChannelsMerge();
     }
 
     private readonly ScalableCanvas _canvas = new();
@@ -256,7 +252,7 @@ public class ResearchWindow : Window
     private readonly List<Slot> _slots = new();
 }
 
-public class Block
+public class Block : ISelectableCanvasObject
 {
     public Block()
     {
@@ -264,6 +260,7 @@ public class Block
 
     public Block(int x, int y, string name)
     {
+        Id = Guid.NewGuid();
         Name = name;
         PosOnCanvas = new Vector2(x, y) * ResearchWindow.BlockSize;
         Inputs = new List<Slot>()
@@ -292,13 +289,21 @@ public class Block
         }
     }
 
-    public Vector2 PosOnCanvas;
     public int UnitHeight;
     public string Name;
     public Type PrimaryType = typeof(float);
 
     public List<Slot> Inputs = new();
     public List<Slot> Outputs = new();
+    public Guid Id { get; }
+    public Vector2 PosOnCanvas { get; set; }
+    public Vector2 Size { get; set; }
+    public bool IsSelected { get; }
+
+    public override string ToString()
+    {
+        return Name;
+    }
 }
 
 public class Group
@@ -329,4 +334,197 @@ public class Connection
     public Slot Source;
     public Slot Target;
     public bool IsSnapped;
+}
+
+public static class DragHandling
+{
+    /// <summary>
+    /// NOTE: This has to be called for ALL movable elements (ops, inputs, outputs) and directly after ImGui.Item
+    /// </summary>
+    public static void HandleItemDragging(ISelectableCanvasObject node, ScalableCanvas canvas)
+    {
+        var justClicked = ImGui.IsItemHovered(ImGuiHoveredFlags.AllowWhenBlockedByPopup) && ImGui.IsMouseClicked(ImGuiMouseButton.Left);
+
+        var isActiveNode = node == _draggedNode;
+        
+        
+        //Log.Debug($"is Active node {node}  {isActiveNode}   DraggedNode:{_draggedNode}");
+        if (justClicked)
+        {
+            //var compositionSymbolId = GraphCanvas.Current.CompositionOp.Symbol.Id;
+            _draggedNode = node;
+            if (node.IsSelected)
+            {
+                _draggedNodes = _selectedNodes;
+            }
+            else
+            {
+                //var parentUi = SymbolUiRegistry.Entries[GraphCanvas.Current.CompositionOp.Symbol.Id];
+
+                _draggedNodes.Add(node);
+            }
+
+            // _moveCommand = new ModifyCanvasElementsCommand(compositionSymbolId, _draggedNodes);
+        }
+        else if (isActiveNode && ImGui.IsMouseDown(ImGuiMouseButton.Left)) // && _moveCommand != null)
+        {
+            // if (!T3Ui.IsCurrentlySaving && ShakeDetector.TestDragForShake(ImGui.GetMousePos()))
+            // {
+            //     _moveCommand.StoreCurrentValues();
+            //     UndoRedoStack.Add(_moveCommand);
+            //     DisconnectDraggedNodes();
+            // }
+            if (!ImGui.IsMouseDragging(ImGuiMouseButton.Left))
+            {
+                _isDragging = false;
+            }
+            else
+            {
+                if (!_isDragging)
+                {
+                    _dragStartPosInOpOnCanvas = canvas.InverseTransformPositionFloat(ImGui.GetMousePos()) - node.PosOnCanvas;
+                    _isDragging = true;
+                }
+
+                var mousePosOnCanvas = canvas.InverseTransformPositionFloat(ImGui.GetMousePos());
+                var newDragPosInCanvas = mousePosOnCanvas - _dragStartPosInOpOnCanvas;
+                //
+                // var bestDistanceInCanvas = float.PositiveInfinity;
+                // var targetSnapPositionInCanvas = Vector2.Zero;
+                //
+                // foreach (var offset in _snapOffsetsInCanvas)
+                // {
+                //     var heightAffectFactor = 0;
+                //     if (Math.Abs(offset.X) < 0.01f)
+                //     {
+                //         if (offset.Y > 0)
+                //         {
+                //             heightAffectFactor = -1;
+                //         }
+                //         else
+                //         {
+                //             heightAffectFactor = 1;
+                //         }
+                //     }
+                //
+                //     foreach (var neighbor in GraphCanvas.Current.SelectableChildren)
+                //     {
+                //         if (neighbor == node || _draggedNodes.Contains(neighbor))
+                //             continue;
+                //
+                //         var offset2 = new Vector2(offset.X, -neighbor.Size.Y * heightAffectFactor + offset.Y);
+                //         var snapToNeighborPos = neighbor.PosOnCanvas + offset2;
+                //
+                //         var d = Vector2.Distance(snapToNeighborPos, newDragPosInCanvas);
+                //         if (!(d < bestDistanceInCanvas))
+                //             continue;
+                //
+                //         targetSnapPositionInCanvas = snapToNeighborPos;
+                //         bestDistanceInCanvas = d;
+                //     }
+                // }
+                //
+                // var snapDistanceInCanvas = GraphCanvas.Current.InverseTransformDirection(new Vector2(20, 0)).X;
+                // var isSnapping = bestDistanceInCanvas < snapDistanceInCanvas;
+
+                //var isSnapping = false;
+                // var moveDeltaOnCanvas = isSnapping
+                //                             ? targetSnapPositionInCanvas - node.PosOnCanvas
+                //                             : newDragPosInCanvas - node.PosOnCanvas;
+
+                var moveDeltaOnCanvas = newDragPosInCanvas - node.PosOnCanvas;
+                // Drag selection
+                foreach (var e in _draggedNodes)
+                {
+                    e.PosOnCanvas += moveDeltaOnCanvas;
+                }
+            }
+        }
+        else if (isActiveNode && ImGui.IsMouseReleased(0))
+        {
+            if (_draggedNode != node)
+                return;
+
+            //var singleDraggedNode = (_draggedNodes.Count == 1) ? _draggedNodes[0] : null;
+            _draggedNode = null;
+            _draggedNodes.Clear();
+
+            var wasDragging = ImGui.GetMouseDragDelta(ImGuiMouseButton.Left).LengthSquared() > UserSettings.Config.ClickThreshold;
+            if (wasDragging)
+            {
+                // connection lines would be split here...
+            }
+            else
+            {
+                if (!IsNodeSelected(node))
+                {
+                    var replaceSelection = !ImGui.GetIO().KeyShift;
+                    if (replaceSelection)
+                    {
+                        SetSelection(node);
+                    }
+                    else
+                    {
+                        AddSelection(node);
+                    }
+                }
+                else
+                {
+                    if (ImGui.GetIO().KeyShift)
+                    {
+                        DeselectNode(node);
+                    }
+                }
+            }
+        }
+        else if (ImGui.IsMouseReleased(0)) // && _moveCommand == null)
+        {
+            // This happens after shake
+            _draggedNodes.Clear();
+            _draggedNode = null;
+        }
+
+        var wasDraggingRight = ImGui.GetMouseDragDelta(ImGuiMouseButton.Right).Length() > UserSettings.Config.ClickThreshold;
+        if (ImGui.IsMouseReleased(ImGuiMouseButton.Right)
+            && !wasDraggingRight
+            && ImGui.IsItemHovered()
+            && !IsNodeSelected(node))
+        {
+            SetSelection(node);
+        }
+    }
+
+    public static void SetSelection(ISelectableCanvasObject selectedObject)
+    {
+        _selectedNodes.Clear();
+        _selectedNodes.Add(selectedObject);
+    }
+
+    public static bool IsNodeSelected(ISelectableCanvasObject node)
+    {
+        return _selectedNodes.Contains(node);
+    }
+
+    public static void AddSelection(IEnumerable<ISelectableCanvasObject> additionalObjects)
+    {
+        _selectedNodes.UnionWith(additionalObjects);
+    }
+
+    public static void AddSelection(ISelectableCanvasObject additionalObject)
+    {
+        _selectedNodes.Add(additionalObject);
+    }
+
+    public static void DeselectNode(ISelectableCanvasObject objectToRemove)
+    {
+        _selectedNodes.Remove(objectToRemove);
+    }
+
+    private static bool _isDragging;
+    private static Vector2 _dragStartPosInOpOnCanvas;
+
+    public static HashSet<ISelectableCanvasObject> _selectedNodes = new();
+
+    private static ISelectableCanvasObject _draggedNode;
+    private static HashSet<ISelectableCanvasObject> _draggedNodes = new();
 }
