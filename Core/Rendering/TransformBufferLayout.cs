9569b454f4183fdc1b9811bbc26df6871ce60bcb
Thomas Mann <thomas.mann@framefield.com>
2023-10-15 13:51:54 +0200

    Renamed [GetCamProperties] -> [GetCamTransformBuffers]

---
+++ Core/Rendering/TransformBufferLayout.cs
@@ -0,0 +1,69 @@
+﻿using System.Runtime.InteropServices;
+using SharpDX;
+
+[StructLayout(LayoutKind.Explicit, Size = 4 * 4 * 4 * 10)]
+public struct TransformBufferLayout
+{
+    public TransformBufferLayout(Matrix cameraToClipSpace, Matrix worldToCamera, Matrix objectToWorld)
+    {
+        Matrix clipSpaceToCamera = cameraToClipSpace;
+        clipSpaceToCamera.Invert();
+        Matrix cameraToWorld = worldToCamera;
+        cameraToWorld.Invert();
+        Matrix worldToObject = objectToWorld;
+        worldToObject.Invert();
+
+        CameraToClipSpace = cameraToClipSpace;
+        ClipSpaceToCamera = clipSpaceToCamera;
+        WorldToCamera = worldToCamera;
+        CameraToWorld = cameraToWorld;
+        WorldToClipSpace = Matrix.Multiply(worldToCamera, cameraToClipSpace);
+        ClipSpaceToWorld = Matrix.Multiply(clipSpaceToCamera, cameraToWorld);
+        ObjectToWorld = objectToWorld;
+        WorldToObject = worldToObject;
+        ObjectToCamera = Matrix.Multiply(objectToWorld, worldToCamera);
+        ObjectToClipSpace = Matrix.Multiply(ObjectToCamera, cameraToClipSpace);
+
+        // transpose all as mem layout in hlsl constant buffer is row based
+        CameraToClipSpace.Transpose();
+        ClipSpaceToCamera.Transpose();
+        WorldToCamera.Transpose();
+        CameraToWorld.Transpose();
+        WorldToClipSpace.Transpose();
+        ClipSpaceToWorld.Transpose();
+        ObjectToWorld.Transpose();
+        WorldToObject.Transpose();
+        ObjectToCamera.Transpose();
+        ObjectToClipSpace.Transpose();
+    }
+
+    [FieldOffset(0)]
+    public Matrix CameraToClipSpace;
+
+    [FieldOffset(64)]
+    public Matrix ClipSpaceToCamera;
+
+    [FieldOffset(128)]
+    public Matrix WorldToCamera;
+
+    [FieldOffset(192)]
+    public Matrix CameraToWorld;
+
+    [FieldOffset(256)]
+    public Matrix WorldToClipSpace;
+
+    [FieldOffset(320)]
+    public Matrix ClipSpaceToWorld;
+
+    [FieldOffset(384)]
+    public Matrix ObjectToWorld;
+
+    [FieldOffset(448)]
+    public Matrix WorldToObject;
+
+    [FieldOffset(512)]
+    public Matrix ObjectToCamera;
+
+    [FieldOffset(576)]
+    public Matrix ObjectToClipSpace;
+}
--- Operators/Types/lib/3d/rendering/_/GetCamTransformBuffer.cs
+++ Operators/Types/lib/3d/rendering/_/GetCamTransformBuffer.cs
@@ -1,5 +1,4 @@
 using System;
-using T3.Core;
 using T3.Core.Logging;
 using T3.Core.Operator;
 using T3.Core.Operator.Attributes;
@@ -7,12 +6,11 @@ using T3.Core.Operator.Slots;
 using T3.Core.Rendering;
 using T3.Core.Resource;
 using T3.Operators.Types.Id_a60adc26_d7c6_4615_af78_8d2d6da46b79;
-using T3.Operators.Utils;
 using Buffer = SharpDX.Direct3D11.Buffer;
 
 namespace T3.Operators.Types.Id_843c9378_6836_4f39_b676_06fd2828af3e
 {
-    public class GetCamProperties : Instance<GetCamProperties>
+    public class GetCamTransformBuffer : Instance<GetCamTransformBuffer>
     {
         [Output(Guid = "FB108D2D-04B0-427D-888D-79EB7EBF1E96", DirtyFlagTrigger = DirtyFlagTrigger.Always)]
         public readonly Slot<Buffer> Buffer = new Slot<Buffer>();
@@ -20,7 +18,7 @@ namespace T3.Operators.Types.Id_843c9378_6836_4f39_b676_06fd2828af3e
         [Output(Guid = "8EDC2DB1-A214-4B77-A334-FA4BF1FF1AB7", DirtyFlagTrigger = DirtyFlagTrigger.Always)]
         public readonly Slot<Buffer> PreviousBuffer = new Slot<Buffer>();
         
-        public GetCamProperties()
+        public GetCamTransformBuffer()
         {
             Buffer.UpdateAction = Update;
             
@@ -54,7 +52,7 @@ namespace T3.Operators.Types.Id_843c9378_6836_4f39_b676_06fd2828af3e
                 PreviousBuffer.DirtyFlag.Clear();
             }
             
-            _bufferContent = new TransformsConstBuffer.TransformBufferLayout(camera.CameraToClipSpace, camera.WorldToCamera, camera.LastObjectToWorld);
+            _bufferContent = new TransformBufferLayout(camera.CameraToClipSpace, camera.WorldToCamera, camera.LastObjectToWorld);
             ResourceManager.SetupConstBuffer(_bufferContent, ref Buffer.Value);
             Buffer.Value.DebugName=nameof(TransformsConstBuffer);
             _previousBufferInitialized = true;
@@ -64,7 +62,7 @@ namespace T3.Operators.Types.Id_843c9378_6836_4f39_b676_06fd2828af3e
         public readonly InputSlot<Object> CameraReference = new InputSlot<Object>();
 
         
-        private TransformsConstBuffer.TransformBufferLayout _bufferContent;
+        private TransformBufferLayout _bufferContent;
         private bool _previousBufferInitialized;
     }
 }
