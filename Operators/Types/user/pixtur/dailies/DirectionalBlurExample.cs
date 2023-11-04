14a30849959f7e7ebb175f9c0b226765bebad584
Thomas Mann <thomas.mann@framefield.com>
2023-11-04 22:37:52 +0100

    Add [DirectionalBlurExample]

--- Operators/Types/user/pixtur/dailies/DirectionalBlurExample.cs
+++ Operators/Types/user/pixtur/dailies/DirectionalBlurExample.cs
@@ -5,7 +5,7 @@ using T3.Core.Operator.Slots;
 
 namespace T3.Operators.Types.Id_b01a8336_6dcc_45cd_a86b_a0880772f9a9
 {
-    public class Daily_2023_11_06 : Instance<Daily_2023_11_06>
+    public class DirectionalBlurExample : Instance<DirectionalBlurExample>
     {
         [Output(Guid = "623bd104-bf6f-416b-b9c0-d8022e87845e")]
         public readonly Slot<Texture2D> ColorBuffer = new Slot<Texture2D>();
