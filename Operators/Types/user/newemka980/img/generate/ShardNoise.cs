65cb90f9edc40d26fcffe37cf8b0ab4c6325c18e
newemka <newemka@users.noreply.github.com>
2023-12-17 18:01:35 +0100

    Sunday polish
    improved UI and UX
    I added direction and rate, enums for methods, please check the comments in the HLSL code.
    There's a few presets.

--- Operators/Types/user/newemka980/img/generate/ShardNoise.cs
+++ Operators/Types/user/newemka980/img/generate/ShardNoise.cs
@@ -17,7 +17,7 @@ namespace T3.Operators.Types.Id_dc34c54b_f856_4fd2_a182_68fd75189d7d
         public readonly InputSlot<System.Numerics.Vector4> ColorB = new InputSlot<System.Numerics.Vector4>();
 
         [Input(Guid = "3abec2c0-a46b-439a-8b00-21e3e9a36933")]
-        public readonly InputSlot<System.Numerics.Vector2> Offset = new InputSlot<System.Numerics.Vector2>();
+        public readonly InputSlot<System.Numerics.Vector2> Direction = new InputSlot<System.Numerics.Vector2>();
 
         [Input(Guid = "3949fce5-e285-4787-9db6-0ef2e533b15e")]
         public readonly InputSlot<System.Numerics.Vector2> Stretch = new InputSlot<System.Numerics.Vector2>();
@@ -28,23 +28,20 @@ namespace T3.Operators.Types.Id_dc34c54b_f856_4fd2_a182_68fd75189d7d
         [Input(Guid = "58ff3aee-7ced-4634-ac4a-871d67d96b57")]
         public readonly InputSlot<float> Sharpen = new InputSlot<float>();
 
-        [Input(Guid = "12d39fbe-cea2-44af-acf7-901df793e0c4")]
-        public readonly InputSlot<float> Phase = new InputSlot<float>();
-
         [Input(Guid = "fe0bda00-1c88-45d6-88a7-bc955ef5db61")]
         public readonly InputSlot<float> Bias = new InputSlot<float>();
 
+        [Input(Guid = "12d39fbe-cea2-44af-acf7-901df793e0c4")]
+        public readonly InputSlot<float> Phase = new InputSlot<float>();
+
+        [Input(Guid = "0b76c7f3-32b0-4aa0-a17d-16cb8bde5f80")]
+        public readonly InputSlot<float> Rate = new InputSlot<float>();
+
+        [Input(Guid = "35852bd6-be0a-4dae-b078-b6db734ca772", MappedType = typeof(Methods))]
+        public readonly InputSlot<int> Method = new InputSlot<int>();
+
         [Input(Guid = "d2e58300-b054-4df5-b3e8-5d7fb36d1ebc")]
         public readonly InputSlot<T3.Core.DataTypes.Vector.Int2> Resolution = new InputSlot<T3.Core.DataTypes.Vector.Int2>();
-
-        [Input(Guid = "abbc8df8-3430-4402-8748-18274ad56f0c")]
-        public readonly InputSlot<System.Numerics.Vector3> WarpOffset = new InputSlot<System.Numerics.Vector3>();
-
-        [Input(Guid = "a2a5bf86-a3f5-42a7-b7ce-19b34f7ee5c3")]
-        public readonly InputSlot<int> Iterations = new InputSlot<int>();
-
-        [Input(Guid = "35852bd6-be0a-4dae-b078-b6db734ca772")]
-        public readonly InputSlot<int> Method = new InputSlot<int>();
 
         [Input(Guid = "1583d95b-da27-4d12-9937-8cd99a61bb18")]
         public readonly InputSlot<bool> GenerateMips = new InputSlot<bool>();
@@ -52,9 +49,9 @@ namespace T3.Operators.Types.Id_dc34c54b_f856_4fd2_a182_68fd75189d7d
 
         private enum Methods
         {
-            Legacy,
-            OpenSimplex2S,
-            OpenSimplex2S_NormalMap,
+            Cubism,
+            Cubism_X_Octaves,
+            Octaves,
         }
     }
 }
