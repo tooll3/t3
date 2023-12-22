51f56ee6462749c260b8ce93ca5b6bfd04f7da1c
Thomas Mann <thomas.mann@framefield.com>
2023-12-22 20:28:11 +0100

    Add DrawMesh Filter parameter

--- Operators/Types/lib/3d/draw/DrawMesh.cs
+++ Operators/Types/lib/3d/draw/DrawMesh.cs
@@ -11,11 +11,11 @@ namespace T3.Operators.Types.Id_a3c5471e_079b_4d4b_886a_ec02d6428ff6
         [Output(Guid = "53b3fdca-9d5e-4808-a02f-4aa743cd8456")]
         public readonly Slot<Command> Output = new Slot<Command>();
 
-        [Input(Guid = "8c9dee45-d165-48c8-b8dd-b7f47e77fd00")]
-        public readonly InputSlot<System.Numerics.Vector4> Color = new InputSlot<System.Numerics.Vector4>();
-
         [Input(Guid = "97429e1f-3f30-4789-89a6-8e930e356ee6")]
         public readonly InputSlot<T3.Core.DataTypes.MeshBuffers> Mesh = new InputSlot<T3.Core.DataTypes.MeshBuffers>();
+
+        [Input(Guid = "8c9dee45-d165-48c8-b8dd-b7f47e77fd00")]
+        public readonly InputSlot<System.Numerics.Vector4> Color = new InputSlot<System.Numerics.Vector4>();
 
         [Input(Guid = "9c17fa15-35f1-49d4-802f-a3a796cad96a", MappedType = typeof(SharedEnums.BlendModes))]
         public readonly InputSlot<int> BlendMode = new InputSlot<int>();
@@ -34,6 +34,9 @@ namespace T3.Operators.Types.Id_a3c5471e_079b_4d4b_886a_ec02d6428ff6
 
         [Input(Guid = "2c4b5f3a-e9ec-432e-b1ae-6d999ae44f1b", MappedType = typeof(FillMode))]
         public readonly InputSlot<int> FillMode = new InputSlot<int>();
+
+        [Input(Guid = "155c2396-0e05-4437-8171-288048b1158a")]
+        public readonly InputSlot<SharpDX.Direct3D11.Filter> Filter = new InputSlot<SharpDX.Direct3D11.Filter>();
 
     }
 }
