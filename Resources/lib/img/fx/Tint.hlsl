13fbed57440850e3c5e8ddc01fcc05ad760d93ca
Thomas Mann <thomas.mann@framefield.com>
2024-01-10 23:57:08 +0100

    Add [Tint] GainAndBias parameter

--- Resources/lib/img/fx/Tint.hlsl
+++ Resources/lib/img/fx/Tint.hlsl
@@ -1,3 +1,5 @@
+#include "lib/shared/bias-functions.hlsl"
+
 Texture2D<float4> inputTexture : register(t0);
 sampler texSampler : register(s0);
 
@@ -7,7 +9,7 @@ cbuffer ParamConstants : register(b0)
     float4 MapWhiteTo;
     float4 ChannelWeights;
     float Amount;
-    float Bias;
+    float2 BiasAndGain;
 }
 
 
@@ -25,15 +27,15 @@ struct vsOutput
     float2 texCoord : TEXCOORD;
 };
 
-float GetSchlickBias(float x, float bias)
-{
-    float x1 = x * 2;
-    float x2 = x * 2 - 1;
-    float bias1 = 1 - bias;
-    return x < 0.5
-        ? x1 / ((1 / bias - 2) * (1 - x1) + 1) / 2
-        : x2 / ((1 / bias1 - 2) * (1 - x2) + 1) / 2 + 0.5;
-}
+// float GetSchlickBias(float x, float bias)
+// {
+//     float x1 = x * 2;
+//     float x2 = x * 2 - 1;
+//     float bias1 = 1 - bias;
+//     return x < 0.5
+//         ? x1 / ((1 / bias - 2) * (1 - x1) + 1) / 2
+//         : x2 / ((1 / bias1 - 2) * (1 - x2) + 1) / 2 + 0.5;
+// }
 
 float4 psMain(vsOutput psInput) : SV_TARGET
 {
@@ -46,8 +48,8 @@ float4 psMain(vsOutput psInput) : SV_TARGET
     // t = Bias> 0 
     //     ? pow( t, Bias+1)
     //     : 1-pow( 1-t, -Bias+1);
-    t = GetSchlickBias(t, clamp(Bias, 0.001, 0.999));
-
+    //t = GetSchlickBias(t, clamp(Bias, 0.001, 0.999));
+    t= GetBiasGain(t, BiasAndGain.x, BiasAndGain.y); 
     float4 mapped = lerp(MapBlackTo, MapWhiteTo, t); 
     //return float4(t,0,0,1);
     float4 cout = lerp(c, mapped, Amount);
