0c3836c9b11e0cc232b53fd59ce54fc4723950c3
Thomas Mann <thomas.mann@framefield.com>
2024-01-21 01:49:03 +0100

    Update [SelectPoints]

--- Resources/lib/points/modify/SelectPoints.hlsl
+++ Resources/lib/points/modify/SelectPoints.hlsl
@@ -2,6 +2,7 @@
 #include "lib/shared/noise-functions.hlsl"
 #include "lib/shared/point.hlsl"
 #include "lib/shared/quat-functions.hlsl"
+#include "lib/shared/bias-functions.hlsl"
 
 cbuffer Transforms : register(b0)
 {
@@ -15,20 +16,25 @@ cbuffer Transforms : register(b0)
     float4x4 WorldToObject;
     float4x4 ObjectToCamera;
     float4x4 ObjectToClipSpace;
-};
+}
 
 cbuffer Params : register(b1)
 {
     float4x4 TransformVolume;
     float FallOff;
-    float Bias;
-    float VolumeShape;
-    float SelectMode;
-    float ClampResult;
     float Strength;
+    float2 BiasAndGain;
     float Phase;
     float Threshold;
-    float DiscardNonSelected;
+}
+
+cbuffer Params : register(b2)
+{
+    int VolumeShape;
+    int SelectMode;
+    int ClampResult;
+    int DiscardNonSelected;
+    int SetW;
 }
 
 StructuredBuffer<Point> SourcePoints : t0;
@@ -36,23 +42,28 @@ RWStructuredBuffer<Point> ResultPoints : u0;
 
 static const float NoisePhase = 0;
 
-static const float VolumeSphere = 0.5;
-static const float VolumeBox = 1.5;
-static const float VolumePlane = 2.5;
-static const float VolumeZebra = 3.5;
-static const float VolumeNoise = 4.5;
+#define VolumeSphere  0
+#define VolumeBox  1
+#define VolumePlane  2
+#define VolumeZebra  3
+#define VolumeNoise  4
 
-static const float ModeOverride = 0.5;
-static const float ModeAdd = 1.5;
-static const float ModeSub = 2.5;
-static const float ModeMultiply = 3.5;
-static const float ModeInvert = 4.5;
+#define ModeOverride    0
+#define ModeAdd         1
+#define ModeSub         2
+#define ModeMultiply    3
+#define ModeInvert      4
 
 float Bias2(float x, float bias)
 {
     return bias < 0
                ? pow(x, clamp(bias + 1, 0.005, 1))
                : 1 - pow(1 - x, clamp(1 - bias, 0.005, 1));
+}
+
+inline float LinearStep(float min, float max, float t) 
+{
+    return saturate((t- min) / (max-min)  );
 }
 
 [numthreads(64, 1, 1)] void main(uint3 i
@@ -69,72 +80,76 @@ float Bias2(float x, float bias)
 
     //ResultPoints[i.x] = SourcePoints[i.x];
 
-    if (!isnan(p.W))
+    if (isnan(p.W))
     {
-        float3 posInObject = p.Position;
-
-        float3 posInVolume = mul(float4(posInObject, 1), TransformVolume).xyz;
-
-        float s = 1;
-
-        if (VolumeShape < VolumeSphere)
-        {
-            float distance = length(posInVolume);
-            s = smoothstep(1 + FallOff, 1, distance);
-        }
-        else if (VolumeShape < VolumeBox)
-        {
-            float3 t = abs(posInVolume);
-            float distance = max(max(t.x, t.y), t.z) + Phase;
-            s = smoothstep(1 + FallOff, 1, distance);
-        }
-        else if (VolumeShape < VolumePlane)
-        {
-            float distance = posInVolume.y;
-            s = smoothstep(FallOff, 0, distance);
-        }
-        else if (VolumeShape < VolumeZebra)
-        {
-            float distance = 1 - abs(mod(posInVolume.y * 1 + Phase, 2) - 1);
-            s = smoothstep(Threshold + 0.5 + FallOff, Threshold + 0.5, distance);
-        }
-        else if (VolumeShape < VolumeNoise)
-        {
-            float3 noiseLookup = (posInVolume * 0.91 + Phase);
-            float noise = snoise(noiseLookup);
-            s = smoothstep(Threshold + FallOff, Threshold, noise);
-        }
-
-        s = Bias2(s, Bias);
-
-        float selected = p.Selected;
-        if (SelectMode < ModeOverride)
-        {
-            s *= Strength;
-        }
-        else if (SelectMode < ModeAdd)
-        {
-            s += selected * Strength;
-        }
-        else if (SelectMode < ModeSub)
-        {
-            s = selected - s * Strength;
-        }
-        else if (SelectMode < ModeMultiply)
-        {
-            s = lerp(selected, selected * s, Strength);
-        }
-        else if (SelectMode < ModeInvert)
-        {
-            s = s * (1 - selected);
-        }
-
-        p.Selected = (DiscardNonSelected > 0.5 && s <= 0)
-                        ? sqrt(-1)
-                    : (ClampResult > 0.5)
-                        ? saturate(s)
-                        : s;
+        ResultPoints[i.x] = p;
+        return;
     }
+
+    float3 posInObject = p.Position;
+
+    float3 posInVolume = mul(float4(posInObject, 1), TransformVolume).xyz;
+
+    float s = 1;
+
+    if (VolumeShape == VolumeSphere)
+    {
+        float distance = length(posInVolume);
+        s = LinearStep(1 + FallOff, 1, distance);
+        
+    }
+    else if (VolumeShape == VolumeBox)
+    {
+        float3 t = abs(posInVolume);
+        float distance = max(max(t.x, t.y), t.z) + Phase;
+        s = LinearStep(1 + FallOff, 1, distance);
+    }
+    else if (VolumeShape == VolumePlane)
+    {
+        float distance = posInVolume.y;
+        s = LinearStep(FallOff, 0, distance);
+    }
+    else if (VolumeShape == VolumeZebra)
+    {
+        float distance = 1 - abs(mod(posInVolume.y * 1 + Phase, 2) - 1);
+        s = LinearStep(Threshold + 0.5 + FallOff, Threshold + 0.5, distance);
+    }
+    else if (VolumeShape == VolumeNoise)
+    {
+        float3 noiseLookup = (posInVolume * 0.91 + Phase);
+        float noise = snoise(noiseLookup);
+        s = LinearStep(Threshold + FallOff, Threshold, noise);
+    }
+
+    s = GetBiasGain(s, BiasAndGain.x, BiasAndGain.y);
+
+    float w = p.W;
+    if (SelectMode == ModeOverride)
+    {
+        s *= Strength;
+    }
+    else if (SelectMode == ModeAdd)
+    {
+        s += w * Strength;
+    }
+    else if (SelectMode == ModeSub)
+    {
+        s = w - s * Strength;
+    }
+    else if (SelectMode == ModeMultiply)
+    {
+        s = lerp(w, w * s, Strength);
+    }
+    else if (SelectMode == ModeInvert)
+    {
+        s = s * (1 - w);
+    }
+
+    p.W = (DiscardNonSelected && s <= 0)
+                    ? sqrt(-1)
+                : (ClampResult)
+                    ? saturate(s)
+                    : s;
 
     ResultPoints[i.x] = p;
 }
