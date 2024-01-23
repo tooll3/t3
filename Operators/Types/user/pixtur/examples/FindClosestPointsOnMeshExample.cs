169f1cb91f41df17b09bcda895e1d627b4e42551
newemka <newemka@gmail.com>
2024-01-23 17:41:25 +0100

    Attempt to Fix [FindClosestPointsOnMesh]
    The HLSL file was giving a warning because of lower case instead of upper case.
    I renamed the op to match with its example.
    Unfortunately I didn't manage to fully fix it.

--- Operators/Types/user/pixtur/examples/FindClosestPointsOnMeshExample.cs
+++ Operators/Types/user/pixtur/examples/FindClosestPointsOnMeshExample.cs
@@ -5,7 +5,7 @@ using T3.Core.Operator.Slots;
 
 namespace T3.Operators.Types.Id_ff01e31e_c987_449f_ab4a_066fedf5d237
 {
-    public class FindClosestPointOnMeshExample : Instance<FindClosestPointOnMeshExample>
+    public class FindClosestPointsOnMeshExample : Instance<FindClosestPointsOnMeshExample>
     {
         [Output(Guid = "fa516e85-0fda-486b-b820-1acb77deea3e")]
         public readonly Slot<Texture2D> ColorBuffer = new();
