09a2d274ea520d18348739d066a9742bc72d2dbf
Thomas Mann <thomas.mann@framefield.com>
2024-01-27 02:20:17 +0100

    Adding operators with context menu will use click position

--- Editor/Gui/Graph/GraphCanvas.cs
+++ Editor/Gui/Graph/GraphCanvas.cs
@@ -672,6 +672,8 @@ namespace T3.Editor.Gui.Graph
 
         private void DrawContextMenuContent()
         {
+            var clickPosition =ImGui.GetMousePosOnOpeningCurrentPopup();
+
             var selectedChildUis = GetSelectedChildUis();
             var nextUndoTitle = UndoRedoStack.CanUndo ? $" ({UndoRedoStack.GetNextUndoTitle()})" : string.Empty;
             if (ImGui.MenuItem("Undo" + nextUndoTitle,
@@ -886,7 +888,8 @@ namespace T3.Editor.Gui.Graph
             {
                 if (ImGui.MenuItem("Add Node...", "TAB", false, true))
                 {
-                    SymbolBrowser.OpenAt(InverseTransformPositionFloat(ImGui.GetMousePos()), null, null, false);
+
+                    SymbolBrowser.OpenAt(InverseTransformPositionFloat(clickPosition), null, null, false);
                 }
 
                 if (ImGui.MenuItem("Add input parameter..."))
