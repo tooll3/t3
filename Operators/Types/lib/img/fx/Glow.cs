44901862b4fec75925f43d52f4107dbc5b6f4f12
Thomas Mann <thomas.mann@framefield.com>
2024-01-31 23:02:56 +0100

    Sort [Blur] parameter

--- Operators/Types/lib/img/fx/Glow.cs
+++ Operators/Types/lib/img/fx/Glow.cs
@@ -14,31 +14,29 @@ namespace T3.Operators.Types.Id_d392d4af_4c78_4f4a_bc3f_4c54c8c73538
         [Output(Guid = "78523193-3df8-4189-88c0-46091d53892e")]
         public readonly Slot<Command> Output = new();
 
-
-
         [Input(Guid = "f6bdd487-c16e-4fb0-bfba-b3801f121314")]
-        public readonly InputSlot<Texture2D> Texture = new();
+        public readonly InputSlot<SharpDX.Direct3D11.Texture2D> Texture = new InputSlot<SharpDX.Direct3D11.Texture2D>();
 
         [Input(Guid = "57968725-0a45-44f9-a9a2-f74c10b728e8")]
-        public readonly InputSlot<float> BlurRadius = new();
-
-        [Input(Guid = "353ac2ee-aed3-4614-adf5-e1328768fd0b")]
-        public readonly InputSlot<float> Samples = new();
+        public readonly InputSlot<float> Radius = new InputSlot<float>();
 
         [Input(Guid = "4927a3fc-87ff-44e7-88c0-499e3efcca55")]
-        public readonly InputSlot<float> GlowAmount = new();
-
-        [Input(Guid = "4c9b9135-f27b-414e-bed7-f9e5640dc526")]
-        public readonly InputSlot<float> Offset = new();
-
-        [Input(Guid = "7f6dba80-bf4e-4e55-bd9a-ac1e2a077898")]
-        public readonly InputSlot<System.Numerics.Vector4> Color = new();
+        public readonly InputSlot<float> Intensity = new InputSlot<float>();
 
         [Input(Guid = "03f2bd5c-b599-47f5-b697-9f881639a598")]
-        public readonly InputSlot<float> AmplifyFineBlur = new();
+        public readonly InputSlot<float> AmplifyCore = new InputSlot<float>();
+
+        [Input(Guid = "4c9b9135-f27b-414e-bed7-f9e5640dc526")]
+        public readonly InputSlot<float> Threshold = new InputSlot<float>();
+
+        [Input(Guid = "7f6dba80-bf4e-4e55-bd9a-ac1e2a077898")]
+        public readonly InputSlot<System.Numerics.Vector4> Color = new InputSlot<System.Numerics.Vector4>();
+
+        [Input(Guid = "353ac2ee-aed3-4614-adf5-e1328768fd0b")]
+        public readonly InputSlot<float> Samples = new InputSlot<float>();
 
         [Input(Guid = "53fe4db2-128c-43e3-8c58-8f01694d13ac", MappedType = typeof(SharedEnums.BlendModes))]
-        public readonly InputSlot<int> BlendMode = new();
+        public readonly InputSlot<int> BlendMode = new InputSlot<int>();
 
     }
 }
