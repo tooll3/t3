360d2a1aab4d4d077e4af2c602ecb8f3d52c29d5
Alix Carolan <vayam.eon@gmail.com>
2024-02-14 16:15:44 +0000

    Add [DampFloatList] operator

---
+++ Operators/Types/lib/math/floats/DampFloatList.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using T3.Core.Animation;
+using T3.Core.Logging;
+using T3.Core.Operator;
+using T3.Core.Operator.Attributes;
+using T3.Core.Operator.Slots;
+using T3.Core.Utils;
+using T3.Operators.Types.Id_cc07b314_4582_4c2c_84b8_bb32f59fc09b;
+
+namespace T3.Operators.Types.Id_c30ba288_9e40_4636_beb5_68401d91fe37
+{
+    public class DampFloatList : Instance<DampFloatList>
+    {
+        [Output(Guid = "23c867c8-d175-463f-bcaa-18e6be5f20c2", DirtyFlagTrigger = DirtyFlagTrigger.Animated)]
+        public readonly Slot<List<float>> Result = new();
+
+        private const float MinTimeElapsedBeforeEvaluation = 1 / 1000f;
+
+        public DampFloatList()
+        {
+            Result.UpdateAction = Update;
+        }
+
+        private void Update(EvaluationContext context)
+        {
+            var inputList = Values.GetValue(context); //get input list - within the loop, cycle between index points in the list
+            var damping = Damping.GetValue(context); //get damping amount - doesn't need to be repeated
+
+            if (inputList.Count == 0)
+            {
+                return;
+            }
+
+            //timing stuff, doesn't need to be repeated
+            var currentTime = UseAppRunTime.GetValue(context) ? Playback.RunTimeInSecs : context.LocalFxTime; 
+            if (Math.Abs(currentTime - _lastEvalTime) < MinTimeElapsedBeforeEvaluation)
+                return;
+            
+            //no idea what this is for, best not to touch
+            if (context.IntVariables.TryGetValue("__MotionBlurPass", out var motionBlurPass))
+            {
+                if (motionBlurPass > 0)
+                {
+                    //Log.Debug($"Skip motion blur pass {motionBlurPass}");
+                    return;
+                }                
+            } 
+
+            _lastEvalTime = currentTime; //don't repeat
+
+            var method = (DampFunctions.Methods)Method.GetValue(context).Clamp(0, 1); //don't repeat
+            //_dampedValue = DampFunctions.DampenFloat(inputValue, _dampedValue, damping, ref _velocity, method); //do the damping - repeat this
+
+            //Log.Debug("List length: " + inputList.Count);
+
+            for(var i = 0; i < inputList.Count; i++)
+            {
+                //Log.Debug("Current index: " + i.ToString());
+
+                if (i > _dampedValues.Count-1)
+                {
+                    //Log.Debug("Index does not exist in output list, appending...");
+                    _dampedValues.Add(0);
+                }
+                
+                var value = inputList[i];
+                var dampedValue = DampFunctions.DampenFloat(value, _dampedValues[i], damping, ref _velocity, method);
+
+                MathUtils.ApplyDefaultIfInvalid(ref dampedValue, 0); 
+                MathUtils.ApplyDefaultIfInvalid(ref _velocity, 0);
+
+                _dampedValues[i] = dampedValue;
+            }
+
+            Result.Value = _dampedValues; //do for list
+        }
+
+        private List<float> _dampedValues = new List<float>(1);
+        private float _velocity;
+        private double _lastEvalTime;
+        
+        [Input(Guid = "491cc9cd-28fc-4ec4-8d98-5a7e0d17082a")]
+        public readonly InputSlot<List<float>> Values = new(new List<float>(20));
+
+        [Input(Guid = "3d63df3d-81c0-4e27-ae0c-acd3092d952c")]
+        public readonly InputSlot<float> Damping = new();
+        
+        [Input(Guid = "ad97ae08-9aeb-4ed5-b5b1-e24d9af21cf7", MappedType = typeof(DampFunctions.Methods))]
+        public readonly InputSlot<int> Method = new();
+        
+        [Input(Guid = "51bbb9f9-2895-4965-9f73-34d7dbae2ad1")]
+        public readonly InputSlot<bool> UseAppRunTime = new();
+
+    }
+}
