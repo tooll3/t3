d0f091537e414b6804fdcb88e11a48651826a36c
1x <dummy@irgendwas.net>
2024-02-14 23:15:34 +0100

    Add [HandsOn002BrickOcean] & Asset & HandsOn001Abstract.cs

---
+++ Operators/Types/user/1x/HandsOn002BrickOcean.cs
@@ -0,0 +1,16 @@
+using T3.Core.DataTypes;
+using T3.Core.Operator;
+using T3.Core.Operator.Attributes;
+using T3.Core.Operator.Slots;
+
+namespace T3.Operators.Types.Id_e21c202f_9b93_456d_aeba_7232d9600572
+{
+    public class HandsOn002BrickOcean : Instance<HandsOn002BrickOcean>
+    {
+        [Output(Guid = "845ab9b2-acd4-413a-9f46-ba31947e42ae")]
+        public readonly Slot<Command> Output = new Slot<Command>();
+
+
+    }
+}
+
