54d5a1117251470c56140cc9f1b04433a37d56b4
1x-vision.net <1x@1x-vision.net>
2024-02-17 22:23:54 +0100

    Add [LookTest] scenes

---
+++ Operators/Types/user/1x/LookTest08.cs
@@ -0,0 +1,16 @@
+using T3.Core.DataTypes;
+using T3.Core.Operator;
+using T3.Core.Operator.Attributes;
+using T3.Core.Operator.Slots;
+
+namespace T3.Operators.Types.Id_5b81c4e6_1320_4d08_987f_a1d83ff62f4d
+{
+    public class LookTest08 : Instance<LookTest08>
+    {
+        [Output(Guid = "a24fd312-8d48-4bc4-8b47-ae4aaa0608e0")]
+        public readonly Slot<Command> Output = new Slot<Command>();
+
+
+    }
+}
+
