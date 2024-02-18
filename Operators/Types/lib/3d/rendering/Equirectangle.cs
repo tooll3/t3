7be63a1b4fe0c561368120d1bd38c86ecb698b23
Leon <leon.denise@free.fr>
2024-02-18 14:55:26 +0100

    enum resolution for Equirectangle

--- Operators/Types/lib/3d/rendering/Equirectangle.cs
+++ Operators/Types/lib/3d/rendering/Equirectangle.cs
@@ -14,13 +14,18 @@ namespace T3.Operators.Types.Id_9d71d46c_f3d8_4bf4_a104_38c0b37cc88b
         [Input(Guid = "2097d7c0-604b-4909-8cf1-ca9793dc53ec")]
         public readonly InputSlot<T3.Core.DataTypes.Command> InputCommand = new InputSlot<T3.Core.DataTypes.Command>();
 
-        [Input(Guid = "9e35ef34-0b02-40f6-93d5-5163346d681a")]
+        [Input(Guid = "9e35ef34-0b02-40f6-93d5-5163346d681a", MappedType = typeof(Resolutions))]
         public readonly InputSlot<int> Dimension = new InputSlot<int>();
 
         [Output(Guid = "bbc85c64-3d0a-47ce-8126-9f90d4b60fac")]
         public readonly Slot<SharpDX.Direct3D11.Texture2D> OutputDepth = new Slot<SharpDX.Direct3D11.Texture2D>();
 
-
+        private enum Resolutions
+        {
+            _512 = 512,
+            _1024 = 1024,
+            _2048 = 2048
+        }
     }
 }
 
