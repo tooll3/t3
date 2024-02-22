5b4a6eef65861a259b6b9a73a758ca25ceeb1be3
newemka <newemka@gmail.com>
2024-02-22 18:40:21 +0100

    Use shared BlendModes + example

--- Resources/lib/points/modify/SamplePointColorAttributes.hlsl
+++ Resources/lib/points/modify/SamplePointColorAttributes.hlsl
@@ -1,6 +1,6 @@
 #include "lib/shared/point.hlsl"
 #include "lib/shared/quat-functions.hlsl"
-//#include "lib/shared/blend-functions.hlsl"
+#include "lib/shared/blend-functions.hlsl"
 
 
 cbuffer Params : register(b0)
@@ -10,7 +10,6 @@ cbuffer Params : register(b0)
     float3 Center;
     float Mode;
     float4 BaseColor;
-    float Mix;
       
 }
 
@@ -41,22 +40,8 @@ sampler texSampler : register(s0);
     float4 c = inputTexture.SampleLevel(texSampler, posInObject.xy * float2(1, -1) + float2(0.5, 0.5), 0.0);
     c *=BaseColor;
 
-    float4 tA = p.Color;
-    float4 tB = c;
-    float gray = (c.r + c.g + c.b) / 3;
-    float3 rgbNormalBlended = (1.0 - tB.a) * tA.rgb + tB.a * tB.rgb;
-    if (Mode < 0.5){
-        p.Color = float4(rgbNormalBlended,1);
-    }
-    else if (Mode < 1.5){
-        p.Color = float4(1 - (1 - tA.rgb) * (1 - tB.rgb * tB.a),1);
-    }
-    else if (Mode < 2.5){
-        c = float4(lerp(tA.rgb, tA.rgb * tB.rgb, tB.a),1);
-        p.Color = c;
-         
-    }
-    
+      
+    p.Color = BlendColors(p.Color, c, (int)Mode);
 
                                      
     ResultPoints[index] = p;
