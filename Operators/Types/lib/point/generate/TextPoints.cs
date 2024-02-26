ac2e33b483e06e5282c1db0c55bdb3f80653e2e5
Thomas Mann <thomas.mann@framefield.com>
2024-02-26 23:46:03 +0100

    Clean-up code formatting of [TextPoints]

--- Operators/Types/lib/point/generate/TextPoints.cs
+++ Operators/Types/lib/point/generate/TextPoints.cs
@@ -4,11 +4,9 @@ using T3.Core.Operator.Slots;
 using T3.Core.DataTypes;
 using T3.Core.Logging;
 using PointT3 = T3.Core.DataTypes.Point;
-
 using System.IO;
 using System.Collections.Generic;
 using System.Numerics;
-
 using SixLabors.Fonts;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Drawing;
@@ -17,7 +15,6 @@ namespace T3.Operators.Types.Id_bdb41a6d_e225_4a8a_8348_820d45153e3f
 {
     public class TextPoints : Instance<TextPoints>
     {
-
         [Input(Guid = "7432f063-9957-47f7-8250-c3e6456ec7c6")]
         public readonly InputSlot<string> InputText = new InputSlot<string>();
 
@@ -36,18 +33,11 @@ namespace T3.Operators.Types.Id_bdb41a6d_e225_4a8a_8348_820d45153e3f
         [Input(Guid = "e2cff4bc-6c40-40f1-95a1-9e48d0c8624f", MappedType = typeof(TextAligns))]
         public readonly InputSlot<int> TextAlign = new InputSlot<int>();
 
-        [Input(Guid = "02163610-108e-4154-a3b2-84b65df852fa", MappedType =  typeof(VerticalAlignment))]
+        [Input(Guid = "02163610-108e-4154-a3b2-84b65df852fa", MappedType = typeof(VerticalAlignment))]
         public readonly InputSlot<int> VerticalAlign = new InputSlot<int>();
 
-        [Input(Guid = "128bd413-10ab-4c2c-9c49-3de925c26f02", MappedType =  typeof(HorizontalAlignment))]
+        [Input(Guid = "128bd413-10ab-4c2c-9c49-3de925c26f02", MappedType = typeof(HorizontalAlignment))]
         public readonly InputSlot<int> HorizontalAlign = new InputSlot<int>();
-
-        public enum TextAligns
-        {
-            Left,
-            Right,
-            Center,
-        }
 
         public TextPoints()
         {
@@ -56,84 +46,91 @@ namespace T3.Operators.Types.Id_bdb41a6d_e225_4a8a_8348_820d45153e3f
 
         private void Update(EvaluationContext context)
         {
-            string inputText = InputText.GetValue(context);
-            string inputFont = InputFont.GetValue(context);
+            var inputText = InputText.GetValue(context);
+            var inputFont = InputFont.GetValue(context);
 
-            // check if text and font are not empty
+            // Check if text and font are not empty
             if (string.IsNullOrEmpty(inputText) || string.IsNullOrEmpty(inputFont))
             {
                 return;
             }
 
-            // check if font file exist
+            // Check if font file exist
             if (!File.Exists(inputFont))
             {
                 Log.Debug($"File {inputFont} doesn't exist", this);
                 return;
             }
 
-            // fetch parameters
-            float size = Size.GetValue(context);
-            float resolution = Resolution.GetValue(context);
-            float lineSpace = LineHeight.GetValue(context);
-            TextAlignment textAlign = (TextAlignment)TextAlign.GetValue(context);
-            VerticalAlignment vertical = (VerticalAlignment)VerticalAlign.GetValue(context);
-            HorizontalAlignment horizontal = (HorizontalAlignment)HorizontalAlign.GetValue(context);
+            // Fetch parameters
+            var size = Size.GetValue(context);
+            var resolution = Resolution.GetValue(context);
+            var appliedSize = size / resolution;
+            var lineSpace = LineHeight.GetValue(context);
+            var textAlign = (TextAlignment)TextAlign.GetValue(context);
+            var vertical = (VerticalAlignment)VerticalAlign.GetValue(context);
+            var horizontal = (HorizontalAlignment)HorizontalAlign.GetValue(context);
 
-            // create font
+            // Create font
             FontCollection collection = new();
-            FontFamily family = collection.Add(inputFont);
-            Font font = family.CreateFont(resolution);
-            TextOptions textOptions = new(font)
-            {
-                TextAlignment = textAlign,
-                VerticalAlignment = vertical,
-                HorizontalAlignment = horizontal,
-                LineSpacing = lineSpace,
-            };
+            var family = collection.Add(inputFont);
+            var font = family.CreateFont(resolution);
 
-            // generate points wiht text from font
-            IPathCollection paths = TextBuilder.GenerateGlyphs(inputText, textOptions);
-            List<PointT3> points = new List<PointT3>();
-            foreach (var path in paths)
+            TextOptions textOptions = new(font)
+                                          {
+                                              TextAlignment = textAlign,
+                                              VerticalAlignment = vertical,
+                                              HorizontalAlignment = horizontal,
+                                              LineSpacing = lineSpace,
+                                          };
+
+            // Generate points with text from font
+            var points = new List<PointT3>();
+            var glyphs = TextBuilder.GenerateGlyphs(inputText, textOptions);
+
+            foreach (var glyph in glyphs)
             {
-                var p = path.Flatten();
-                foreach (var q in p)
+
+                var simplePaths = glyph.Flatten();
+                foreach (var simplePath in simplePaths)
                 {
-                    // fill list with points from glyph (and close the path)
-                    int count = q.Points.Length;
-                    for (int i = 0; i < count+1; ++i)
+                    // Fill list with points from glyph (and close the path)
+                    var count = simplePath.Points.Length;
+                    const int offsetForClosedPolygons = 1;
+                    for (var pointIndex = 0; pointIndex < count + offsetForClosedPolygons; ++pointIndex)
                     {
-                        points.Add(GetPoint(q.Points.Span[i%count], size/resolution));
+                        var pointF = simplePath.Points.Span[pointIndex % count];
+                        points.Add(new PointT3
+                                       {
+                                           Position = new Vector3(pointF.X * appliedSize, -pointF.Y * appliedSize, 0f),
+                                           W = 1f,
+                                           Orientation = new Quaternion(0f, 0f, 0f, 1f),
+                                           Color = new Vector4(1f, 1f, 1f, 1f)
+                                       });
                     }
 
-                    // add line separator
+                    // Add line separator
                     points.Add(PointT3.Separator());
                 }
             }
 
-            // no points
-            if (points.Count == 0) return;
+            if (points.Count == 0)
+                return;
 
-            // output list
-            StructuredList<PointT3> list = new StructuredList<PointT3>(points.Count);
-            for (int index = 0; index < points.Count; index++) list.TypedElements[index] = points[index];
+            // Output list
+            var list = new StructuredList<PointT3>(points.Count);
+            for (var index = 0; index < points.Count; index++) list.TypedElements[index] = points[index];
             OutputList.Value = list;
         }
 
-        PointT3 GetPoint (PointF p, float size)
+        private enum TextAligns
         {
-            return new PointT3
-            {
-                Position = new Vector3(p.X * size, -p.Y * size, 0f),
-                W = 1f,
-                Orientation = new Quaternion(0f, 0f, 0f, 1f),
-                Color = new Vector4(1f, 1f, 1f, 1f)
-            };
+            Left,
+            Right,
+            Center,
         }
 
         [Output(Guid = "c65da6e8-3eb7-4152-9b79-34fcaaa31807")]
         public readonly Slot<StructuredList> OutputList = new Slot<StructuredList>();
     }
 }
-
