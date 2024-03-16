240f44fbdd8b10c61c5b184b2b6b1f4169114e2d
newemka <newemka@gmail.com>
2024-03-16 20:43:35 +0100

    DefromMesh adding TwistPivot and Taper works with selected verts

--- Resources/lib/3d/mesh/mesh-Deform.hlsl
+++ Resources/lib/3d/mesh/mesh-Deform.hlsl
@@ -1,144 +1,119 @@
 #include "lib/shared/hash-functions.hlsl"
 #include "lib/shared/noise-functions.hlsl"
-#include "lib/shared/point.hlsl"
-#include "lib/shared/quat-functions.hlsl"
 #include "lib/shared/pbr.hlsl"
 
-cbuffer Transforms : register(b0)
+cbuffer Params : register(b0)
 {
-    float4x4 CameraToClipSpace;
-    float4x4 ClipSpaceToCamera;
-    float4x4 WorldToCamera;
-    float4x4 CameraToWorld;
-    float4x4 WorldToClipSpace;
-    float4x4 ClipSpaceToWorld;
-    float4x4 ObjectToWorld;
-    float4x4 WorldToObject;
-    float4x4 ObjectToCamera;
-    float4x4 ObjectToClipSpace;
-};
-
-cbuffer Params : register(b1)
-{
-    float Amount;
-    float Frequency;
-    float Phase;
-    float Variation;
-
-    float3 AmountDistribution;
-    float RotationLookupDistance;
-
-    float UseWAsWeight;
-    float Space;
-    float Direction;
-    float OffsetDirection;
-
     float UseVertexSelection;
-    float twistAxis;
+    float Spherize;
+    float Radius;
+    float TaperAmount; // New parameter for tapering
 
+    float TwistAmount; // New parameter for twisting
+    float TaperAxis;
+    float TwistAxis;
+    float __padding;
+
+    float3 Pivot;
+    float __padding1;
+    float3 TwistPivot;
+    float AbsTaper;
 }
 
-StructuredBuffer<PbrVertex> SourceVertices : t0;        
-RWStructuredBuffer<PbrVertex> ResultVertices : u0;   
+StructuredBuffer<PbrVertex> SourceVerts : t0;
+RWStructuredBuffer<PbrVertex> ResultVerts : u0;
 
-float3 GetNoise(float3 pos, float3 variation) 
+inline float3 NormalizeToSphere(float3 position, float targetRadius)
 {
-    float3 noiseLookup = (pos * 0.91 + variation) * Frequency +  Phase ;
-    float3 noise = snoiseVec3(noiseLookup);
-    return (noise + OffsetDirection) * Amount/100 * AmountDistribution ;
+    float3 posWithPivot = position - Pivot; // Not sure why this isn't working...
+    float currentRadius = length(posWithPivot);
+    return posWithPivot * (targetRadius / currentRadius);
 }
 
-float3 twistFunction(float3 position, float twistAmount)
+// Tapering function based on selected axis
+inline float TaperFunction(float y, float taperAmount)
+{
+    return 1.0 - taperAmount * y;
+}
+
+
+inline float3 TwistFunction(float3 position, float twistAmount)
 {
     float angle;
     float cosA;
     float sinA;
     float3 twisted;
-    switch ((int)twistAxis)
+    float3 posWithPivot = position - TwistPivot; 
+    switch ((int)TwistAxis)
     {
-        case 0:
-         angle = position.x * twistAmount;
-         cosA = cos(angle);
-         sinA = sin(angle);
-         twisted = float3(position.x, position.y * cosA - position.z * sinA,  position.y * sinA + position.z * cosA);
-    break;
-     case 1:
-         angle = position.y * twistAmount;
-         cosA = cos(angle);
-         sinA = sin(angle);
-         twisted = float3(position.x * cosA - position.z * sinA, position.y, position.x * sinA + position.z * cosA);
-    break;
-     case 2:
-         angle = position.z * twistAmount;
-         cosA = cos(angle);
-         sinA = sin(angle);
-         twisted = float3(position.x * cosA - position.y * sinA, position.x * sinA + position.y * cosA, position.z);
-    break;
+    case 0:
+        angle = posWithPivot.x * twistAmount;
+        cosA = cos(angle);
+        sinA = sin(angle);
+        twisted = float3(posWithPivot.x, posWithPivot.y * cosA - posWithPivot.z * sinA, posWithPivot.y * sinA + posWithPivot.z * cosA);
+        break;
+    case 1:
+        angle = posWithPivot.y * twistAmount;
+        cosA = cos(angle);
+        sinA = sin(angle);
+        twisted = float3(posWithPivot.x * cosA - posWithPivot.z * sinA, posWithPivot.y, posWithPivot.x * sinA + posWithPivot.z * cosA);
+        break;
+    case 2:
+        angle = posWithPivot.z * twistAmount;
+        cosA = cos(angle);
+        sinA = sin(angle);
+        twisted = float3(posWithPivot.x * cosA - posWithPivot.y * sinA, posWithPivot.x * sinA + posWithPivot.y * cosA, posWithPivot.z);
+        break;
     }
-    return twisted;
+    return (twisted + TwistPivot);
 }
 
-static float3 variationOffset;
-
-[numthreads(64,1,1)]
-void main(uint3 i : SV_DispatchThreadID)
+[numthreads(64, 1, 1)] void main(uint3 i : SV_DispatchThreadID)
 {
     uint numStructs, stride;
-    SourceVertices.GetDimensions(numStructs, stride);
-    if(i.x >= numStructs) {
+    SourceVerts.GetDimensions(numStructs, stride);
+    if (i.x >= numStructs)
+    {
         return;
     }
 
-    float3 variationOffset = hash31((float)(i.x%1234)/0.123 ) * Variation;
+    float s = UseVertexSelection > 0.5 ? SourceVerts[i.x].Selected : 1;
 
-    PbrVertex v = SourceVertices[i.x];
-    ResultVertices[i.x] = SourceVertices[i.x];
+    float3 pos = SourceVerts[i.x].Position;
 
-    float weight = 1;
-    float3 offset;
+    float3 twisted = SourceVerts[i.x].Position; // for twist
 
-    float3 posInWorld = v.Position;
- 
-    if(Space< 0.5) {
-        posInWorld = float3(v.TexCoord.xy, 0);
-    }
-    else if(Space < 1.5){
-        posInWorld = mul(float4(posInWorld ,1), ObjectToWorld).xyz;
-    }
+    // Normalize the position to be on a sphere with a desired radius
+    float3 spherePos = NormalizeToSphere(pos, Radius);
+    pos = lerp(pos, lerp(pos, spherePos, Spherize), s);
+    float3 tapered = pos;
 
-    //offset = GetNoise(posInWorld, variationOffset) * weight;
-    offset = twistFunction(posInWorld, Amount) ;
-    //offset += offset * Amount/100 * 2;
-
-    if(Direction > 0.5)
+    // Apply taper transformation
+    switch ((int)TaperAxis)
     {
-        float3x3 TBN = float3x3(v.Tangent, v.Bitangent, v.Normal);
-        TBN = mul(TBN, (float3x3)ObjectToWorld);
-        offset = mul(offset,TBN);
+    case 0:
+        tapered.yz *= TaperFunction(pos.x, TaperAmount);
+        break;
+    case 1:
+        tapered.xz *= TaperFunction(pos.y, TaperAmount);
+        break;
+    case 2:
+        tapered.xy *= TaperFunction(pos.z, TaperAmount);
+        break;
     }
+    pos = lerp(pos, tapered, s);
 
-    float selection = UseVertexSelection > 0.5 ? v.Selected : 1;
-    offset *= selection;
+    //  Apply twist transformation 
+    twisted = TwistFunction(pos, TwistAmount) ;
 
-    float3 newPos = posInWorld + offset;
-    float lookUpDistance = RotationLookupDistance / Frequency;
+    // Results
+    ResultVerts[i.x].Position = lerp(pos, twisted, s);
 
-    float3 tAnchor = posInWorld + v.Tangent * lookUpDistance;
-    float3 tAnchor2 = posInWorld - v.Tangent * lookUpDistance;
+    ResultVerts[i.x].Normal = SourceVerts[i.x].Normal;
+    ResultVerts[i.x].Tangent = SourceVerts[i.x].Tangent;
+    ResultVerts[i.x].Bitangent = SourceVerts[i.x].Bitangent;
 
-    weight *= selection;
-    float3 newTangent  = normalize( tAnchor + twistFunction(tAnchor, Amount) * weight - newPos);
-    float3 newTangent2  = -normalize( tAnchor2 + twistFunction(tAnchor2, Amount) * weight - newPos);
-    ResultVertices[i.x].Tangent = lerp(newTangent, newTangent2, 0.5);
+    ResultVerts[i.x].TexCoord = SourceVerts[i.x].TexCoord;
 
-    float3 bAnchor = posInWorld + v.Bitangent * lookUpDistance;
-    float3 bAnchor2 = posInWorld - v.Bitangent * lookUpDistance;
-
-    float3 newBitangent  = normalize( bAnchor + twistFunction(bAnchor, Amount) * weight - newPos);
-    float3 newBitangent2  = -normalize( bAnchor2 + twistFunction(bAnchor2, Amount) * weight - newPos);
-    ResultVertices[i.x].Bitangent = lerp(newBitangent, newBitangent2, 0.5);
-
-    ResultVertices[i.x].Normal = cross(ResultVertices[i.x].Tangent, ResultVertices[i.x].Bitangent);
-    ResultVertices[i.x].Position =  offset;
+    ResultVerts[i.x].Selected = SourceVerts[i.x].Selected;
 }
-
