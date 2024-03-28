70ef8a5c7ec9a53319d7567dea24e91070d7035f
ylmrx <ylmrx@users.noreply.github.com>
2024-03-28 16:31:11 +0100

    rebasing...

--- Operators/Types/lib/io/osc/OscOutput.cs
+++ Operators/Types/lib/io/osc/OscOutput.cs
@@ -11,28 +11,42 @@ namespace T3.Operators.Types.Id_4e99da86_482f_4037_8664_b2371526d632
 {
     public class OscOutput : Instance<OscOutput>
     {
-        private bool _connected;
-        private IPAddress _address;
-        private int _port;
-        private float _num_value;
-        private string _str_value;
-        private OscSender _sender;
+        [Input(Guid = "98f38caa-4f79-425c-ab46-22d7dbe62978")]
+        public readonly InputSlot<string> IpAddress = new InputSlot<string>();
+
+        [Input(Guid = "6c0e07ba-7ea6-4dab-af0b-61cf9cb74ad7")]
+        public readonly InputSlot<int> Port = new InputSlot<int>();
+
+        [Input(Guid = "9016e418-7761-4916-aafb-c95599f77f38")]
+        public readonly InputSlot<string> Location = new InputSlot<string>();
+
+        [Input(Guid = "d5e9e9be-093b-4d57-9070-9b2cf33aa45b")]
+        public readonly InputSlot<string> String = new InputSlot<string>();
+
+        [Input(Guid = "827c7ae2-c129-4d04-a715-328c0a86bf8a")]
+        public readonly InputSlot<float> Number = new InputSlot<float>();
+
+        [Input(Guid = "4931217b-e30d-42a6-9cda-bc49a8fd8d67", MappedType = typeof(OscTypes))]
+        public readonly InputSlot<int> OscValueType = new ();
+
+        [Output(Guid = "a6679d6c-fc34-4588-ab20-5079ad8f8a03")]
         public OscOutput()
         {
             Result.UpdateAction = Update;
         }
 
-        private enum OscType
+        private enum OscTypes
         {
             Number,
             String
         }
+
         private void Update(EvaluationContext context)
         {
             var location = Location.GetValue(context);
             var num = Number.GetValue(context);
             var str = String.GetValue(context);
-            var oscType = OscValueType.GetEnumValue<OscType>(context);
+            var oscType = OscValueType.GetEnumValue<OscTypes>(context);
 
             CheckAddr(context);
             if (OscAddress.IsValidAddressPattern(location) == false)
@@ -42,7 +56,7 @@ namespace T3.Operators.Types.Id_4e99da86_482f_4037_8664_b2371526d632
             }
             switch (oscType)
             {
-                case OscType.Number:
+                case OscTypes.Number:
                     if (num != _num_value)
                     {
                         // Log.Info("Sending:" + num);
@@ -52,11 +66,11 @@ namespace T3.Operators.Types.Id_4e99da86_482f_4037_8664_b2371526d632
                             _sender.Send(message);
                             _num_value = num;
                         }
-                        else Log.Error("failed to build the OSC message (float): " + message.ErrorMessage);
+                        else Log.Error("Failed to build the OSC message (float): " + message.ErrorMessage);
                     }
                     break;
 
-                case OscType.String:
+                case OscTypes.String:
                     if (str != _str_value)
                     {
                         // Log.Info("Sending:" + str);
@@ -66,7 +80,7 @@ namespace T3.Operators.Types.Id_4e99da86_482f_4037_8664_b2371526d632
                             _sender.Send(message);
                             _str_value = str;
                         }
-                        else Log.Error("failed to build the OSC message (string): " + message.ErrorMessage);
+                        else Log.Error("Failed to build the OSC message (string): " + message.ErrorMessage);
                     }
                     break;
             }
@@ -77,7 +91,7 @@ namespace T3.Operators.Types.Id_4e99da86_482f_4037_8664_b2371526d632
             IPAddress address;
             try
             {
-                address = IPAddress.Parse(Address.GetValue(context));
+                address = IPAddress.Parse(IpAddress.GetValue(context));
             }
             catch (System.FormatException e)
             {
@@ -117,26 +131,12 @@ namespace T3.Operators.Types.Id_4e99da86_482f_4037_8664_b2371526d632
             _connected = false;
         }
 
-        [Input(Guid = "98f38caa-4f79-425c-ab46-22d7dbe62978")]
-        public readonly InputSlot<string> Address = new InputSlot<string>();
-
-        [Input(Guid = "6c0e07ba-7ea6-4dab-af0b-61cf9cb74ad7")]
-        public readonly InputSlot<int> Port = new InputSlot<int>();
-
-
-        [Input(Guid = "9016e418-7761-4916-aafb-c95599f77f38")]
-        public readonly InputSlot<string> Location = new InputSlot<string>();
-
-        [Input(Guid = "d5e9e9be-093b-4d57-9070-9b2cf33aa45b")]
-        public readonly InputSlot<string> String = new InputSlot<string>();
-
-        [Input(Guid = "827c7ae2-c129-4d04-a715-328c0a86bf8a")]
-        public readonly InputSlot<float> Number = new InputSlot<float>();
-
-        [Input(Guid = "4931217b-e30d-42a6-9cda-bc49a8fd8d67", MappedType = typeof(OscType))]
-        public readonly InputSlot<int> OscValueType = new ();
-
-        [Output(Guid = "a6679d6c-fc34-4588-ab20-5079ad8f8a03")]
         public readonly Slot<T3.Core.DataTypes.Command> Result = new Slot<T3.Core.DataTypes.Command>();
+        private bool _connected;
+        private IPAddress _address;
+        private int _port;
+        private float _num_value;
+        private string _str_value;
+        private OscSender _sender;
     }
 }
