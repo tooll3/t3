861b73b7485b6b7ddda53f137c9382536829d201
Dom <domportera@gmail.com>
2024-04-13 03:38:03 -0400

    file tables!!

--- SilkWindows/Implementations/FileManager/ItemDrawers/FileDrawer.cs
+++ SilkWindows/Implementations/FileManager/ItemDrawers/FileDrawer.cs
@@ -5,23 +5,35 @@ namespace SilkWindows.Implementations.FileManager.ItemDrawers;
 // ReSharper disable once ClassWithVirtualMembersNeverInherited.Global
 internal class FileDrawer : FileSystemDrawer
 {
-    internal sealed override string DisplayName => _displayName;
+    internal sealed override string DisplayName => _file.Name;
     internal sealed override string Path => _file.FullName;
     internal sealed override bool IsDirectory => false;
     public sealed override bool IsReadOnly => ParentDirectoryDrawer!.IsReadOnly;
     
-    private readonly string _displayName;
-    private readonly string _displayText;
     private readonly FileInfo _file;
+    private readonly string _relativePath;
+    private readonly string _extensionDisplay;
+    private readonly string _lastModifiedDisplay;
     
     public FileDrawer(IFileManager fileManager, FileInfo file, DirectoryDrawer parent) : base(fileManager, parent)
     {
-        _displayName = file.Name;
         _file = file;
-        _displayText = file.Extension == "" ? _displayName : $"[{file.Extension}] {_displayName}";
+        _extensionDisplay = '[' + file.Extension + ']';
         
-        if(ParentDirectoryDrawer == null)
+        if (ParentDirectoryDrawer == null)
             throw new InvalidOperationException("File drawer must have a parent directory drawer");
+        
+        var topParent = ParentDirectoryDrawer;
+        while (topParent.ParentDirectoryDrawer != null)
+            topParent = topParent.ParentDirectoryDrawer;
+        
+        var topParentDisplayName = topParent.DisplayName;
+        var topParentPath = topParent.Path;
+        var relativePath = System.IO.Path.GetRelativePath(topParentPath, Path);
+        relativePath = System.IO.Path.Combine(topParentDisplayName, relativePath);
+        _relativePath = fileManager.FormatPathForDisplay(relativePath);
+        
+        _lastModifiedDisplay = file.LastWriteTime.ToShortDateString();
     }
     
     protected override void DrawSelectable(ImFonts fonts, bool isSelected)
@@ -43,7 +55,7 @@ internal class FileDrawer : FileSystemDrawer
     /// </summary>
     protected virtual void DrawBasic(bool isCurrentlySelected, ImFonts fonts)
     {
-        ImGui.Selectable(_displayText, isCurrentlySelected);
+        ImGui.Selectable(_file.Name, isCurrentlySelected);
     }
     
     /// <summary>
@@ -70,6 +82,25 @@ internal class FileDrawer : FileSystemDrawer
     
     protected override void DrawTooltip(ImFonts fonts)
     {
+        ImGui.Text(_relativePath);
+    }
+    
+    protected sealed override FileSystemInfo FileSystemInfo => _file;
+    
+    public void DrawFileExtension(ImFonts fonts)
+    {
+        ImGui.PushFont(fonts.Small);
+        ImGui.Text(_extensionDisplay);
+        if (ImGui.BeginItemTooltip())
+        {
+            ImGui.Text("File Extension: " + _extensionDisplay);
+            ImGui.EndTooltip();
+        }
+        ImGui.PopFont();
+    }
+    
+    public void DrawSize(ImFonts fonts)
+    {
         const double kb = 1024;
         const double mb = kb * 1024;
         const double toKb = 1 / kb;
@@ -77,14 +108,27 @@ internal class FileDrawer : FileSystemDrawer
         var sizeInBytes = _file.Length;
         
         var showInMb = sizeInBytes > mb;
-        ImGui.Text("size: ");
-        ImGui.SameLine();
-        ImGui.Text(showInMb ? $"{sizeInBytes * toMb: 0.0} MB" : $"{sizeInBytes * toKb: 0.0} KB");
-        
+        var display = showInMb ? $"{sizeInBytes * toMb: 0.0} MB" : $"{sizeInBytes * toKb: 0.0} KB";
         ImGui.PushFont(fonts.Small);
-        ImGui.Text("Last modified: " + FileSystemInfo.LastWriteTime.ToShortDateString());
+        ImGui.Text(display);
+        if (ImGui.BeginItemTooltip())
+        {
+            ImGui.Text("Size: " + display);
+            ImGui.EndTooltip();
+        }
         ImGui.PopFont();
     }
     
-    protected sealed override FileSystemInfo FileSystemInfo => _file;
+    public void DrawLastModified(ImFonts fonts)
+    {
+        ImGui.PushFont(fonts.Small);
+        ImGui.Text(_lastModifiedDisplay);
+        
+        if (ImGui.BeginItemTooltip())
+        {
+            ImGui.Text("Time last modified: " + _lastModifiedDisplay);
+            ImGui.EndTooltip();
+        }
+        ImGui.PopFont();
+    }
 }
