c1ef77f9c82115e2e1cf25cc333b3d8d9b0bc35a
Dom <domportera@gmail.com>
2024-04-19 23:19:21 -0400

    maybe a more coherent abstraction? at least abstracted enough for the vulkan implementation

--- ImguiWindows/IImguiImplementation.cs
+++ ImguiWindows/IImguiImplementation.cs
@@ -1,13 +1,15 @@
 using System.Drawing;
 using Silk.NET.Core.Native;
+using Silk.NET.Input;
 using Silk.NET.Maths;
 using Silk.NET.Windowing;
 
-namespace SilkWindows;
+namespace ImguiWindows;
 
 public interface IWindowImplementation
 {
     public WindowOptions WindowOptions { get; }
+    Color DefaultClearColor { get; }
     
     /// <summary>
     /// This function is called first and is used to set up your rendering context within your window, which was created
@@ -15,40 +17,47 @@ public interface IWindowImplementation
     /// </summary>
     /// <param name="window"></param>
     /// <returns></returns>
-    NativeAPI? InitializeGraphicsAndInputContexts(IWindow window);
+    NativeAPI? InitializeGraphicsAndInputContexts(IWindow window, out IInputContext inputContext);
     
     /// <summary>
-    /// This is called first in the rendering process - used to clear the frame with a specific color.
+    /// This is called first in the rendering process - used to clear the frame with a specific color and set up the frame.
     /// </summary>
-    /// <param name="color"></param>
-    public void ClearFrame(in Color color);
+    /// <param name="clearColor"></param>
+    /// <param name="deltaTime">The time between frames</param>
+    /// <returns>True if rendering should proceed</returns>
+    public bool Render(in Color clearColor, double deltaTime);
     
-    /// <summary>
-    /// This is called to prime your implementation for rendering a new frame, after the frame has been cleared.
-    /// </summary>
-    /// <param name="deltaTime"></param>
-    public void StartUpdate(float deltaTime);
     
     /// <summary>
     /// This is called after all draw commands have been issued, and is used to do the final rendering step of the Imgui frame.
     /// </summary>
-    public void Render();
+    public void EndRender();
     
     /// <summary>
     /// Dispose of any resources that need to be disposed of.
     /// </summary>
     public void Dispose();
     
+    public IImguiImplementation GetImguiImplementation();
+    
+    void OnWindowResize(Vector2D<int> size);
+}
+
+public interface IImguiImplementation
+{
+    public string Title { get; }
+    
     /// <summary>
     /// Returns the imgui context pointer
     /// </summary>
-    IntPtr InitializeControllerContext();
+    IntPtr InitializeControllerContext(Action onConfigureIO);
     
-    void ResizeGraphicsContext(Vector2D<int> size);
-}
-public interface IImguiImplementation: IWindowImplementation
-{
-    public string MainWindowId => $"{WindowOptions.Title}##{Interlocked.Increment(ref _windowCounter)}";
-    public string ChildWindowId => $"{WindowOptions.Title}##{Interlocked.Increment(ref _windowCounter)}";
+    public void StartImguiFrame(float deltaSeconds);
+    public void EndImguiFrame();
+    
+    public void Dispose();
+    
+    public string MainWindowId => $"{Title}##{Interlocked.Increment(ref _windowCounter)}";
+    public string ChildWindowId => $"{Title}##{Interlocked.Increment(ref _windowCounter)}";
     private static int _windowCounter = 999;
 }
