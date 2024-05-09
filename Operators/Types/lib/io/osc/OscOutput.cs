8fe08eb52b3801af713273451724d089462d26ac
Thomas Mann <thomas.mann@framefield.com>
2024-05-09 11:34:40 +0200

    Minor cleanup of OscOutput

--- Operators/Types/lib/io/osc/OscOutput.cs
+++ Operators/Types/lib/io/osc/OscOutput.cs
@@ -12,25 +12,25 @@ namespace T3.Operators.Types.Id_4e99da86_482f_4037_8664_b2371526d632
     public class OscOutput : Instance<OscOutput>, IStatusProvider
     {
         [Input(Guid = "98f38caa-4f79-425c-ab46-22d7dbe62978")]
-        public readonly InputSlot<string> IpAddress = new InputSlot<string>();
+        public readonly InputSlot<string> IpAddress = new();
 
         [Input(Guid = "6c0e07ba-7ea6-4dab-af0b-61cf9cb74ad7")]
-        public readonly InputSlot<int> Port = new InputSlot<int>();
+        public readonly InputSlot<int> Port = new();
 
         [Input(Guid = "9016e418-7761-4916-aafb-c95599f77f38")]
-        public readonly InputSlot<string> Location = new InputSlot<string>();
+        public readonly InputSlot<string> Location = new();
 
         [Input(Guid = "d5e9e9be-093b-4d57-9070-9b2cf33aa45b")]
-        public readonly InputSlot<string> String = new InputSlot<string>();
+        public readonly InputSlot<string> String = new();
 
         [Input(Guid = "827c7ae2-c129-4d04-a715-328c0a86bf8a")]
-        public readonly InputSlot<float> Number = new InputSlot<float>();
+        public readonly InputSlot<float> Number = new();
 
         [Input(Guid = "4931217b-e30d-42a6-9cda-bc49a8fd8d67", MappedType = typeof(OscTypes))]
         public readonly InputSlot<int> OscValueType = new();
 
         [Output(Guid = "a6679d6c-fc34-4588-ab20-5079ad8f8a03")]
-        public readonly Slot<T3.Core.DataTypes.Command> Result = new Slot<T3.Core.DataTypes.Command>();
+        public readonly Slot<T3.Core.DataTypes.Command> Result = new();
 
         public OscOutput()
         {
@@ -83,20 +83,21 @@ namespace T3.Operators.Types.Id_4e99da86_482f_4037_8664_b2371526d632
                 return;
             }
 
-            var _invalidMessage = $"Failed to build the OSC message({oscType})";
+            var invalidMessage = $"Failed to build the OSC message({oscType})";
+            
             switch (oscType)
             {
                 case OscTypes.Number:
-                    if (num != _numericValue)
+                    if (Math.Abs(num - _numericValue) > 0.0001f)
                     {
-                        OscMessage message = new OscMessage(location, num);
+                        var message = new OscMessage(location, num);
                         if (message.Error == OscPacketError.None)
                         {
                             _sender.Send(message);
                             _numericValue = num;
                             _lastErrorMessage = null;
                         }
-                        else _lastErrorMessage = $"{_invalidMessage}: {message.ErrorMessage}";
+                        else _lastErrorMessage = $"{invalidMessage}: {message.ErrorMessage}";
                     }
                     break;
 
@@ -110,7 +111,7 @@ namespace T3.Operators.Types.Id_4e99da86_482f_4037_8664_b2371526d632
                             _stringValue = str;
                             _lastErrorMessage = null;
                         }
-                        else _lastErrorMessage = $"{_invalidMessage}: {message.ErrorMessage}";
+                        else _lastErrorMessage = $"{invalidMessage}: {message.ErrorMessage}";
                     }
                     break;
             }
@@ -167,12 +168,14 @@ namespace T3.Operators.Types.Id_4e99da86_482f_4037_8664_b2371526d632
         private float _numericValue;
         private string _stringValue;
         private OscSender _sender;
-        private string _lastErrorMessage = null;
+        
+        
         public IStatusProvider.StatusLevel GetStatusLevel()
         {
             return string.IsNullOrEmpty(_lastErrorMessage) ? IStatusProvider.StatusLevel.Success : IStatusProvider.StatusLevel.Warning;
         }
 
-        public string GetStatusMessage() { return _lastErrorMessage; }
+        string IStatusProvider.GetStatusMessage() => _lastErrorMessage;
+        private string _lastErrorMessage;
     }
 }
