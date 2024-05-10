c2393f010e8148daf8944b8f78ebb8aed1c78cc7
Thomas Mann <thomas.mann@framefield.com>
2024-05-10 13:08:00 +0200

    Update [BoundBoxPoints] and add support for stretch to [RepeatAtPoints]

--- Resources/lib/points/generate/BoundingBoxPoints.hlsl
+++ Resources/lib/points/generate/BoundingBoxPoints.hlsl
@@ -3,7 +3,7 @@
 
 cbuffer Params : register(b0)
 {
-    
+    float DebugParam;
 }
 
 cbuffer Params : register(b1)
@@ -12,98 +12,118 @@ cbuffer Params : register(b1)
     int ResultCount;
 }
 
-StructuredBuffer<Point> SourcePoints : t0;   // input
-RWStructuredBuffer<Point> ResultPoints : u0; // output
-
-
-[numthreads(64, 1, 1)] void main(uint3 i : SV_DispatchThreadID)
+struct MinMax
 {
-    uint index = (uint)i.x;
-    uint pointCount, stride;
-    SourcePoints.GetDimensions(pointCount, stride);
-    if(index >= pointCount) {        
-        return;
-    }
-   
-    if (i.x >= ResultCount)
-        return;
+    uint3 Min;
+    uint3 Max;
+};
 
-    // Retrieve source point
-    Point sourcePoint = SourcePoints[index];
+StructuredBuffer<Point> SourcePoints : register(t0);
+RWStructuredBuffer<Point> ResultPoints : register(u0);
+RWStructuredBuffer<MinMax> Bounds : register(u1);
 
-    // Check if W value is -nan(ind), if so, skip this point
-    if (isnan(sourcePoint.W)) {
-        return;
-    }
+groupshared uint3 g_MinValue = 0xffffffffu;
+groupshared uint3 g_MaxValue = 0;
 
-    // Initialize min and max bounds with the first point
-    float3 minBounds = SourcePoints[0].Position;
-    float3 maxBounds = SourcePoints[0].Position;
-    float3 separator = 0;
+inline uint FloatToOInt(float value)
+{
+    // For negative values, the mask becomes 0xffffffff.
+    // For positive values, the mask becomes 0x80000000.
+    uint uvalue = asuint(value);
+    uint mask = -int(uvalue >> 31) | 0x80000000;
+    return uvalue ^ mask;
+}
 
-    // Iterate over all valid source points to find the min and max bounds
-    for (uint j = index + 1; j < pointCount; ++j)
+inline float OIntToFloat(uint value)
+{
+    // If the msb is set, the mask becomes 0x80000000.
+    // If the msb is unset, the mask becomes 0xffffffff.
+    uint mask = ((value >> 31) - 1) | 0x80000000;
+    return asfloat(value ^ mask);
+}
+
+[numthreads(1, 1, 1)] void clear2(uint3 DTid : SV_DispatchThreadID)
+{
+    Bounds[0].Min = 0xffffffffu;
+    Bounds[0].Max = 0;
+}
+
+    [numthreads(512, 1, 1)] void main(uint3 DTid : SV_DispatchThreadID, uint Gi : SV_GroupIndex)
+{
+    uint stride, sourcePointCount;
+    SourcePoints.GetDimensions(sourcePointCount, stride);
+
+    Point sourcePoint = SourcePoints[DTid.x];
+    float3 position = sourcePoint.Position;
+
+    // float isValid = !(DTid.x >= sourcePointCount || isnan(position.x) || isnan(position.y) || isnan(position.z));
+
+    float isValid = !(isnan(position.x) || isnan(position.y) || isnan(position.z));
+    uint3 intPos = uint3(
+        FloatToOInt(position.x),
+        FloatToOInt(position.y),
+        FloatToOInt(position.z));
+
+    // This using thread group shared memory to fist compute the group
+    // bounds. This might be slightly faster but the current implementation
+    // is glitchy.
+
+    // if (isValid)
+    // {
+    //     InterlockedMin(g_MinValue.x, intPos.x);
+    //     InterlockedMin(g_MinValue.y, intPos.y);
+    //     InterlockedMin(g_MinValue.z, intPos.z);
+
+    //     InterlockedMax(g_MaxValue.x, intPos.x);
+    //     InterlockedMax(g_MaxValue.y, intPos.y);
+    //     InterlockedMax(g_MaxValue.z, intPos.z);
+    // }
+
+    // //--------------------------------------------
+    // GroupMemoryBarrierWithGroupSync();
+
+    // if (Gi.x == 0)
+    // {
+    //     InterlockedMin(Bounds[0].Min.x, g_MinValue.x);
+    //     InterlockedMin(Bounds[0].Min.y, g_MinValue.y);
+    //     InterlockedMin(Bounds[0].Min.z, g_MinValue.z);
+
+    //     InterlockedMax(Bounds[0].Max.x, g_MaxValue.x);
+    //     InterlockedMax(Bounds[0].Max.y, g_MaxValue.y);
+    //     InterlockedMax(Bounds[0].Max.z, g_MaxValue.z);
+    // }
+
+    if (isValid)
     {
-        Point nextPoint = SourcePoints[j];
-        if (!isnan(nextPoint.W)) {
-            minBounds = min(minBounds, nextPoint.Position);
-            maxBounds = max(maxBounds, nextPoint.Position);
-        }
+        InterlockedMin(Bounds[0].Min.x, intPos.x);
+        InterlockedMin(Bounds[0].Min.y, intPos.y);
+        InterlockedMin(Bounds[0].Min.z, intPos.z);
+
+        InterlockedMax(Bounds[0].Max.x, intPos.x);
+        InterlockedMax(Bounds[0].Max.y, intPos.y);
+        InterlockedMax(Bounds[0].Max.z, intPos.z);
     }
 
-    // Set the bounding box points in the ResultPoints buffer
-    if (i.x == 0)
+    AllMemoryBarrierWithGroupSync();
+
+    float3 minPos = float3(
+        OIntToFloat(Bounds[0].Min.x),
+        OIntToFloat(Bounds[0].Min.y),
+        OIntToFloat(Bounds[0].Min.z));
+    float3 maxPos = float3(
+        OIntToFloat(Bounds[0].Max.x),
+        OIntToFloat(Bounds[0].Max.y),
+        OIntToFloat(Bounds[0].Max.z));
+
+    float3 centerPos = (minPos + maxPos) * 0.5;
+
+    if (Gi.x == 0)
     {
-        
-        // Set the points to draw the bounding box
-        // Back face
-        ResultPoints[1].Position = minBounds;
-        ResultPoints[2].Position = float3(minBounds.x, maxBounds.y, minBounds.z);
-        ResultPoints[3].Position = float3(maxBounds.x, maxBounds.y, minBounds.z);
-        ResultPoints[4].Position = float3(maxBounds.x, minBounds.y, minBounds.z);
-        ResultPoints[5].Position = minBounds;
-
-        // Front face
-        ResultPoints[6].Position = float3(minBounds.x, minBounds.y, maxBounds.z);//identical
-        ResultPoints[7].Position = float3(minBounds.x, maxBounds.y, maxBounds.z);
-        ResultPoints[8].Position = maxBounds;
-        ResultPoints[9].Position = float3(maxBounds.x, minBounds.y, maxBounds.z);
-        ResultPoints[10].Position = float3(minBounds.x, minBounds.y, maxBounds.z);//identical
-
-        //Connections between Back and Front
-        ResultPoints[11].Position = float3(minBounds.x, maxBounds.y, maxBounds.z);//same as [7]
-        ResultPoints[12].Position = float3(minBounds.x, maxBounds.y, minBounds.z);//same as [2]
-        ResultPoints[13].Position = separator;
-
-        ResultPoints[14].Position = maxBounds;
-        ResultPoints[15].Position = float3(maxBounds.x, maxBounds.y, minBounds.z);//same as [3]
-        ResultPoints[16].Position = separator;
-
-        ResultPoints[17].Position = float3(maxBounds.x, minBounds.y, maxBounds.z);//same as [9]
-        ResultPoints[18].Position = float3(maxBounds.x, minBounds.y, minBounds.z);//same as [4]
-        ResultPoints[19].Position = separator;
-
-
-        float3 middlePoint = (maxBounds + minBounds) * 0.5;
-        ResultPoints[0].Position = middlePoint;
-        
+        ResultPoints[0].Position = centerPos;
+        ResultPoints[0].Stretch = maxPos - minPos;
+        ResultPoints[0].Selected = 1;
+        ResultPoints[0].Color = 1;
+        ResultPoints[0].W = 1;
+        ResultPoints[0].Rotation = float4(0, 0, 0, 1);
     }
-
-    // color helpers for development
-    /* ResultPoints[1,5].Color = float4(1,0,0,1); //minBounds
-    ResultPoints[7].Color = float4(1,0,1,1);
-    ResultPoints[8,14].Color = float4(0,1,0,1);//maxBounds
-    ResultPoints[0].Color = float4(0,0,1,1);//middlePoint */
-
-    ResultPoints[i.x].W = 1;
-    ResultPoints[i.x].Color = 1;
-    ResultPoints[i.x].Stretch = 1;
-    ResultPoints[i.x].Selected = 1;
-    ResultPoints[i.x].Rotation = float4(0, 0, 0, 1);
-
-    //Separators
-    ResultPoints[0].W = NAN;
-    ResultPoints[13].W = NAN;
-    ResultPoints[16].W = NAN;
-    ResultPoints[19].W = NAN;
 }
