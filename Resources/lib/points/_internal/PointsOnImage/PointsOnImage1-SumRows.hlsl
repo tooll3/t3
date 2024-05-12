b470ee42d51024a4e4f3e7f04c6ab355b3415f61
Thomas Mann <thomas.mann@framefield.com>
2024-05-12 02:13:55 +0200

    Improved distribution for PointsOnImage

--- Resources/lib/points/_internal/PointsOnImage/PointsOnImage1-SumRows.hlsl
+++ Resources/lib/points/_internal/PointsOnImage/PointsOnImage1-SumRows.hlsl
@@ -1,29 +1,44 @@
-Texture2D<float> InputTexture : register(t0);
-RWTexture2D<float> summedRows : register (u0);
-
-[numthreads(1, 8, 1)]
-void SumRows(uint3 threadID : SV_DispatchThreadID)
+cbuffer Params : register(b0)
 {
+    int ImageWidth;
+    int ImageHeight;
+    int WidthWithSums;
+    int HeightWithSums;
+}
+
+Texture2D<float4> InputTexture : register(t0);
+RWTexture2D<float> CDF : register(u0);
+
+[numthreads(4, 1, 1)] void SumRows(uint3 threadID : SV_DispatchThreadID)
+{
+    uint rowIndex = threadID.x;
+
+    if (threadID.y >= ImageHeight)
+        return;
+
+    int RowSumIndex = ImageWidth;
+    int ColumnSumIndex = ImageHeight;
+
     float sum = 0;
-    uint width, height;
-    InputTexture.GetDimensions(width, height);
 
-    // first get sum of row
-    for (uint x = 0; x < width; ++x)
+    // First get sum of row
+    for (uint x = 0; x < ImageWidth; ++x)
     {
-        sum += InputTexture[uint2(x, threadID.y)].r;
+        float4 rgba = InputTexture[uint2(x, rowIndex)];
+        float l = (rgba.r + rgba.g + rgba.b) * rgba.a;
+        sum += l;
     }
-    summedRows[uint2(width + 1, threadID.y)] = sum;
-    
-    float sumReciproc = (sum == 0.0f) ? 0 : 1.0f/sum;
 
-    float summedUp = 0;
-    // now sum up, scale by overall sum and store
-    for (uint x2 = 0; x2 < width; ++x2)
+    CDF[uint2(RowSumIndex, rowIndex)] = sum;
+    float sumReciproc = (sum == 0.0f) ? 0 : 1.0f / sum;
+
+    // Now sum up, scale by overall sum and store
+    sum = 0;
+    for (x = 0; x < ImageWidth; ++x)
     {
-        summedUp += InputTexture[uint2(x2, threadID.y)].r * sumReciproc;
-        summedRows[uint2(x2, threadID.y)].r = summedUp;
+        float4 rgba = InputTexture[uint2(x, rowIndex)];
+        float l = (rgba.r + rgba.g + rgba.b) * rgba.a * sumReciproc;
+        sum += l;
+        CDF[uint2(x, rowIndex)].r = sum;
     }
-    
-    summedRows[uint2(width, threadID.y)] =  1.0f;
 }
--- Resources/lib/points/_internal/PointsOnImage/PointsOnImage3-EmitPoints.hlsl
+++ Resources/lib/points/_internal/PointsOnImage/PointsOnImage3-EmitPoints.hlsl
@@ -2,86 +2,53 @@
 #include "lib/shared/quat-functions.hlsl"
 #include "lib/shared/hash-functions.hlsl"
 
-cbuffer EmitParameter : register(b0)
+cbuffer Params : register(b0)
+{
+    int ImageWidth;
+    int ImageHeight;
+}
+
+cbuffer EmitParameter : register(b1)
 {
     float Seed;
-};
-
-// float radicalInverse_VdC(uint bits) 
-// {
-//      bits = (bits << 16u) | (bits >> 16u);
-//      bits = ((bits & 0x55555555u) << 1u) | ((bits & 0xAAAAAAAAu) >> 1u);
-//      bits = ((bits & 0x33333333u) << 2u) | ((bits & 0xCCCCCCCCu) >> 2u);
-//      bits = ((bits & 0x0F0F0F0Fu) << 4u) | ((bits & 0xF0F0F0F0u) >> 4u);
-//      bits = ((bits & 0x00FF00FFu) << 8u) | ((bits & 0xFF00FF00u) >> 8u);
-//      return float(bits) * 2.3283064365386963e-10; // / 0x100000000
-// }
-
-
-// float2 hammersley2d(uint i, uint N) 
-// {
-//     return float2(float(i)/float(N), radicalInverse_VdC(i));
-// }
-
-uint wang_hash(in out uint seed)
-{
-    seed = (seed ^ 61) ^ (seed >> 16);
-    seed *= 9;
-    seed = seed ^ (seed >> 4);
-    seed *= 0x27d4eb2d;
-    seed = seed ^ (seed >> 15);
-    return seed;
 }
 
 int NumSamples;
-Texture2D<float> CDF : register (t0);
-Texture2D<float4> Image : register (t1);
+Texture2D<float> CDF : register(t0);
+Texture2D<float4> Image : register(t1);
 RWStructuredBuffer<Point> ResultPoints : u0;
 
 sampler texSampler : register(s0);
 
-[numthreads(256,1,1)]
-void GeneratePoints(uint3 threadID : SV_DispatchThreadID)
+[numthreads(256, 1, 1)] void GeneratePoints(uint3 threadID : SV_DispatchThreadID)
 {
-    uint rowWidth, columnHeight;
-    //cdfRows.GetDimensions(rowWidth, columnHeight);
-    CDF.GetDimensions(rowWidth, columnHeight);
-    
-    rowWidth -= 2; columnHeight -= 2;
-    //float2 prob = hammersley2d(threadID.x, NumSamples);
+    int RowSumIndex = ImageWidth;
+    int ColumnSumIndex = ImageHeight;
 
-    uint rng_stateX = (threadID.x * (uint)(Seed * 10317));
-    uint rng_stateY = (threadID.x * (uint)(Seed * 1331337));
+    float2 probability = float2(
+        hash11u(((threadID.x + (int)Seed) * _PRIME0) * _PRIME0),
+        hash11u((threadID.x + 13 + (int)Seed * _PRIME1) * _PRIME0));
 
-    // float xi = ;
-    // float xi = (float(wang_hash(rng_stateY)) * (1.0 / 4294967296.0));
-    float2 prob = float2(
-        (float(wang_hash(rng_stateX)) * (1.0 / 4294967296.0)),
-        (float(wang_hash(rng_stateY)) * (1.0 / 4294967296.0)));
-
-    // use prob.x to find pos in cdf column
-    uint index = columnHeight/2;   
-   
+    // use probability.x to find pos in cdf column
+    uint index = ImageHeight / 2;
     uint left = 0;
-    uint right = columnHeight;
-    uint steps = log2(columnHeight) + 1;
+    uint right = ImageHeight;
+    uint steps = log2(ImageHeight) + 1;
     for (uint j = 0; j < steps; ++j)
     {
-        uint middle = left + (right - left)/2;
-        // float cdfSegStart = cdfColumn[uint2(0, middle)].x;
-        // float cdfSegEnd = cdfColumn[uint2(0, middle + 1)].x;
-        float cdfSegStart = CDF[uint2(0, middle)].x;
-        float cdfSegEnd = CDF[uint2(0, middle + 1)].x;
-        if (!((prob.x >= cdfSegStart) && (prob.x <= cdfSegEnd)))
+        uint middle = left + (right - left) / 2;
+        float cdfSegStart = CDF[uint2(RowSumIndex, middle)];
+        float cdfSegEnd = CDF[uint2(RowSumIndex, middle + 1)];
+        if (!((probability.x >= cdfSegStart) && (probability.x <= cdfSegEnd)))
         {
-            if (prob.x < cdfSegStart)
+            if (probability.x < cdfSegStart)
             {
                 right = middle;
             }
             else
             {
                 left = middle + 1;
-            }    
+            }
         }
         else
         {
@@ -92,25 +59,24 @@ void GeneratePoints(uint3 threadID : SV_DispatchThreadID)
 
     // now search cdf row for x index
     left = 0;
-    right = rowWidth;
-    steps = log2(rowWidth) + 1;
+    right = ImageWidth;
+    steps = log2(ImageWidth) + 0;
     for (uint i = 0; i < steps; ++i)
     {
-        uint middle = left + (right - left)/2;
-        // float cdfSegStart = cdfRows[uint2(middle, rowIndex)].x;
-        // float cdfSegEnd = cdfRows[uint2(middle + 1, rowIndex)].x;
-        float cdfSegStart = CDF[uint2(middle, rowIndex)].x;
-        float cdfSegEnd = CDF[uint2(middle + 1, rowIndex)].x;
-        if (!((prob.y >= cdfSegStart) && (prob.y <= cdfSegEnd)))
+        uint middle = left + (right - left) / 2;
+        index = middle;
+        float cdfSegStart = CDF[uint2(middle, rowIndex)];
+        float cdfSegEnd = CDF[uint2(middle + 1, rowIndex)];
+        if (!((probability.y >= cdfSegStart) && (probability.y <= cdfSegEnd)))
         {
-            if (prob.y < cdfSegStart)
+            if (probability.y < cdfSegStart)
             {
                 right = middle;
             }
             else
             {
                 left = middle + 1;
-            }    
+            }
         }
         else
         {
@@ -119,16 +85,17 @@ void GeneratePoints(uint3 threadID : SV_DispatchThreadID)
     }
     uint columnIndex = index;
 
-    float aspectRatio = (columnHeight - 1.0) / (rowWidth -2);
-    //float2 samplePosInUV = float2(float(columnIndex)/float(columnHeight -1), (1-float(rowIndex)/float(rowWidth -2)));
-    float2 samplePosInUV = float2(float(columnIndex)/float(rowWidth -1), (1-float(rowIndex)/float(columnHeight -2)));
+    float2 samplePosInUV = float2(float(columnIndex + 0.5) / (ImageWidth - 1),
+                                  (1 - float(rowIndex + 0.5) / (ImageHeight - 1)));
 
-    
-    //ResultPoints[threadID.x].position = float3(samplePosInUV * float2(2, 2/ aspectRatio) + float2(0,-0.5) - 2 ,0);
-    float2 posXY = (samplePosInUV -0.5) * 2 * float2(1/aspectRatio,1);
-    ResultPoints[threadID.x].Position = float3(posXY ,0);
+    float aspectRatio = (float)ImageHeight / ImageWidth;
+    ResultPoints[threadID.x].Color = Image.SampleLevel(texSampler, samplePosInUV * float2(1, -1) + float2(0, 1), 0);
+    float2 scatter = float2(hash11u(threadID.x), hash11u(threadID.x * _PRIME0));
+
+    float2 posXY = (samplePosInUV - 0.5) * 2 * float2(1 / aspectRatio, 1) + (scatter - 0.5) * 2 / float2(ImageWidth, ImageHeight);
+    ResultPoints[threadID.x].Position = float3(posXY, 0);
     ResultPoints[threadID.x].W = 1;
-    ResultPoints[threadID.x].Rotation = float4(0,0,0,1);
-    ResultPoints[threadID.x].Color = Image.SampleLevel(texSampler,  samplePosInUV * float2(1, -1) + float2(0,1), 0);
+    ResultPoints[threadID.x].Rotation = float4(0, 0, 0, 1);
     ResultPoints[threadID.x].Selected = 1;
+    ResultPoints[threadID.x].Stretch = 1;
 }
