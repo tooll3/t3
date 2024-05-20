eaa6f52314a8da2ddc4c721d6a749b43cf72d182
Thomas Mann <thomas.mann@framefield.com>
2024-05-21 00:23:06 +0200

    Rewrote SnapPointsToGrid

---
+++ Editor/Gui/Windows/Utilities/OperatorFormatMigrationHelper.cs
@@ -0,0 +1,29 @@
+﻿using ImGuiNET;
+using T3.Core.Logging;
+using T3.Editor.Gui.Styling;
+using T3.Editor.Gui.UiHelpers;
+
+namespace T3.Editor.Gui.Windows;
+
+static class OperatorFormatMigrationHelper
+{
+    public static void Draw()
+    {
+        FormInputs.AddSectionHeader("This is an internal tool for migrating operators to new versions. Use with caution.");
+        var filepathModified = FormInputs.AddFilePicker("Operator Folder",
+                                                        ref _soundtrackFilePath,
+                                                        "operator folder",
+                                                        warning,
+                                                        FileOperations.FilePickerTypes.Folder
+                                                       );
+
+        if (ImGui.Button("Migrate Operators"))
+        {
+            Log.Debug("...should do something here");
+        }
+
+    }
+    
+    private static string _soundtrackFilePath = "";
+    private static string warning = "Please select a soundtrack file";
+}
--- Operators/Types/lib/point/_internal/SnapPointsToGrid.cs
+++ Operators/Types/lib/point/_internal/SnapPointsToGrid.cs
@@ -11,25 +11,42 @@ namespace T3.Operators.Types.Id_bc88304a_a2c7_4df9_93d8_b7dfecbce3de
         public readonly Slot<T3.Core.DataTypes.BufferWithViews> Output = new();
 
         [Input(Guid = "953a95d0-5226-46bb-80c3-f20b27a32064")]
-        public readonly InputSlot<T3.Core.DataTypes.BufferWithViews> Points = new();
+        public readonly InputSlot<T3.Core.DataTypes.BufferWithViews> Points = new InputSlot<T3.Core.DataTypes.BufferWithViews>();
+
+        [Input(Guid = "3e1dbc97-76d9-47da-a12e-1aefa384cf81")]
+        public readonly InputSlot<float> Amount = new InputSlot<float>();
+
+        [Input(Guid = "8343ab8b-6d3a-48f7-93ea-c52e1c36a5fa", MappedType = typeof(SnapModes))]
+        public readonly InputSlot<int> Mode = new InputSlot<int>();
 
         [Input(Guid = "4d7f1f34-ca1b-43ee-803f-cbc14bcc8679")]
-        public readonly InputSlot<float> Scale = new();
+        public readonly InputSlot<float> GridScale = new InputSlot<float>();
 
         [Input(Guid = "eacc6bf8-1e12-44fb-8541-91ac4a557745")]
-        public readonly InputSlot<System.Numerics.Vector3> GridStretch = new();
+        public readonly InputSlot<System.Numerics.Vector3> GridStretch = new InputSlot<System.Numerics.Vector3>();
 
         [Input(Guid = "04c5ee70-9f9a-4236-8871-772814b8b2ab")]
-        public readonly InputSlot<System.Numerics.Vector3> Offset = new();
+        public readonly InputSlot<System.Numerics.Vector3> Offset = new InputSlot<System.Numerics.Vector3>();
 
         [Input(Guid = "a7d80f3e-298d-4eb5-9751-ec432cda4065")]
-        public readonly InputSlot<float> SnapFraction = new();
+        public readonly InputSlot<float> Scatter = new InputSlot<float>();
 
-        [Input(Guid = "dc5e2101-e4d5-495a-8291-58aef1876657")]
-        public readonly InputSlot<float> FractionBlend = new();
+        [Input(Guid = "f9df2ced-a40b-4679-8161-e2765bab7823")]
+        public readonly InputSlot<System.Numerics.Vector2> BiasAndGain = new InputSlot<System.Numerics.Vector2>();
 
         [Input(Guid = "bf7fef5e-a9d5-41e6-8025-0f74a9e07373")]
-        public readonly InputSlot<bool> UseWAsWeight = new();
+        public readonly InputSlot<bool> UseWAsWeight = new InputSlot<bool>();
+
+        [Input(Guid = "c0328a18-cdce-4177-86f8-0242ccc1fb24")]
+        public readonly InputSlot<bool> UseSelection = new InputSlot<bool>();
+
+        private enum SnapModes
+        {
+            CenterDistance,
+            CornersDistance,
+            AxisCenterDistance,
+            AxisEdgeDistance,
+        }
     }
 }
 
