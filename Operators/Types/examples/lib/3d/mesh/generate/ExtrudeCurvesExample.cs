79d39fb784c5bed7ce420182c46233d928f786b6
newemka <newemka@gmail.com>
2024-05-24 11:21:52 +0200

    ExtrudeCurvesExample Update

--- Operators/Types/examples/lib/3d/mesh/generate/ExtrudeCurvesExample.cs
+++ Operators/Types/examples/lib/3d/mesh/generate/ExtrudeCurvesExample.cs
@@ -10,6 +10,9 @@ namespace T3.Operators.Types.Id_a469ca9f_a7f9_46b8_b34c_0a1c109e1222
         [Output(Guid = "7661ebdf-0d10-437a-8b3e-71fac37cbd1a")]
         public readonly Slot<Texture2D> ColorBuffer = new Slot<Texture2D>();
 
+        [Input(Guid = "7c55d6c6-d712-4464-8dd8-2b7fac96e270")]
+        public readonly InputSlot<bool> FixHoles = new InputSlot<bool>();
+
 
     }
 }
