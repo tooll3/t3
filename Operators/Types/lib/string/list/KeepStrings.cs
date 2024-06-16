03d11b6648d8ac81f923a5dd2d22aa773e197ef2
Thomas Mann <thomas.mann@framefield.com>
2024-06-16 13:49:08 +0200

    Add [KeepStrings] that can collect strings in a list

---
+++ Operators/Types/lib/string/list/KeepStrings.cs
@@ -0,0 +1,158 @@
+using System.Collections.Generic;
+using System.Windows.Forms;
+using T3.Core.Operator;
+using T3.Core.Operator.Attributes;
+using T3.Core.Operator.Slots;
+using T3.Core.Utils;
+
+namespace T3.Operators.Types.Id_56eda8f4_09fc_48a3_ab1d_fbff4f4b6438
+{
+    public class KeepStrings : Instance<KeepStrings>
+    {
+        [Output(Guid = "5e1b1aad-2fe2-49f3-9954-a35dc7b3ec25")]
+        public readonly Slot<List<string>> Strings = new();
+
+        [Output(Guid = "9661F810-7B94-4187-A2F0-2B26950A2F2E")]
+        public readonly Slot<List<float>> InsertTimes = new();
+
+        [Output(Guid = "30760da6-3ed4-41df-aa76-545bd203ea57")]
+        public readonly Slot<int> Count = new();
+
+        public KeepStrings()
+        {
+            Strings.UpdateAction = Update;
+        }
+
+        private void Update(EvaluationContext context)
+        {
+            var maxCount = MaxCount.GetValue(context).Clamp(0, 10000);
+            //var insertTriggered = MathUtils.WasTriggered(InsertTrigger.GetValue(context), ref _insertTrigger);
+            var insertTriggered = InsertTrigger.GetValue(context);
+
+            var onlyOnChanges = OnlyOnChanges.GetValue(context);
+            var newStr = NewString.GetValue(context);
+            var hasStringChanged = newStr != _lastString;
+            if (hasStringChanged)
+            {
+                _lastString = newStr;
+            }
+
+            var insertMode = InsertMode.GetEnumValue<InsertModes>(context);
+
+            if (insertTriggered)
+            {
+                switch (insertMode)
+                {
+                    case InsertModes.Append:
+                        if (hasStringChanged || !onlyOnChanges)
+                        {
+                            _strings.Add(newStr);
+                            _insertTimes.Add((float)context.LocalFxTime);
+
+                            while (_strings.Count > maxCount && _strings.Count > 1)
+                            {
+                                _strings.RemoveAt(0);
+                                _insertTimes.RemoveAt(0);
+                            }
+
+                            _index = _strings.Count - 1;
+                        }
+
+                        break;
+                    case InsertModes.Insert:
+                        if (hasStringChanged || !onlyOnChanges)
+                        {
+                            _strings.Insert(0, newStr);
+                            _insertTimes.Insert(0, (float)context.LocalFxTime);
+
+                            while (_strings.Count > maxCount && _strings.Count > 1)
+                            {
+                                _strings.RemoveAt(_strings.Count - 1);
+                                _insertTimes.RemoveAt(_insertTimes.Count - 1);
+                            }
+
+                            _index = 0;
+                        }
+
+                        break;
+                    case InsertModes.Overwrite:
+                        if (hasStringChanged || !onlyOnChanges)
+                        {
+                            if (maxCount > 0)
+                            {
+                                if (_strings.Count == 0)
+                                {
+                                    _strings.Add(newStr);
+                                    _insertTimes.Add((float)context.LocalFxTime);
+                                }
+                                else if(_strings.Count < maxCount)
+                                {
+                                    _strings.Add(newStr);
+                                    _insertTimes.Add((float)context.LocalFxTime);
+                                    _index = _strings.Count - 1;
+                                }
+                                else
+                                {
+                                    _index = (_index+1) % _strings.Count;
+                                    _strings[_index] = newStr;
+                                    _insertTimes[_index] = (float)context.LocalFxTime;
+                                }
+                            }
+
+                            while (_strings.Count > maxCount && _strings.Count > 1)
+                            {
+                                _strings.RemoveAt(_strings.Count - 1);
+                                _insertTimes.RemoveAt(_insertTimes.Count - 1);
+                            }
+                        }
+
+                        break;
+                }
+            }
+
+            Strings.Value = _strings;
+            InsertTimes.Value = _insertTimes;
+            Count.Value = _strings.Count;
+        }
+
+        private bool _insertTrigger;
+
+        private List<string> _strings = new();
+        private List<float> _insertTimes = new();
+        private int _index;
+
+        private string _lastString = string.Empty;
+
+        private enum InsertModes
+        {
+            Append,
+            Insert,
+            Overwrite,
+            IndexIfConnected,
+        }
+
+        [Input(Guid = "26ADB8D2-14E7-4006-99ED-BCBBEDE8352A")]
+        public readonly InputSlot<string> NewString = new();
+
+        [Input(Guid = "D045F941-7035-4A84-B0B0-2691477EF375")]
+        public readonly InputSlot<bool> InsertTrigger = new();
+
+        [Input(Guid = "1B777F1E-E219-4B8F-B185-94466C280881")]
+        public readonly InputSlot<int> MaxCount = new();
+
+        [Input(Guid = "2FF583B8-8B3E-4441-AA60-7365F4C54320")]
+        public readonly InputSlot<bool> ClearTrigger = new();
+
+        [Input(Guid = "3A9E3E1C-E8C2-46C7-96AC-B7C317205ED4")]
+        public readonly InputSlot<bool> OnlyOnChanges = new();
+
+        [Input(Guid = "694574A3-A3A9-4073-BEEC-A6A10ED64B81", MappedType = typeof(InsertModes))]
+        public readonly InputSlot<int> InsertMode = new();
+
+        [Input(Guid = "D44903AE-379F-4F7A-80FA-56379E3CFAE9")]
+        public readonly InputSlot<int> InsertIndex = new();
+
+        [Input(Guid = "ba5cd2ae-15df-4d67-8690-a5b0d1b81971")]
+        public readonly InputSlot<int> Index = new(0);
+    }
+}
