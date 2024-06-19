30199f9f80741de51e576440b465640f0c7fac0f
Thomas Mann <thomas.mann@framefield.com>
2024-06-19 22:47:03 +0200

    Fix [PointOnImages] flickering

--- Resources/lib/points/_internal/PointsOnImage/PointsOnImage2-SumColumns.hlsl
+++ Resources/lib/points/_internal/PointsOnImage/PointsOnImage2-SumColumns.hlsl
@@ -6,18 +6,26 @@ cbuffer Params : register(b0)
 
 RWTexture2D<float> Sums : register(u0);
 
+groupshared float MidSums[4096];
+
 [numthreads(1, 1, 1)] void SumColumns(uint3 threadID : SV_DispatchThreadID)
 {
-    // First get sum of column
     float sum = 0;
     int RowSumIndex = ImageWidth;
     int ColumnSumIndex = ImageHeight;
 
     for (uint y = 0; y < ImageHeight; ++y)
     {
-        sum += Sums[uint2(RowSumIndex, y)];
+        MidSums[y] = Sums[uint2(RowSumIndex, y)];
     }
-    // Sums[int2(0, ColumnSumIndex)] = sum;
+
+    // Not needed with single thread group
+    // AllMemoryBarrierWithGroupSync();
+
+    for (y = 0; y < ImageHeight; ++y)
+    {
+        sum += MidSums[y];
+    }
 
     float sumReciproc = (sum == 0.0f) ? 0 : 1.0f / sum;
     float normalizedSum = 0;
@@ -25,8 +33,7 @@ RWTexture2D<float> Sums : register(u0);
     // Now sum up, scale by overall sum and store
     for (y = 0; y < ImageHeight; ++y)
     {
-        normalizedSum += Sums[uint2(RowSumIndex, y)] * sumReciproc;
+        normalizedSum += MidSums[y] * sumReciproc;
         Sums[int2(RowSumIndex, y)].r = normalizedSum;
     }
-    // Sums[int2(0, ImageHeight)] = 1;
 }
