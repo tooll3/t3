647b1a02e28a30f775537cba8436da5d2ea2593c
Thomas Mann <thomas.mann@framefield.com>
2024-06-20 21:35:52 +0200

    Working on VJ Content

--- Operators/Types/user/wake/summer2024/scenes/PeopleDoStuff.cs
+++ Operators/Types/user/wake/summer2024/scenes/PeopleDoStuff.cs
@@ -11,10 +11,10 @@ namespace T3.Operators.Types.Id_37cb4087_463a_4790_b3f2_07971d3cf6c3
         public readonly Slot<T3.Core.DataTypes.Command> Scene = new Slot<T3.Core.DataTypes.Command>();
 
         [Input(Guid = "c068eed3-5cd3-4d33-96f7-8cf4454310f9")]
-        public readonly MultiInputSlot<bool> Input2 = new MultiInputSlot<bool>();
+        public readonly InputSlot<bool> TriggerName = new InputSlot<bool>();
 
         [Input(Guid = "555dcc25-3991-4f72-8668-0a96566a2137")]
-        public readonly MultiInputSlot<bool> Input = new MultiInputSlot<bool>();
+        public readonly InputSlot<bool> TriggerAction = new InputSlot<bool>();
 
     }
 }
