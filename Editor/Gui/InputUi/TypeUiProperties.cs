eb340a20f17a285e455f250938a5d5cd2d7f6011
dom <domportera@gmail.com>
2024-07-03 20:55:30 -0400

    fix runtime theme modification for types' ui colors

--- Editor/Gui/InputUi/TypeUiProperties.cs
+++ Editor/Gui/InputUi/TypeUiProperties.cs
@@ -3,14 +3,16 @@ using T3.Editor.Gui.Styling;
 
 namespace T3.Editor.Gui.InputUi
 {
-    public record struct UiProperties(Color Color)
+    public readonly struct UiProperties(Func<Color> getColor)
     {
-        internal static readonly UiProperties Value = new(UiColors.ColorForValues);
+        public Color Color => getColor();
+
+        internal static readonly UiProperties Value = new(() => UiColors.ColorForValues);
         internal static readonly UiProperties Default = Value;
-        internal static readonly UiProperties String = new(UiColors.ColorForString);
-        internal static readonly UiProperties Texture = new(UiColors.ColorForTextures);
-        internal static readonly UiProperties Command = new(UiColors.ColorForCommands);
-        internal static readonly UiProperties Shader = new(UiColors.ColorForDX11);
-        internal static readonly UiProperties GpuData = new(UiColors.ColorForGpuData);
+        internal static readonly UiProperties String = new(() => UiColors.ColorForString);
+        internal static readonly UiProperties Texture = new(() => UiColors.ColorForTextures);
+        internal static readonly UiProperties Command = new(() => UiColors.ColorForCommands);
+        internal static readonly UiProperties Shader = new(() => UiColors.ColorForDX11);
+        internal static readonly UiProperties GpuData = new(() =>UiColors.ColorForGpuData);
     }
 }
