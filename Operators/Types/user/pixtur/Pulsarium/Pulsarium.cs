1aebb45354025a9479009f3c60d5e3da5f3e3b23
Thomas Mann <thomas.mann@framefield.com>
2024-07-21 23:07:13 +0200

    Some demo content

---
+++ Operators/Types/user/pixtur/Pulsarium/Pulsarium.cs
@@ -0,0 +1,14 @@
+using SharpDX.Direct3D11;
+using T3.Core.Operator;
+using T3.Core.Operator.Attributes;
+using T3.Core.Operator.Slots;
+
+namespace T3.Operators.Types.Id_198321f6_3aa4_42ad_a34f_b39e02fe314b
+{
+    public class Pulsarium : Instance<Pulsarium>
+    {
+
+
+    }
+}
+
