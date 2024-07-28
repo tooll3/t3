1a41058fe3b04f8e0d3356c5e3285e0dafb8b815
mrvux <julien@mrvux.com>
2024-07-28 18:49:08 +0300

    Add sharpDX XInput as package
    Add Gamepad input state as struct (so we can use different APIs
    Add functions to convert gamepad from Xinput to generic version
    Add DynamicUploadTexture
    Add VideoCaptureThread
    Add VideoFrame
    Add first part for Gamepad new operator

---
+++ Core/IO/GamePadInput.cs
@@ -0,0 +1,54 @@
+using SharpDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3.Core.IO
+{
+    public struct DirectionalPadState
+    {
+        public bool Left, Right, Up, Down;
+
+        public DirectionalPadState(bool left, bool right, bool up, bool down)
+        {
+            this.Left = left;
+            this.Right = right;
+            this.Up = up;
+            this.Down = down;
+        }
+    }
+
+    public struct ButtonsState
+    {
+        public bool A, B, X, Y;
+
+        public ButtonsState(bool a, bool b, bool x, bool y)
+        {
+            this.A = a;
+            this.B = b;
+            this.X = y;
+            this.Y = y;
+        }
+    }
+
+    public struct GamePadState
+    {
+        public DirectionalPadState DirectionalPad;
+        public ButtonsState Buttons;
+
+        public bool Back;
+        public bool Start;
+
+        public Vector2 LeftThumb;
+        public Vector2 RightThumb;
+
+        public float LeftTrigger;
+        public float RightTrigger;
+
+        public bool LeftShoulder;
+        public bool RightShoulder;
+
+    }
+}
