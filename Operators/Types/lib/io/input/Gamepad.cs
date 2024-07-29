ae9ee3f5fd6981f52b1a7e6ecaf170c99e67f7cf
newemka <newemka@gmail.com>
2024-07-30 01:21:38 +0200

    Gamepad.cs adding controls

--- Operators/Types/lib/io/input/Gamepad.cs
+++ Operators/Types/lib/io/input/Gamepad.cs
@@ -1,5 +1,6 @@
 using SharpDX;
 using System.IO.Ports;
+using System.Runtime.InteropServices;
 using T3.Core.IO;
 using T3.Core.Operator;
 using T3.Core.Operator.Attributes;
@@ -18,8 +19,20 @@ namespace T3.Operators.Types.Id_d69e0f2e_8fe2_478b_ba4e_2a55a92670ae
         [Output(Guid = "68f0c486-5922-4b76-9d4e-70d80bf491bf", DirtyFlagTrigger = DirtyFlagTrigger.Animated)]
         public readonly Slot<System.Numerics.Vector2> LeftThumb = new();
 
+        [Output(Guid = "2BA7BA57-4575-4F00-BAF1-7FA8D589C04F", DirtyFlagTrigger = DirtyFlagTrigger.Animated)]
+        public readonly Slot<System.Numerics.Vector2> RightThumb = new();
+        
         [Output(Guid = "2ccb0b7f-a26e-4db2-b527-7d238c9a9589", DirtyFlagTrigger = DirtyFlagTrigger.Animated)]
         public readonly Slot<float> LeftTrigger = new Slot<float>();
+
+        [Output(Guid = "358AE1A6-26EE-4637-9B47-5DB294B0063B", DirtyFlagTrigger = DirtyFlagTrigger.Animated)]
+        public readonly Slot<float> RightTrigger = new Slot<float>();
+
+        [Output(Guid = "87BA8362-03DD-4D8A-809A-2E9890FB8AA9", DirtyFlagTrigger = DirtyFlagTrigger.Animated)]
+        public readonly Slot<bool> LeftShoulder = new Slot<bool>();
+
+        [Output(Guid = "C99D778E-9678-43CC-86C6-E9FA2A794507", DirtyFlagTrigger = DirtyFlagTrigger.Animated)]
+        public readonly Slot<bool> RightShoulder = new Slot<bool>();
 
         [Input(Guid = "b37ab751-1e41-4e58-9e27-5ac9052662d8")]
         public readonly InputSlot<int> Index = new InputSlot<int>();
@@ -32,7 +45,12 @@ namespace T3.Operators.Types.Id_d69e0f2e_8fe2_478b_ba4e_2a55a92670ae
             Start.UpdateAction = Update;
             Back.UpdateAction = Update;
             LeftThumb.UpdateAction = Update;
+            RightThumb.UpdateAction = Update;
             LeftTrigger.UpdateAction = Update;
+            RightTrigger.UpdateAction = Update;
+            LeftShoulder.UpdateAction = Update;
+            RightShoulder.UpdateAction = Update;
+            
         }
 
         private void Update(EvaluationContext context)
@@ -50,7 +68,12 @@ namespace T3.Operators.Types.Id_d69e0f2e_8fe2_478b_ba4e_2a55a92670ae
             Start.Value = state.Start;
             Back.Value = state.Back;
             LeftThumb.Value = new System.Numerics.Vector2(state.LeftThumb.X, state.LeftThumb.Y);
+            RightThumb.Value = new System.Numerics.Vector2(state.RightThumb.X, state.RightThumb.Y);
             LeftTrigger.Value = state.LeftTrigger;
+            RightTrigger.Value = state.RightTrigger;
+            LeftShoulder.Value = state.LeftShoulder;
+            RightShoulder.Value = state.RightShoulder;
+
 
 
             //Result.Value = Input1.GetValue(context) + Input2.GetValue(context);
