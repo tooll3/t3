b12893eed12998e73abbb5a4abe42c2ccb39aab6
Thomas Mann <thomas.mann@framefield.com>
2024-08-12 00:00:26 +0200

    Cleaning up LoadGltfScene

--- Operators/Types/user/pixtur/research/gltf/LoadGltfScene.cs
+++ Operators/Types/user/pixtur/research/gltf/LoadGltfScene.cs
@@ -190,19 +190,26 @@ public class LoadGltfScene : Instance<LoadGltfScene>
                 var meshIndex = 0;
                 foreach (var meshPrimitive in child.Mesh.Primitives)
                 {
-                    meshIndex++;
-                    if (!TryGenerateMeshBuffersFromGltfChild(meshPrimitive, out var meshBuffers, out var errorMessage))
-                    {
-                        ShowError(errorMessage);
-                        meshBuffers = null;
-                    }
-
-                    if (meshBuffers == null)
+                    if(meshPrimitive == null)
                         continue;
 
-                    Log.Debug($" mesh:{child.Name} {child?.Mesh?.Name}  {meshIndex}");
-                    _meshBuffersForPrimitives[meshPrimitive] = meshBuffers;
+                    if (!_meshBuffersForPrimitives.TryGetValue(meshPrimitive, out var meshBuffers))
+                    {
+                        meshIndex++;
+                        if (!TryGenerateMeshBuffersFromGltfChild(meshPrimitive, out meshBuffers, out var errorMessage))
+                        {
+                            ShowError(errorMessage);
+                            meshBuffers = null;
+                        }
 
+                        if (meshBuffers == null)
+                            continue;
+
+                        _meshBuffersForPrimitives[meshPrimitive] = meshBuffers;
+                    }
+                    
+                    Log.Debug($" mesh:{child.Name} {child.Mesh?.Name}  {meshIndex}");
+                    
                     SceneSetup.SceneMaterial materialDef = GetOrCreateMaterialDefinition(meshPrimitive.Material);
 
                     //Log.Debug("Material: " + materialDef);
@@ -575,150 +582,15 @@ public class LoadGltfScene : Instance<LoadGltfScene>
         // TODO: return cached mesh to reuse buffer
         newMesh = new MeshBuffers();
         message = null;
-
-        var vertexBufferData = Array.Empty<PbrVertex>();
-        var indexBufferData = Array.Empty<Int3>();
-
+        
+        if (!GetMeshDataFromPrimitive(meshPrimitive, out var vertexBufferData, out var indexBufferData, out message))
+            return false;
+        
         try
         {
-            // Convert vertices
-            var verticesCount = 0;
-            {
-                // TODO: Iterate over all primitives
-                var vertexAccessors = meshPrimitive.VertexAccessors;
-
-                // Collect positions
-                if (!vertexAccessors.TryGetValue("POSITION", out var positionAccessor))
-                {
-                    message = "Can't find POSITION attribute in gltf mesh";
-                    return false;
-                }
-
-                verticesCount = positionAccessor.Count;
-
-                if (vertexBufferData.Length != verticesCount)
-                    vertexBufferData = new PbrVertex[verticesCount];
-
-                var positions = positionAccessor.AsVector3Array();
-
-                // Collect normals
-                Vector3[] normals = null;
-                if (vertexAccessors.TryGetValue("NORMAL", out var normalAccess))
-                {
-                    normals = normalAccess.AsVector3Array().ToArray();
-                }
-
-                // Collect texture coords
-                Vector2[] texCoords = null;
-                if (vertexAccessors.TryGetValue("TEXCOORD_0", out var texAccess))
-                {
-                    texCoords = texAccess.AsVector2Array().ToArray();
-                }
-
-                // Write vertex buffer
-                for (var vertexIndex = 0; vertexIndex < positions.Count; vertexIndex++)
-                {
-                    var position = positions[vertexIndex];
-                    vertexBufferData[vertexIndex] = new PbrVertex
-                                                        {
-                                                            Position = new Vector3(position.X, position.Y, position.Z),
-                                                            Normal = normals == null ? VectorT3.Up : normals[vertexIndex],
-                                                            Tangent = VectorT3.Right,
-                                                            Bitangent = VectorT3.ForwardLH,
-                                                            Texcoord = texCoords == null
-                                                                           ? Vector2.Zero
-                                                                           : new Vector2(texCoords[vertexIndex].X,
-                                                                                         1 - texCoords[vertexIndex].Y),
-                                                            Selection = 1,
-                                                        };
-                }
-            }
-
-            // Convert indices
-            int faceCount;
-            int updatedTangentCount = 0;
-            {
-                var indices = meshPrimitive.GetTriangleIndices().ToList();
-                faceCount = indices.Count;
-                if (indexBufferData.Length != faceCount)
-                    indexBufferData = new Int3[faceCount];
-
-                var faceIndex = 0;
-                foreach (var (a, b, c) in indices)
-                {
-                    indexBufferData[faceIndex] = new Int3(a, b, c);
-                    faceIndex++;
-
-                    // Calc TBN space
-                    var p1 = vertexBufferData[a].Position;
-                    var p2 = vertexBufferData[b].Position;
-                    var p3 = vertexBufferData[c].Position;
-
-                    // check for degenerated triangle
-                    if (p1 == p2 || p1 == p3 || p2 == p3) continue;
-
-                    var uv1 = vertexBufferData[a].Texcoord;
-                    var uv2 = vertexBufferData[b].Texcoord;
-                    var uv3 = vertexBufferData[c].Texcoord;
-
-                    // check for degenerated triangle
-                    if (uv1 == uv2 || uv1 == uv3 || uv2 == uv3) continue;
-
-                    var n1 = vertexBufferData[a].Normal;
-                    var n2 = vertexBufferData[b].Normal;
-                    var n3 = vertexBufferData[c].Normal;
-
-                    // Taken from https://github.com/vpenades/SharpGLTF/blob/master/examples/SharpGLTF.Runtime.MonoGame/NormalTangentFactories.cs
-                    var s = p2 - p1;
-                    var t = p3 - p1;
-
-                    var sUv = uv2 - uv1;
-                    var tUv = uv3 - uv1;
-                    //var tUv =  uv1 - uv3;
-                    //tUv.Y = 1 - tUv.Y; 
-
-                    var sx = sUv.X;
-                    var tx = tUv.X;
-                    var sy = sUv.Y;
-                    var ty = tUv.Y;
-
-                    var r = 1.0F / ((sx * ty) - (tx * sy));
-
-                    if (!r._IsFinite()) continue;
-
-                    var sDir = new Vector3((ty * s.X) - (sy * t.X), (ty * s.Y) - (sy * t.Y), (ty * s.Z) - (sy * t.Z)) * r;
-                    var tDir = new Vector3((sx * t.X) - (tx * s.X), (sx * t.Y) - (tx * s.Y), (sx * t.Z) - (tx * s.Z)) * r;
-
-                    if (!sDir._IsFinite()) continue;
-                    if (!tDir._IsFinite()) continue;
-
-                    // Ill-fated attempt with brute force 
-                    // sDir =  Vector3.Cross(n1, Vector3.UnitY);
-                    // tDir =  Vector3.Cross(n1, sDir);
-
-                    // Todo: Sadly this fill add significant artifacts to complex meshes
-
-                    vertexBufferData[a].Tangent = Vector3.Normalize(sDir);
-                    vertexBufferData[a].Bitangent = Vector3.Normalize(tDir);
-
-                    updatedTangentCount++;
-                }
-
-                if (faceCount == 0)
-                {
-                    message = "No faces found";
-                    return false;
-                }
-            }
-
-            if (verticesCount == 0)
-            {
-                message = "No vertices found";
-                return false;
-            }
-
-            // Log.Debug($"  loaded gltf-child:  {verticesCount} vertices  {faceCount} faces  updated {updatedTangentCount} tangents", this);
-
+            var faceCount = indexBufferData.Length;
+            var verticesCount = vertexBufferData.Length;
+            
             const int stride = 3 * 4;
             ResourceManager.SetupStructuredBuffer(indexBufferData, stride * faceCount, stride, ref newMesh.IndicesBuffer.Buffer);
             ResourceManager.CreateStructuredBufferSrv(newMesh.IndicesBuffer.Buffer, ref newMesh.IndicesBuffer.Srv);
@@ -736,7 +608,146 @@ public class LoadGltfScene : Instance<LoadGltfScene>
             message = $"Failed loading gltf: {e.Message}";
             return false;
         }
+        return true;
+    }
 
+    private static bool GetMeshDataFromPrimitive(MeshPrimitive meshPrimitive, out PbrVertex[] vertexBufferData, out Int3[] indexBufferData, out string message)
+    {
+        vertexBufferData = Array.Empty<PbrVertex>();
+        indexBufferData = Array.Empty<Int3>();
+            
+        // Convert vertices
+        {
+            // TODO: Iterate over all primitives
+            var vertexAccessors = meshPrimitive.VertexAccessors;
+
+            // Collect positions
+            if (!vertexAccessors.TryGetValue("POSITION", out var positionAccessor))
+            {
+                message = "Can't find POSITION attribute in gltf mesh";
+                return false;
+            }
+
+            var verticesCount = positionAccessor.Count;
+
+            if (vertexBufferData.Length != verticesCount)
+                vertexBufferData = new PbrVertex[verticesCount];
+
+            var positions = positionAccessor.AsVector3Array();
+
+            // Collect normals
+            Vector3[] normals = null;
+            if (vertexAccessors.TryGetValue("NORMAL", out var normalAccess))
+            {
+                normals = normalAccess.AsVector3Array().ToArray();
+            }
+
+            // Collect texture coords
+            Vector2[] texCoords = null;
+            if (vertexAccessors.TryGetValue("TEXCOORD_0", out var texAccess))
+            {
+                texCoords = texAccess.AsVector2Array().ToArray();
+            }
+
+            // Write vertex buffer
+            for (var vertexIndex = 0; vertexIndex < positions.Count; vertexIndex++)
+            {
+                var position = positions[vertexIndex];
+                vertexBufferData[vertexIndex] = new PbrVertex
+                                                    {
+                                                        Position = new Vector3(position.X, position.Y, position.Z),
+                                                        Normal = normals == null ? VectorT3.Up : normals[vertexIndex],
+                                                        Tangent = VectorT3.Right,
+                                                        Bitangent = VectorT3.ForwardLH,
+                                                        Texcoord = texCoords == null
+                                                                       ? Vector2.Zero
+                                                                       : new Vector2(texCoords[vertexIndex].X,
+                                                                                     1 - texCoords[vertexIndex].Y),
+                                                        Selection = 1,
+                                                    };
+            }
+            
+            if (verticesCount == 0)
+            {
+                message = "No vertices found";
+                return false;
+            }
+        }
+
+        // Convert indices
+        {
+            var indices = meshPrimitive.GetTriangleIndices().ToList();
+            var faceCount = indices.Count;
+            if (indexBufferData.Length != faceCount)
+                indexBufferData = new Int3[faceCount];
+
+            var faceIndex = 0;
+            foreach (var (a, b, c) in indices)
+            {
+                indexBufferData[faceIndex] = new Int3(a, b, c);
+                faceIndex++;
+
+                // Calc TBN space
+                var p1 = vertexBufferData[a].Position;
+                var p2 = vertexBufferData[b].Position;
+                var p3 = vertexBufferData[c].Position;
+
+                // check for degenerated triangle
+                if (p1 == p2 || p1 == p3 || p2 == p3) continue;
+
+                var uv1 = vertexBufferData[a].Texcoord;
+                var uv2 = vertexBufferData[b].Texcoord;
+                var uv3 = vertexBufferData[c].Texcoord;
+
+                // check for degenerated triangle
+                if (uv1 == uv2 || uv1 == uv3 || uv2 == uv3) continue;
+
+                var n1 = vertexBufferData[a].Normal;
+                var n2 = vertexBufferData[b].Normal;
+                var n3 = vertexBufferData[c].Normal;
+
+                // Taken from https://github.com/vpenades/SharpGLTF/blob/master/examples/SharpGLTF.Runtime.MonoGame/NormalTangentFactories.cs
+                var s = p2 - p1;
+                var t = p3 - p1;
+
+                var sUv = uv2 - uv1;
+                var tUv = uv3 - uv1;
+                //var tUv =  uv1 - uv3;
+                //tUv.Y = 1 - tUv.Y; 
+
+                var sx = sUv.X;
+                var tx = tUv.X;
+                var sy = sUv.Y;
+                var ty = tUv.Y;
+
+                var r = 1.0F / ((sx * ty) - (tx * sy));
+
+                if (!r._IsFinite()) continue;
+
+                var sDir = new Vector3((ty * s.X) - (sy * t.X), (ty * s.Y) - (sy * t.Y), (ty * s.Z) - (sy * t.Z)) * r;
+                var tDir = new Vector3((sx * t.X) - (tx * s.X), (sx * t.Y) - (tx * s.Y), (sx * t.Z) - (tx * s.Z)) * r;
+
+                if (!sDir._IsFinite()) continue;
+                if (!tDir._IsFinite()) continue;
+
+                // Ill-fated attempt with brute force 
+                // sDir =  Vector3.Cross(n1, Vector3.UnitY);
+                // tDir =  Vector3.Cross(n1, sDir);
+
+                // Todo: Sadly this fill add significant artifacts to complex meshes
+
+                vertexBufferData[a].Tangent = Vector3.Normalize(sDir);
+                vertexBufferData[a].Bitangent = Vector3.Normalize(tDir);
+            }
+
+            if (faceCount == 0)
+            {
+                message = "No faces found";
+                return false;
+            }
+        }
+        
+        message = null;
         return true;
     }
     #endregion
