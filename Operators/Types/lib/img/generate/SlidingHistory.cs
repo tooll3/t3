2b526f925da661b084822342e515214dde5a05f9
Thomas Mann <thomas.mann@framefield.com>
2024-08-20 23:43:16 +0200

    [SlidingHistory] supports scrolling towards left

--- Operators/Types/lib/img/generate/SlidingHistory.cs
+++ Operators/Types/lib/img/generate/SlidingHistory.cs
@@ -22,11 +22,17 @@ namespace T3.Operators.Types.Id_1b69d98a_0b38_4563_aa43_aac5b8395c2b
         [Input(Guid = "72db9342-9c07-4557-8b67-e3dc6ee55271")]
         public readonly InputSlot<bool> ResetTrigger = new();
 
-        [Input(Guid = "adc812a8-d86b-4ac9-b33f-99f78e0c8c44")]
+        [Input(Guid = "adc812a8-d86b-4ac9-b33f-99f78e0c8c44", MappedType = typeof(Directions))]
         public readonly InputSlot<int> Direction = new();
 
         [Input(Guid = "561b8bdc-557c-4a7d-8759-14486def65e4")]
         public readonly InputSlot<float> SourceSlice = new();
 
+        private enum Directions
+        {
+            Down,
+            Left,
+            Right,
+        }
     }
 }
