a90992e774610af40add1d8d6bb9b9c877c9c759
Thomas Mann <thomas.mann@framefield.com>
2024-08-28 21:27:52 +0200

    Add [FollowMeshSurfaceForce] with an example

---
+++ Operators/Types/examples/lib/point/particle/FollowMeshSurfaceForceExample.cs
@@ -0,0 +1,16 @@
+using SharpDX.Direct3D11;
+using T3.Core.Operator;
+using T3.Core.Operator.Attributes;
+using T3.Core.Operator.Slots;
+
+namespace T3.Operators.Types.Id_16235faf_61b7_474f_bdc7_f7abe3c538eb
+{
+    public class FollowMeshSurfaceForceExample : Instance<FollowMeshSurfaceForceExample>
+    {
+        [Output(Guid = "96f1f7c7-e2f6-42df-817c-51096cb89771")]
+        public readonly Slot<Texture2D> Output = new Slot<Texture2D>();
+
+
+    }
+}
+
