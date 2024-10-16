9122abec196c064158fba32acd848cd1bb79c082
newemka <newemka@gmail.com>
2024-10-17 00:35:34 +0200

    VAT Example

---
+++ Operators/Types/examples/lib/3d/animate/DisplaceMeshVATBetaExample.cs
@@ -0,0 +1,15 @@
+using T3.Core.Operator;
+using T3.Core.Operator.Attributes;
+using T3.Core.Operator.Slots;
+
+namespace T3.Operators.Types.Id_0ae30792_9e06_4080_b11d_52f61c633a3e
+{
+    public class DisplaceMeshVATBetaExample : Instance<DisplaceMeshVATBetaExample>
+    {
+
+        [Output(Guid = "d72b1519-797c-40f6-9660-731b83261020")]
+        public readonly Slot<SharpDX.Direct3D11.Texture2D> Output = new Slot<SharpDX.Direct3D11.Texture2D>();
+
+    }
+}
+
