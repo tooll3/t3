59bc26d15a5a7fa9f7e478a3fce664376437b09a
Thomas Mann <thomas.mann@framefield.com>
2024-10-17 12:06:04 +0200

    Working on [DrawLines] with fx-points

--- Operators/Lib/Resources/points/draw/DrawLines.hlsl
+++ Operators/Lib/Resources/points/draw/DrawLines.hlsl
@@ -1,14 +1,14 @@
 #include "shared/point.hlsl"
 #include "shared/quat-functions.hlsl"
 
-static const float3 Corners[] = 
-{
-  float3(0, -1, 0),
-  float3(1, -1, 0), 
-  float3(1,  1, 0), 
-  float3(1,  1, 0), 
-  float3(0,  1, 0), 
-  float3(0, -1, 0),  
+static const float3 Corners[] =
+    {
+        float3(0, -1, 0),
+        float3(1, -1, 0),
+        float3(1, 1, 0),
+        float3(1, 1, 0),
+        float3(0, 1, 0),
+        float3(0, -1, 0),
 };
 
 cbuffer Params : register(b0)
@@ -18,13 +18,18 @@ cbuffer Params : register(b0)
     float Size;
     float ShrinkWithDistance;
     float OffsetU;
-    float UseWForWidth;
-    float UseWForU;
     float UvScale;
 };
 
+cbuffer Params : register(b1)
+{
+    int UvMode;
+    int WidthFX;
+    int UseWForU;
+    int UseWForWidth;
+};
 
-cbuffer Transforms : register(b1)
+cbuffer Transforms : register(b2)
 {
     float4x4 CameraToClipSpace;
     float4x4 ClipSpaceToCamera;
@@ -38,11 +43,11 @@ cbuffer Transforms : register(b1)
     float4x4 ObjectToClipSpace;
 };
 
-cbuffer FogParams : register(b2)
+cbuffer FogParams : register(b3)
 {
     float4 FogColor;
     float FogDistance;
-    float FogBias;  
+    float FogBias;
 }
 
 struct psInput
@@ -50,15 +55,15 @@ struct psInput
     float4 position : SV_POSITION;
     float4 color : COLOR;
     float2 texCoord : TEXCOORD;
-    float fog: FOG;
+    float fog : FOG;
 };
 
 sampler texSampler : register(s0);
 
-StructuredBuffer<LegacyPoint> Points : t0;
+StructuredBuffer<Point> Points : t0;
 Texture2D<float4> texture2 : register(t1);
 
-psInput vsMain(uint id: SV_VertexID)
+psInput vsMain(uint id : SV_VertexID)
 {
     psInput output;
     float discardFactor = 1;
@@ -66,109 +71,106 @@ psInput vsMain(uint id: SV_VertexID)
     uint SegmentCount, Stride;
     Points.GetDimensions(SegmentCount, Stride);
 
-    float4 aspect = float4(CameraToClipSpace[1][1] / CameraToClipSpace[0][0],1,1,1);
+    float4 aspect = float4(CameraToClipSpace[1][1] / CameraToClipSpace[0][0], 1, 1, 1);
     int quadIndex = id % 6;
     uint particleId = id / 6;
     float3 cornerFactors = Corners[quadIndex];
-    
-    LegacyPoint pointAA = Points[ particleId<1 ? 0: particleId-1];
-    LegacyPoint pointA = Points[particleId];
-    LegacyPoint pointB = Points[particleId+1];
-    LegacyPoint pointBB = Points[particleId > SegmentCount-2 ? SegmentCount-2: particleId+2];
+
+    Point pointAA = Points[particleId < 1 ? 0 : particleId - 1];
+    Point pointA = Points[particleId];
+    Point pointB = Points[particleId + 1];
+    Point pointBB = Points[particleId > SegmentCount - 2 ? SegmentCount - 2 : particleId + 2];
 
     float3 posInObject = cornerFactors.x < 0.5
-        ? pointA.Position
-        : pointB.Position;
+                             ? pointA.Position
+                             : pointB.Position;
 
-
-    float4 aaInScreen  = mul(float4(pointAA.Position,1), ObjectToClipSpace) * aspect;
+    float4 aaInScreen = mul(float4(pointAA.Position, 1), ObjectToClipSpace) * aspect;
     aaInScreen /= aaInScreen.w;
-    float4 aInScreen  = mul(float4(pointA.Position,1), ObjectToClipSpace) * aspect;
-    if(aInScreen.z < -0)
+    float4 aInScreen = mul(float4(pointA.Position, 1), ObjectToClipSpace) * aspect;
+    if (aInScreen.z < -0)
         discardFactor = 0;
     aInScreen /= aInScreen.w;
 
-    
-    float4 bInScreen  = mul(float4(pointB.Position,1), ObjectToClipSpace) * aspect;
-    if(bInScreen.z < -0)
+    float4 bInScreen = mul(float4(pointB.Position, 1), ObjectToClipSpace) * aspect;
+    if (bInScreen.z < -0)
         discardFactor = 0;
 
     bInScreen /= bInScreen.w;
-    float4 bbInScreen  = mul(float4(pointBB.Position,1), ObjectToClipSpace) * aspect;
+    float4 bbInScreen = mul(float4(pointBB.Position, 1), ObjectToClipSpace) * aspect;
     bbInScreen /= bbInScreen.w;
 
     float3 direction = (aInScreen - bInScreen).xyz;
-    float3 directionA = particleId > 0 
+    float3 directionA = particleId > 0
                             ? (aaInScreen - aInScreen).xyz
                             : direction;
-    float3 directionB = particleId < SegmentCount- 1
+    float3 directionB = particleId < SegmentCount - 1
                             ? (bInScreen - bbInScreen).xyz
                             : direction;
 
-    float3 normal =  normalize( cross(direction, float3(0,0,1))); 
-    float3 normalA =  normalize( cross(directionA, float3(0,0,1))); 
-    float3 normalB =  normalize( cross(directionB, float3(0,0,1))); 
-    
-    //if(isnan(pointAA.w) || pointAA.w < 0.000001) {
-    if(isnan(pointAA.W)) {
-        normalA =normal;
+    float3 normal = normalize(cross(direction, float3(0, 0, 1)));
+    float3 normalA = normalize(cross(directionA, float3(0, 0, 1)));
+    float3 normalB = normalize(cross(directionB, float3(0, 0, 1)));
+
+    if (isnan(pointAA.Scale.x))
+    {
+        normalA = normal;
     }
-    //if(isnan(pointBB.w) || pointAA.w < 0.000001) {
-    if(isnan(pointBB.W)) {
-        normalB =normal;
+    if (isnan(pointBB.Scale.x))
+    {
+        normalB = normal;
     }
 
     float3 neighboarNormal = lerp(normalA, normalB, cornerFactors.x);
     float3 meterNormal = (normal + neighboarNormal) / 2;
     float4 pos = lerp(aInScreen, bInScreen, cornerFactors.x);
-    
 
-    float4 posInCamSpace = mul(float4(posInObject,1), ObjectToCamera);
+    float4 posInCamSpace = mul(float4(posInObject, 1), ObjectToCamera);
     posInCamSpace.xyz /= posInCamSpace.w;
     posInCamSpace.w = 1;
 
-    float wAtPoint = lerp( pointA.W  , pointB.W , cornerFactors.x);
-        
-    if(UseWForU > 0.5 && !isnan(wAtPoint)) 
+    float wAtPoint = lerp(pointA.Scale.x, pointB.Scale.x, cornerFactors.x);
+
+    if (UseWForU > 0.5 && !isnan(wAtPoint))
     {
-        output.texCoord = float2(wAtPoint, cornerFactors.y /2 +0.5);
+        output.texCoord = float2(wAtPoint, cornerFactors.y / 2 + 0.5);
     }
-    else {
-        float strokeFactor = (particleId+ cornerFactors.x) / SegmentCount;
-        output.texCoord = float2(strokeFactor, cornerFactors.y /2 +0.5);
+    else
+    {
+        float strokeFactor = (particleId + cornerFactors.x) / SegmentCount;
+        output.texCoord = float2(strokeFactor, cornerFactors.y / 2 + 0.5);
     }
 
     output.texCoord.x *= UvScale;
     output.texCoord.x += OffsetU;
-    
 
-    float thickness = Size * discardFactor * lerp(1, 1/(posInCamSpace.z), ShrinkWithDistance);
+    float thickness = Size * discardFactor * lerp(1, 1 / (posInCamSpace.z), ShrinkWithDistance);
 
-    thickness *= UseWForWidth < 0 ? lerp(1, 1-wAtPoint, -UseWForWidth) 
-                                : lerp(1, wAtPoint, UseWForWidth) ;
+    thickness *= UseWForWidth < 0 ? lerp(1, 1 - wAtPoint, -UseWForWidth)
+                                  : lerp(1, wAtPoint, UseWForWidth);
 
     float miter = dot(-meterNormal, normal);
-    pos+= cornerFactors.y * 0.1f * thickness * float4(meterNormal,0) / clamp(miter, -1,-0.16) ;   
+    pos += cornerFactors.y * 0.1f * thickness * float4(meterNormal, 0) / clamp(miter, -1, -0.16);
 
     output.position = pos / aspect;
-    
-    float3 n = cornerFactors.x < 0.5 
-        ? cross(pointA.Position - pointAA.Position, pointA.Position - pointB.Position)
-        : cross(pointB.Position - pointA.Position, pointB.Position - pointBB.Position);
-    n =normalize(n);
 
-    output.fog = pow(saturate(-posInCamSpace.z/FogDistance), FogBias);
-    output.color =  Color * lerp(pointA.Color, pointB.Color, cornerFactors.x);
-    return output;    
+    float3 n = cornerFactors.x < 0.5
+                   ? cross(pointA.Position - pointAA.Position, pointA.Position - pointB.Position)
+                   : cross(pointB.Position - pointA.Position, pointB.Position - pointBB.Position);
+    n = normalize(n);
+
+    output.fog = pow(saturate(-posInCamSpace.z / FogDistance), FogBias);
+    output.color = Color * lerp(pointA.Color, pointB.Color, cornerFactors.x);
+    return output;
 }
 
 float4 psMain(psInput input) : SV_TARGET
 {
     float4 imgColor = texture2.Sample(texSampler, input.texCoord);
 
-    float dFromLineCenter= abs(input.texCoord.y -0.5)*2;
+    float dFromLineCenter = abs(input.texCoord.y - 0.5) * 2;
 
     float4 col = input.color * imgColor;
     col.rgb = lerp(col.rgb, FogColor.rgb, input.fog);
-    return clamp(col, float4(0,0,0,0), float4(1000,1000,1000,1));
+    return clamp(col, float4(0, 0, 0, 0), float4(1000, 1000, 1000, 1));
 }
--- Operators/Lib/point/draw/DrawLines.cs
+++ Operators/Lib/point/draw/DrawLines.cs
@@ -17,33 +17,54 @@ internal sealed class DrawLines : Instance<DrawLines>
         [Input(Guid = "df158fcf-3042-48cf-8383-7bf4c1bcb8a6")]
         public readonly InputSlot<float> Size = new InputSlot<float>();
 
+        [Input(Guid = "00cb7dc1-edc7-4102-a6a0-6cc44a4f0e56", MappedType = typeof(WidthFXs))]
+        public readonly InputSlot<int> WidthFX = new InputSlot<int>();
+
         [Input(Guid = "d0919481-203a-4379-8094-187d6209e00d")]
         public readonly InputSlot<float> ShrinkWithDistance = new InputSlot<float>();
-
-        [Input(Guid = "28f85ae9-ebae-4300-8aa0-738c6327cc44")]
-        public readonly InputSlot<float> TransitionProgress = new InputSlot<float>();
-
-        [Input(Guid = "567794ab-b3d3-43f6-ae95-4d654f797577")]
-        public readonly InputSlot<bool> UseWAsTexCoordV = new InputSlot<bool>();
-
-        [Input(Guid = "c10f9c6c-9923-42c6-848d-6b98097acc67")]
-        public readonly InputSlot<T3.Core.DataTypes.Texture2D> Texture_ = new InputSlot<T3.Core.DataTypes.Texture2D>();
 
         [Input(Guid = "9ba2aa59-e55b-4ebe-aa98-0f79ed77c7aa")]
         public readonly InputSlot<bool> EnableZTest = new InputSlot<bool>();
 
-        [Input(Guid = "c9cf2182-1297-463c-b5c1-d4ee7ad0895c")]
-        public readonly InputSlot<bool> EnableZWrite = new InputSlot<bool>();
+        [Input(Guid = "c10f9c6c-9923-42c6-848d-6b98097acc67")]
+        public readonly InputSlot<T3.Core.DataTypes.Texture2D> Texture_ = new InputSlot<T3.Core.DataTypes.Texture2D>();
 
         [Input(Guid = "d90ff4e6-7d70-441f-a064-b40401025c36", MappedType = typeof(SharedEnums.BlendModes))]
         public readonly InputSlot<int> BlendMod = new InputSlot<int>();
 
-        [Input(Guid = "e797d93b-3847-4324-898e-09018267ea82")]
-        public readonly InputSlot<float> UvScale = new InputSlot<float>();
+        [Input(Guid = "c9cf2182-1297-463c-b5c1-d4ee7ad0895c")]
+        public readonly InputSlot<bool> EnableZWrite = new InputSlot<bool>();
+
+        [Input(Guid = "28f85ae9-ebae-4300-8aa0-738c6327cc44")]
+        public readonly InputSlot<float> TransitionProgress = new InputSlot<float>();
 
         [Input(Guid = "ba83a66f-5a4c-4355-abb2-d4b7cd55d542")]
         public readonly InputSlot<SharpDX.Direct3D11.TextureAddressMode> WrapMode = new InputSlot<SharpDX.Direct3D11.TextureAddressMode>();
 
+        [Input(Guid = "e797d93b-3847-4324-898e-09018267ea82")]
+        public readonly InputSlot<float> UvScale = new InputSlot<float>();
+
+        [Input(Guid = "520c428e-77b0-44d7-95af-d1cbf9b7e4a7", MappedType = typeof(UvModes))]
+        public readonly InputSlot<int> UvMode = new InputSlot<int>();
+
         [Input(Guid = "039e11ea-2155-4f90-aa8a-74ead604679c")]
         public readonly InputSlot<float> UseWForWidth = new InputSlot<float>();
+
+        [Input(Guid = "567794ab-b3d3-43f6-ae95-4d654f797577")]
+        public readonly InputSlot<bool> UseWAsTexCoordV = new InputSlot<bool>();
+        
+        private enum WidthFXs
+        {
+            None,
+            FX1,
+            FX2,
+        }
+
+        private enum UvModes
+        {
+            Stretch,
+            UFromFX1,
+            UFromFX2,
+        }
+        
 }
