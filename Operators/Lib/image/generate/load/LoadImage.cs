891fe57a6dacadf0ac089806211433dfbdef7de0
Thomas Mann <thomas.mann@framefield.com>
2024-12-31 17:20:02 +0100

    Remove obsolete LoadImage srv output

--- Operators/Lib/image/generate/load/LoadImage.cs
+++ Operators/Lib/image/generate/load/LoadImage.cs
@@ -3,11 +3,8 @@ namespace Lib.image.generate.load;
 [Guid("0b3436db-e283-436e-ba85-2f3a1de76a9d")]
 internal sealed class LoadImage : Instance<LoadImage>, IDescriptiveFilename, IStatusProvider
 {
-    [Output(Guid = "{E0C4FEDD-5C2F-46C8-B67D-5667435FB037}")]
+    [Output(Guid = "E0C4FEDD-5C2F-46C8-B67D-5667435FB037")]
     public readonly Slot<Texture2D> Texture = new();
-
-    [Output(Guid = "{A4A46C04-FF03-48CE-83C9-0C0BAA0F72E7}")]
-    public readonly Slot<ShaderResourceView> ShaderResourceView = new();
 
     public LoadImage()
     {
@@ -16,7 +13,6 @@ internal sealed class LoadImage : Instance<LoadImage>, IDescriptiveFilename, ISt
 
         
         Texture.UpdateAction = UpdateTexture;
-        ShaderResourceView.UpdateAction = UpdateTexture;
     }
 
     private void UpdateTexture(EvaluationContext context)
@@ -24,32 +20,14 @@ internal sealed class LoadImage : Instance<LoadImage>, IDescriptiveFilename, ISt
         Texture.Value = _textureResource.GetValue(context);
         Texture.DirtyFlag.Clear();
 
-        var currentSrv = ShaderResourceView.Value;
         if (Texture.Value == null)
         {
             _lastErrorMessage = "Failed to load texture: " + Path.Value;
             Log.Warning(_lastErrorMessage, this);
-            currentSrv?.Dispose();
-            ShaderResourceView.Value = null;
-            ShaderResourceView.DirtyFlag.Clear();
             return;
         }
 
         _lastErrorMessage = string.Empty;
-            
-        Texture.Value.CreateShaderResourceView(ref currentSrv, "");
-
-        try
-        {
-            ResourceManager.Device.ImmediateContext.GenerateMips(currentSrv);
-        }
-        catch (Exception exception)
-        {
-            Log.Error($"Failed to generate mipmaps for texture {Path.Value}:" + exception);
-        }
-
-        ShaderResourceView.Value = currentSrv;
-        ShaderResourceView.DirtyFlag.Clear();
     }
 
     [Input(Guid = "{76CC3811-4AE0-48B2-A119-890DB5A4EEB2}")]
