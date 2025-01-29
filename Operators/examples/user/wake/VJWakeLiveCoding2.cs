ebe9f6b7a3d22dd470f74651811dc663b9148993
Thomas Mann <thomas.mann@framefield.com>
2025-01-30 00:05:02 +0100

    [Content] Improve VJ-Examples

---
+++ Operators/examples/user/wake/VJWakeLiveCoding2.cs
@@ -0,0 +1,10 @@
+namespace Examples.user.wake;
+
+[Guid("413478b6-9a7b-4ff5-821b-c7d837e39bcc")]
+ internal sealed class VJWakeLiveCoding2 : Instance<VJWakeLiveCoding2>
+{
+    [Output(Guid = "44a62b3e-1940-40cf-8953-68a7b4ad1839")]
+    public readonly Slot<Texture2D> ColorBuffer = new Slot<Texture2D>();
+
+
+}
