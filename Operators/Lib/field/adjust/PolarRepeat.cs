e00b72234456e5a2d50926c45fb714c7494beff8
Thomas Mann <thomas.mann@framefield.com>
2025-03-28 22:19:44 +0100

    Refactored [PolarRepeat] field op to define and use global method

--- Operators/Lib/field/adjust/PolarRepeat.cs
+++ Operators/Lib/field/adjust/PolarRepeat.cs
@@ -8,19 +8,18 @@ internal sealed class PolarRepeat : Instance<PolarRepeat>
 {
     [Output(Guid = "de78d5d8-b232-44f6-ab18-cc765f81eb38")]
     public readonly Slot<ShaderGraphNode> Result = new();
-    
+
     public PolarRepeat()
     {
         ShaderNode = new ShaderGraphNode(this, null, InputField);
         Result.Value = ShaderNode;
         Result.UpdateAction += Update;
     }
-    
-    
+
     private void Update(EvaluationContext context)
     {
         ShaderNode.Update(context);
-        
+
         var axis = Axis.GetEnumValue<AxisTypes>(context);
         var templateChanged = axis != _axis;
         if (templateChanged)
@@ -28,66 +27,58 @@ internal sealed class PolarRepeat : Instance<PolarRepeat>
             _axis = axis;
             ShaderNode.CollectedChanges |= ShaderGraphNode.ChangedFlags.Code;
         }
-        
-        
-        _inputFn= ShaderNode.InputNodes.Count == 1
-                      ? ShaderNode.InputNodes[0].ToString()
-                      : string.Empty;
-        
+
+        _inputFn = ShaderNode.InputNodes.Count == 1
+                       ? ShaderNode.InputNodes[0].ToString()
+                       : string.Empty;
     }
-    
+
     public ShaderGraphNode ShaderNode { get; }
-    
+
     public void GetShaderCode(StringBuilder shaderStringBuilder, Dictionary<string, string> globals)
     {
-// Repeat around the origin by a fixed angle.
-// For easier use, num of repetitions is use to specify the angle.
+        globals["PI"] = """
+                        #ifndef PI
+                        #define PI 3.14159265359
+                        #endif
+                        """;
 
+        globals["mod"] = """
+                         #ifndef mod
+                         #define mod(x, y) ((x) - (y) * floor((x) / (y)))
+                         #endif
+                         """;
 
+        // Repeat around the origin by a fixed angle.
+        // For easier use, num of repetitions is use to specify the angle.
+        
+        globals["pModPolar"] =  """
+                                // https://mercury.sexy/hg_sdf/
+                                void pModPolar(inout float2 p, float repetitions, float offset) {
+                                    float angle = 2*PI/repetitions;
+                                    float a = atan2(p.y, p.x) + angle/2. +  offset / (180 *PI);
+                                    float r = length(p);
+                                    float c = floor(a/angle);
+                                    a = mod(a,angle) - angle/2.;
+                                    p = float2(cos(a), sin(a))*r;
+                                }
+                                """;
 
-
-shaderStringBuilder.AppendLine($@"
-
-#ifndef PI
-#define PI 3.14159265359f
-#endif
-
-#ifndef mod
-#define mod(x, y) ((x) - (y) * floor((x) / (y)))
-#endif
-    
-float {ShaderNode}(float3 p) 
-{{
-    float angle = 2 * PI / {ShaderNode}Repetitions;
-    float2 t = p.{_axisCodes0[(int)_axis]};
-    float a = atan2(t.y, t.x) +  {ShaderNode}Offset / (180 *PI) + angle / 2.;
-    float r = length(t);
-    float c = floor(a / angle);
-    a = mod(a, angle) - angle / 2.;
-    p = {_axisCodes2[(int)_axis]};
-    return {_inputFn}(p);                
-}}
-");
+        shaderStringBuilder.AppendLine($$"""
+                                         float {{ShaderNode}}(float3 p) 
+                                         {
+                                             pModPolar(p.{{_axisCodes0[(int)_axis]}}, {{ShaderNode}}Repetitions, {{ShaderNode}}Offset); 
+                                             return {{_inputFn}}(p);                
+                                         }
+                                         """);
     }
 
-    private readonly string[] _axisCodes0 =[
+    private readonly string[] _axisCodes0 =
+        [
             "zy",
             "zx",
             "yx",
         ];
-    
-    private readonly string[] _axisCodes1 =[
-        "atan2(p.z,p.y)",
-        "atan2(p.z,p.x)",
-        "atan2(p.y,p.x)",
-        ];
-    
-    private readonly string[] _axisCodes2 = [
-        "float3(p.x, float2(cos(a), sin(a)) * r)",
-        "float3(cos(a) * r, p.y, sin(a) * r )",
-        "float3(float2(cos(a), sin(a)) * r, p.z)",
-    ];
-    
     
     private string _inputFn;
     private AxisTypes _axis;
@@ -98,13 +89,12 @@ float {ShaderNode}(float3 p)
         Y,
         Z,
     }
-    
-    [Input(Guid = "7248C680-7279-4C1D-B968-3864CB849C77")]
-    public readonly InputSlot<ShaderGraphNode> InputField = new();    
-    
-    [Input(Guid = "02E4130F-8A0C-4EFB-B75F-F7DA29CC95EB", MappedType = typeof(AxisTypes))]
-    public readonly InputSlot<int> Axis = new();    
 
+    [Input(Guid = "7248C680-7279-4C1D-B968-3864CB849C77")]
+    public readonly InputSlot<ShaderGraphNode> InputField = new();
+
+    [Input(Guid = "02E4130F-8A0C-4EFB-B75F-F7DA29CC95EB", MappedType = typeof(AxisTypes))]
+    public readonly InputSlot<int> Axis = new();
 
     [GraphParam]
     [Input(Guid = "b4c551a3-28c1-418a-83b4-ebdd61ed599c")]
@@ -113,7 +103,4 @@ float {ShaderNode}(float3 p)
     [GraphParam]
     [Input(Guid = "A0231B91-8AB8-4591-A3DA-3CD7F3980D2F")]
     public readonly InputSlot<float> Offset = new();
-
-    
 }
-
