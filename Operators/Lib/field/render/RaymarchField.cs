02c647c931c81f9bcbebb059efb30ecd388ab507
Thomas Mann <thomas.mann@framefield.com>
2025-03-31 10:28:53 +0200

    Rename [RaymarchFieldWithMaterial] -> [RaymarchField]

--- Operators/Lib/field/render/RaymarchField.cs
+++ Operators/Lib/field/render/RaymarchField.cs
@@ -1,50 +1,59 @@
+using T3.Core.DataTypes.ShaderGraph;
+
 namespace Lib.field.render;
 
-[Guid("83e4575c-2190-4c8d-b952-60b20b6c3489")]
+[Guid("9323e32f-078c-4156-941b-203f4c265ff5")]
 public class RaymarchField : Instance<RaymarchField>
 {
-    [Output(Guid = "6cfa828f-e874-4c5c-ba2e-6db538490bfd")]
+    [Output(Guid = "e178ef02-c9ac-48cd-a8cb-df3aec5941bb")]
     public readonly Slot<Command> DrawCommand = new();
 
-        [Input(Guid = "16a89f4b-c426-45d5-98f6-f0e84326e46d")]
+        [Output(Guid = "8e293517-dc6f-4b1f-9710-069420f9da09")]
+        public readonly Slot<string> ShaderCode = new Slot<string>();
+
+        [Input(Guid = "340ca675-9356-4548-ba64-732181bebeef")]
         public readonly InputSlot<T3.Core.DataTypes.ShaderGraphNode> SdfField = new InputSlot<T3.Core.DataTypes.ShaderGraphNode>();
 
-        [Input(Guid = "596e7b55-06c2-4988-b9a2-67c635ad47b7")]
-        public readonly InputSlot<float> MaxSteps = new InputSlot<float>();
+        [Input(Guid = "9715075b-b02b-4290-9332-9bbfe67933f2")]
+        public readonly InputSlot<System.Numerics.Vector4> Color = new InputSlot<System.Numerics.Vector4>();
 
-        [Input(Guid = "a589b7ad-03d7-48bb-942d-3c93b047af0e")]
-        public readonly InputSlot<float> StepSize = new InputSlot<float>();
-
-        [Input(Guid = "4256209c-5cf4-4b8b-a813-fd0c37495981")]
-        public readonly InputSlot<float> MinDistance = new InputSlot<float>();
-
-        [Input(Guid = "779f396e-c28a-4b67-babe-e16e12b49f4d")]
-        public readonly InputSlot<float> MaxDistance = new InputSlot<float>();
-
-        [Input(Guid = "2d25481c-4247-4ce1-b3b9-8c19f1fcd1c5")]
-        public readonly InputSlot<System.Numerics.Vector4> Specular = new InputSlot<System.Numerics.Vector4>();
-
-        [Input(Guid = "7bdc7eb1-e8ec-4861-9408-e92f63a6ba65")]
-        public readonly InputSlot<System.Numerics.Vector4> Glow_ = new InputSlot<System.Numerics.Vector4>();
-
-        [Input(Guid = "b2924806-ea7a-4e94-98ac-1c10ae96870e")]
+        [Input(Guid = "e3a85c27-b94c-4e77-b0c2-4644cd3a22d4")]
         public readonly InputSlot<System.Numerics.Vector4> AmbientOcclusion = new InputSlot<System.Numerics.Vector4>();
 
-        [Input(Guid = "8c80d2fc-af31-45a4-8f00-dc02b2e51808")]
-        public readonly InputSlot<System.Numerics.Vector4> Background = new InputSlot<System.Numerics.Vector4>();
-
-        [Input(Guid = "7def17fd-dcf2-481a-bfc1-e0c05a8131a6")]
-        public readonly InputSlot<System.Numerics.Vector2> Spec = new InputSlot<System.Numerics.Vector2>();
-
-        [Input(Guid = "00874001-e2ff-45a4-891d-79f1a4049b4e")]
+        [Input(Guid = "ffb73f4d-6d24-4f4c-866b-5bdd6f876e6f")]
         public readonly InputSlot<float> AoDistance = new InputSlot<float>();
 
-        [Input(Guid = "f141edef-a8da-4694-abfc-aef6898adf1c")]
-        public readonly InputSlot<float> Fog = new InputSlot<float>();
+        [Input(Guid = "89218016-a0ca-4150-95d4-23f415cf07f0")]
+        public readonly InputSlot<float> TextureScale = new InputSlot<float>();
 
-        [Input(Guid = "50d24336-51e5-45c5-afbb-538e9f50694c")]
-        public readonly InputSlot<System.Numerics.Vector3> LightPos = new InputSlot<System.Numerics.Vector3>();
+        [Input(Guid = "3a23730d-09b9-44bd-84b7-c252dd83e1f9", MappedType = typeof(MappingModes))]
+        public readonly InputSlot<int> UVMapping = new InputSlot<int>();
 
-        [Input(Guid = "a96601d0-7d49-46d4-b48a-5b4dcd82b9a6")]
+        [Input(Guid = "f14e7a2f-cd4e-4399-b137-ea0b87c7dfbd")]
+        public readonly InputSlot<float> NormalSamplingD = new InputSlot<float>();
+
+        [Input(Guid = "3148d927-8779-47ab-9e0a-fa63206f3002")]
+        public readonly InputSlot<float> MaxSteps = new InputSlot<float>();
+
+        [Input(Guid = "0b4d60de-261f-4dbf-ad44-6395cda3a496")]
+        public readonly InputSlot<float> MinDistance = new InputSlot<float>();
+
+        [Input(Guid = "adeb374b-bce0-4af2-867b-efb3ce6289c9")]
+        public readonly InputSlot<float> MaxDistance = new InputSlot<float>();
+
+        [Input(Guid = "561768f6-adf6-4d3d-a36a-20b6f35ff151")]
+        public readonly InputSlot<float> StepSize = new InputSlot<float>();
+
+        [Input(Guid = "1251368b-f8f4-4210-be1e-4d05223caf21")]
         public readonly InputSlot<float> DistToColor = new InputSlot<float>();
+
+        private enum MappingModes
+        {
+            Global_Triplanar,
+            Local_Triplanar,
+            Local_XY,
+            Local_YZ,
+            Local_XZ,
+        }
+        
 }
