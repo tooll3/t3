b7cf745926c5063af8ff8d9c89ab310eacd61769
Thomas Mann <thomas.mann@framefield.com>
2025-04-01 01:01:24 +0200

    Add more SDF combine methods (work in progress)

--- Operators/Lib/Resources/particles/FieldVolumeForce.hlsl
+++ Operators/Lib/Resources/particles/FieldVolumeForce.hlsl
@@ -88,106 +88,25 @@ float4 q_from_tangentAndNormal(float3 dx, float3 dz)
     if (isnan(Particles[gi].BirthTime))
         return;
 
-    // return;
-    // if (isnan(TransformVolume._11) || TransformVolume._11 == 0)
-    // {
-    //     return;
-    // }
-
     float3 pos = Particles[gi].Position;
+    float distance = GetDistance(pos);
+    float3 surfaceN = GetNormal(pos, NormalSamplingDistance);
+
     float4 rot = Particles[gi].Rotation;
     float3 velocity = Particles[gi].Velocity;
-    float r = Particles[gi].Radius;
-
     float3 posNext = float4(pos + velocity * SpeedFactor * 0.01 * 2, 1);
-
-    float rUnitLength = r / 2;
-    // float3 rInVolume = length(mul(float4(unitLength.xxx, 0), TransformVolume));
-
-    // float s = 1;
-    // float distance = 0;
-    // float distanceNext = 0;
-
-    // float rUnitSphere = 0.5 * ;
-    float distance = GetDistance(pos);
     float distanceNext = GetDistance(posNext);
-    float3 surfaceN = GetNormal(pos, NormalSamplingDistance);
-    // s = smoothstep(1 + FallOff, 1, distance);
-
-    // if (VolumeShape == VolumeSphere)
-    // {
-    //     float rUnitSphere = 0.5;
-    //     distance = length(posInVolume) - rUnitSphere;
-    //     distanceNext = length(posInVolumeNext) - rUnitSphere;
-    //     surfaceN = normalize(posInVolume);
-    // // s = smoothstep(1 + FallOff, 1, distance);
-    // }
-    // else if (VolumeShape == VolumeBox)
-    // {
-    //     float3 t1 = abs(posInVolume);
-    //     surfaceN = t1.x > t1.y ? (t1.x > t1.z ? float3(sign(posInVolume.x), 0, 0) : float3(0, 0, sign(posInVolume.z)))
-    //                            : (t1.y > t1.z ? float3(0, sign(posInVolume.y), 0) : float3(0, 0, sign(posInVolume.z)));
-
-    //     float r1 = length(abs(rInVolume * surfaceN)) * InvertVolumeFactor;
-
-    //     float rUnitSphere = 0.5;
-    //     distance = max(max(t1.x, t1.y), t1.z) - rUnitSphere - r1;
-
-    //     float3 t2 = abs(posInVolumeNext);
-    //     distanceNext = max(max(t2.x, t2.y), t2.z) - rUnitSphere - r1;
-    // }
-    // else if (VolumeShape == VolumePlane)
-    // {
-    //     distance = posInVolume.y - r * InvertVolumeFactor;
-    //     distanceNext = posInVolumeNext.y - r * InvertVolumeFactor;
-    //     surfaceN = float3(0, 1, 0);
-    //     // s = smoothstep(FallOff, 0, distance);
-    // }
-    // else if (VolumeShape == VolumeCylinder)
-    // {
-    //     // Assuming the cylinder is aligned along the y-axis
-    //     float rCylinder = 0.5;
-    //     float heightCylinder = 1.0;
-
-    //     float2 xyPos = posInVolume.xz;
-    //     float2 xyPosNext = posInVolumeNext.xz;
-
-    //     float distanceToCenter = length(xyPos);
-    //     float distanceToCenterNext = length(xyPosNext);
-
-    //     // Check if the particle is within the radius of the cylinder
-    //     if (distanceToCenter <= rCylinder)
-    //     {
-    //         distance = abs(posInVolume.y) - heightCylinder * 0.5;
-    //         distanceNext = abs(posInVolumeNext.y) - heightCylinder * 0.5;
-
-    //         // Set the surface normal based on the cylinder's orientation
-    //         surfaceN = float3(0, sign(posInVolume.y), 0);
-    //     }
-    //     else
-    //     {
-    //         // Particle is outside the cylinder, use the distance to the cylinder surface
-    //         distance = distanceToCenter - rCylinder;
-    //         distanceNext = distanceToCenterNext - rCylinder;
-
-    //         // Set the surface normal based on the cylinder's orientation
-    //         surfaceN = float3(xyPos.x, 0, xyPos.y);
-    //         surfaceN.y = 0; // Ignore the y-component, as it's already handled above
-    //         surfaceN = normalize(surfaceN);
-    //     }
-    // }
 
     float3 force = 0;
-
     surfaceN *= InvertVolumeFactor;
-    //   float3 surfaceInWorld = normalize(mul(float4(surfaceN, 0), InverseTransformVolume).xyz);
-    //    float3 surfaceInWorld = surfaceN;
 
     // Reflect if distance changes
     if (sign(distance * distanceNext) < 0 && distance * InvertVolumeFactor > 0)
     {
         float4 rand = hash41u(gi);
-        velocity = reflect(velocity, surfaceN + (RandomizeReflection * (rand.xyz - 0.5))) * Bounciness * (RandomizeBounce * (rand.z - 0.5) + 1);
+        velocity = lerp(velocity,
+                        reflect(velocity, surfaceN + (RandomizeReflection * (rand.xyz - 0.5))) * Bounciness * (RandomizeBounce * (rand.z - 0.5) + 1),
+                        Amount);
     }
     else
     {
@@ -199,7 +118,7 @@ float4 q_from_tangentAndNormal(float3 dx, float3 dz)
         {
             force = -surfaceN * Attraction / (1 + distance * AttractionDecay);
         }
-        velocity += force;
+        velocity += force * Amount;
     }
 
     if (!isnan(velocity.x) && !isnan(velocity.y) && !isnan(velocity.z))
--- Operators/Lib/field/adjust/CombineFields.cs
+++ Operators/Lib/field/adjust/CombineFields.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using T3.Core.DataTypes.ShaderGraph;
 using T3.Core.Utils;
+
 // ReSharper disable UnusedMember.Local
 
 namespace Lib.field.adjust;
@@ -26,7 +27,7 @@ internal sealed class CombineFields : Instance<CombineFields>, IGraphNodeOp
             _combineMethod = combineMethod;
             ShaderNode.FlagCodeChanged();
         }
-        
+
         ShaderNode.Update(context);
 
         // Get all parameters to clear operator dirty flag
@@ -39,31 +40,228 @@ internal sealed class CombineFields : Instance<CombineFields>, IGraphNodeOp
         switch (_combineMethod)
         {
             case CombineMethods.SmoothUnion:
-                cac.Globals["fOpSmoothUnion"] = """
-                                                float fOpSmoothUnion(float a, float b, float k) {
-                                                    float h = max(k - abs(a - b), 0.0);
-                                                    return min(a, b) - (h * h) / (4.0 * k);
-                                                };
-                                                """;
+                cac.Globals["fOpSmoothUnion"]
+                    = """
+                      float fOpSmoothUnion(float a, float b, float k) {
+                          float h = max(k - abs(a - b), 0.0);
+                          return min(a, b) - (h * h) / (4.0 * k);
+                      };
+                      """;
+                break;
+
+            case CombineMethods.UnionChamfer:
+                cac.Globals["fOpUnionChamfer"]
+                    = """
+                      // The "Chamfer" flavour makes a 45-degree chamfered edge (the diagonal of a square of size <r>):
+                      float fOpUnionChamfer(float a, float b, float r) {
+                          return min(min(a, b), (a - r + b)*sqrt(0.5));
+                      }
+                      """;
+                break;
+            case CombineMethods.IntersectionChamfer:
+            case CombineMethods.DifferenceChamfer:
+                cac.Globals["fOpIntersectionChamfer"]
+                    = """
+                      // Intersection has to deal with what is normally the inside of the resulting object
+                      // when using union, which we normally don't care about too much. Thus, intersection
+                      // implementations sometimes differ from union implementations.
+                      float fOpIntersectionChamfer(float a, float b, float r) {
+                          return max(max(a, b), (a + r + b)*sqrt(0.5));
+                      }
+                      """;
+                
+                cac.Globals["fOpDifferenceChamfer"]
+                    = """
+                      // Difference can be built from Intersection or Union:
+                      float fOpDifferenceChamfer (float a, float b, float r) {
+                          return fOpIntersectionChamfer(a, -b, r);
+                      }
+                      """;
+                break;
+            case CombineMethods.UnionRound:
+                cac.Globals["fOpUnionRound"]
+                    = """
+                       // The "Round" variant uses a quarter-circle to join the two objects smoothly:
+                       float fOpUnionRound(float a, float b, float r) {
+                           float2 u = max(float2(r - a,r - b), float2(0));
+                           return max(r, min (a, b)) - length(u);
+                       }
+                      """;
+                break;
+
+            case CombineMethods.DifferenceRound:
+                cac.Globals["fOpDifferenceRound"]
+                    = """
+                      float fOpDifferenceRound (float a, float b, float r) {
+                          return fOpIntersectionRound(a, -b, r);
+                      }
+                      """;
+                break;
+
+            // The "Columns" flavour makes n-1 circular columns at a 45 degree angle:
+            case CombineMethods.UnionColumns:
+                cac.Globals["fOpUnionColumns"]
+                    = """
+                      float fOpUnionColumns(float a, float b, float r, float n) {
+                       if ((a < r) && (b < r)) {
+                        float2 p = float2(a, b);
+                        float columnradius = r*sqrt(2)/((n-1)*2+sqrt(2));
+                        pR45(p);
+                        p.x -= sqrt(2)/2*r;
+                        p.x += columnradius*sqrt(2);
+                        if (mod(n,2) == 1) {
+                         p.y += columnradius;
+                        }
+                        // At this point, we have turned 45 degrees and moved at a point on the
+                        // diagonal that we want to place the columns on.
+                        // Now, repeat the domain along this direction and place a circle.
+                        pMod1(p.y, columnradius*2);
+                        float result = length(p) - columnradius;
+                        result = min(result, p.x);
+                        result = min(result, a);
+                        return min(result, b);
+                       } else {
+                        return min(a, b);
+                       }
+                      }
+                      """;
+                break;
+
+            case CombineMethods.DifferenceColumns:
+            case CombineMethods.IntersectionColumns:
+                cac.Globals["fOpDifferenceColumns"]
+                    = """
+                      float fOpDifferenceColumns(float a, float b, float r, float n) {
+                          a = -a;
+                          float m = min(a, b);
+                          //avoid the expensive computation where not needed (produces discontinuity though)
+                          if ((a < r) && (b < r)) {
+                          float2 p = float2(a, b);
+                          float columnradius = r*sqrt(2)/n/2.0;
+                          columnradius = r*sqrt(2)/((n-1)*2+sqrt(2));
+                      
+                          pR45(p);
+                          p.y += columnradius;
+                          p.x -= sqrt(2)/2*r;
+                          p.x += -columnradius*sqrt(2)/2;
+                      
+                          if (mod(n,2) == 1) {
+                          p.y += columnradius;
+                          }
+                          pMod1(p.y,columnradius*2);
+                      
+                          float result = -length(p) + columnradius;
+                          result = max(result, p.x);
+                          result = min(result, a);
+                          return -min(result, b);
+                          } else {
+                          return -m;
+                          }
+                      }
+                      """;
+
+                cac.Globals["fOpIntersectionColumns"]
+                    = """
+                      float fOpIntersectionColumns(float a, float b, float r, float n) {
+                           return fOpDifferenceColumns(a,-b,r, n);
+                      }
+                      """;
+                break;
+            // The "Stairs" flavour produces n-1 steps of a staircase:
+            // much less stupid version by paniq
+            case CombineMethods.UnionStairs:
+                cac.Globals["fOpUnionStairs"]
+                    = """
+                      float fOpUnionStairs(float a, float b, float r, float n) {
+                          float s = r/n;
+                          float u = b-r;
+                          return min(min(a,b), 0.5 * (u + a + abs ((mod (u - a + s, 2 * s)) - s)));
+                      }
+                      """;
+                break;
+
+            // We can just call Union since stairs are symmetric.
+            case CombineMethods.IntersectionStairs:
+                cac.Globals["fOpIntersectionStairs"] = """
+                                                       float fOpIntersectionStairs(float a, float b, float r, float n) {
+                                                       	   return -fOpUnionStairs(-a, -b, r, n);
+                                                       }
+                                                       """;
+                break;
+
+            case CombineMethods.DifferenceStairs:
+                cac.Globals["fOpDifferenceStairs"] = """
+                                                     float fOpDifferenceStairs(float a, float b, float r, float n) {
+                                                     	return -fOpUnionStairs(-a, b, r, n);
+                                                     }
+                                                     """;
+                break;
+
+            // Similar to fOpUnionRound, but more lipschitz-y at acute angles
+            // (and less so at 90 degrees). Useful when fudging around too much
+            // by MediaMolecule, from Alex Evans' siggraph slides
+            case CombineMethods.UnionSoft:
+                cac.Globals["fOpUnionSoft"] = """
+                                              float fOpUnionSoft(float a, float b, float r) {
+                                              	float e = max(r - abs(a - b), 0);
+                                              	return min(a, b) - e*e*0.25/r;
+                                              }
+                                              """;
+                break;
+
+            // produces a cylindical pipe that runs along the intersection.
+            // No objects remain, only the pipe. This is not a boolean operator.
+            case CombineMethods.Pipe:
+                cac.Globals["fOpPipe"] = """
+                                         float fOpPipe(float a, float b, float r) {
+                                         	return length(float2(a, b)) - r;
+                                         }
+                                         """;
+                break;
+            // first object gets a v-shaped engraving where it intersect the second
+            case CombineMethods.Engrave:
+                cac.Globals["fOpEngrave"] = """
+                                            float fOpEngrave(float a, float b, float r) {
+                                            	return max(a, (a + r - abs(b))*sqrt(0.5));
+                                            }
+                                            """;
+                break;
+
+            // first object gets a capenter-style groove cut out
+            case CombineMethods.Groove:
+                cac.Globals["fOpGroove"] = """
+                                                                        float fOpGroove(float a, float b, float ra, float rb) {
+                                           	return max(a, min(a + ra, rb - abs(b)));
+                                           }
+                                           """;
+                break;
+
+            // first object gets a capenter-style tongue attached
+            case CombineMethods.Tongue:
+                cac.Globals["fOpTongue"] = """
+                                                                    float fOpTongue(float a, float b, float ra, float rb) {
+                                           return min(a, max(a - ra, abs(b) - rb));
+                                           }
+                                                   
+                                           """;
                 break;
         }
     }
-    
-    
+
     public void GetPostShaderCode(CodeAssembleContext cac, int inputIndex)
     {
         // Just pass along subcontext if not enough connected fields...
-        if ( ShaderNode.InputNodes.Count <= 1)
+        if (ShaderNode.InputNodes.Count <= 1)
         {
             cac.AppendCall("// skipping combine with single or no input...");
             return;
         }
-        
-        Debug.Assert(cac.ContextIdStack.Count>=2);
-        
+
+        Debug.Assert(cac.ContextIdStack.Count >= 2);
+
         var contextId = cac.ContextIdStack[^2];
         var subContextId = cac.ContextIdStack[^1];
-        
+
         if (inputIndex == 0)
         {
             // Keep initial value
@@ -95,21 +293,69 @@ internal sealed class CombineFields : Instance<CombineFields>, IGraphNodeOp
                 case CombineMethods.CutOut:
                     cac.AppendCall($"f{contextId}.w = max(f{contextId}.w, -f{subContextId}.w);");
                     break;
-                case CombineMethods.TestBlend:
-                    cac.AppendCall($"f{contextId}.w = lerp(f{contextId}.w, f{subContextId}.w, {ShaderNode}K);");
+                
+                case CombineMethods.UnionChamfer:
+                    cac.AppendCall($"f{contextId}.w = fOpUnionChamfer(f{contextId}.w, f{subContextId}.w, {ShaderNode}K);");
                     break;
-                default:
-                    throw new ArgumentOutOfRangeException();
+                case CombineMethods.IntersectionChamfer:
+                    cac.AppendCall($"f{contextId}.w = fOpIntersectionChamfer(f{contextId}.w, f{subContextId}.w, {ShaderNode}K);");
+                    break;
+                case CombineMethods.DifferenceChamfer:
+                    cac.AppendCall($"f{contextId}.w = fOpDifferenceChamfer(f{contextId}.w, f{subContextId}.w, {ShaderNode}K);");
+                    break;
+                case CombineMethods.UnionRound:
+                    cac.AppendCall($"f{contextId}.w = fOpUnionRound(f{contextId}.w, f{subContextId}.w, {ShaderNode}K);");
+                    break;
+
+                    
+                
+                
+                case CombineMethods.DifferenceRound:
+                    cac.AppendCall($"f{contextId}.w = fOpSmoothUnion(f{contextId}.w, f{subContextId}.w, {ShaderNode}K);");
+                    break;
+                case CombineMethods.UnionColumns:
+                    cac.AppendCall($"f{contextId}.w = fOpUnionColumns(f{contextId}.w, f{subContextId}.w, {ShaderNode}K);");
+                    break;
+                case CombineMethods.DifferenceColumns:
+                    cac.AppendCall($"f{contextId}.w = fOpDifferenceColumns(f{contextId}.w, f{subContextId}.w, {ShaderNode}K);");
+                    break;
+                case CombineMethods.IntersectionColumns:
+                    cac.AppendCall($"f{contextId}.w = fOpIntersectionColumns(f{contextId}.w, f{subContextId}.w, {ShaderNode}K);");
+                    break;
+                case CombineMethods.UnionStairs:
+                    cac.AppendCall($"f{contextId}.w = fOpUnionStairs(f{contextId}.w, f{subContextId}.w, {ShaderNode}K);");
+                    break;
+                case CombineMethods.IntersectionStairs:
+                    cac.AppendCall($"f{contextId}.w = fOpIntersectionStairs(f{contextId}.w, f{subContextId}.w, {ShaderNode}K);");
+                    break;
+                case CombineMethods.DifferenceStairs:
+                    cac.AppendCall($"f{contextId}.w = fOpDifferenceStairs(f{contextId}.w, f{subContextId}.w, {ShaderNode}K);");
+                    break;
+                case CombineMethods.UnionSoft:
+                    cac.AppendCall($"f{contextId}.w = fOpUnionSoft(f{contextId}.w, f{subContextId}.w, {ShaderNode}K);");
+                    break;
+                case CombineMethods.Pipe:
+                    cac.AppendCall($"f{contextId}.w = fOpPipe(f{contextId}.w, f{subContextId}.w, {ShaderNode}K);");
+                    break;
+                case CombineMethods.Engrave:
+                    cac.AppendCall($"f{contextId}.w = fOpEngrave(f{contextId}.w, f{subContextId}.w, {ShaderNode}K);");
+                    break;
+                case CombineMethods.Groove:
+                    cac.AppendCall($"f{contextId}.w = fOpGroove(f{contextId}.w, f{subContextId}.w, {ShaderNode}K);");
+                    break;
+                case CombineMethods.Tongue:
+                    cac.AppendCall($"f{contextId}.w = fOpTongue(f{contextId}.w, f{subContextId}.w, {ShaderNode}K);");
+                    break;
             }
-            
+
             cac.AppendCall($"f{contextId}.xyz = f{contextId}.w < f{subContextId}.w ? f{contextId}.xyz : f{subContextId}.xyz;");
         }
     }
-    
+
     public ShaderGraphNode ShaderNode { get; }
 
     private CombineMethods _combineMethod;
-    
+
     private enum CombineMethods
     {
         Add,
@@ -119,7 +365,24 @@ internal sealed class CombineFields : Instance<CombineFields>, IGraphNodeOp
         Max,
         SmoothUnion,
         CutOut,
-        TestBlend,
+
+        UnionChamfer,
+        IntersectionChamfer,
+        DifferenceChamfer,
+        UnionRound,
+        
+        DifferenceRound,
+        UnionColumns,
+        DifferenceColumns,
+        IntersectionColumns,
+        UnionStairs,
+        IntersectionStairs,
+        DifferenceStairs,
+        UnionSoft,
+        Pipe,
+        Engrave,
+        Groove,
+        Tongue,
     }
 
     [Input(Guid = "7248C680-7279-4C1D-B968-3864CB849C77")]
