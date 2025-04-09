8496f60fe779fce0ca16c4701c1b4dd8abc5a2eb
Thomas Mann <thomas.mann@framefield.com>
2025-04-09 10:28:10 +0200

    Add [SetSdfMaterial] Albedo field input

--- Operators/Lib/SetSDFMaterial.cs
+++ Operators/Lib/SetSDFMaterial.cs
@@ -5,14 +5,14 @@ namespace Lib;
 
 [Guid("88926602-4694-4632-9fd7-04c8d6ddd728")]
 internal sealed class SetSDFMaterial : Instance<SetSDFMaterial>
-,IGraphNodeOp
+                                     , IGraphNodeOp
 {
     [Output(Guid = "51c8b9dd-9798-44e6-a9d0-0baecfc9c9a5")]
     public readonly Slot<ShaderGraphNode> Result = new();
 
     public SetSDFMaterial()
     {
-        ShaderNode = new ShaderGraphNode(this, null, InputField);
+        ShaderNode = new ShaderGraphNode(this, null, SdfField, ColorField);
         Result.Value = ShaderNode;
         Result.UpdateAction += Update;
     }
@@ -20,44 +20,54 @@ internal sealed class SetSDFMaterial : Instance<SetSDFMaterial>
     private void Update(EvaluationContext context)
     {
         ShaderNode.Update(context);
-
-        // var axis = Axis.GetEnumValue<AxisTypes>(context);
-        //
-        // var templateChanged = axis != _axis;
-        // if (!templateChanged)
-        //     return;
-        //
-        // _axis = axis;
-        // ShaderNode.FlagCodeChanged();
     }
 
     public ShaderGraphNode ShaderNode { get; }
-
-    public void GetPreShaderCode(CodeAssembleContext c, int inputIndex)
+    
+    bool IGraphNodeOp.TryBuildCustomCode(CodeAssembleContext c)
     {
-        c.Definitions.AppendLine( $$"""
-                                 
-                                 float4 {{ShaderNode}}fMaterial(float4 pc, float4 f) 
-                                 {
-                                    if(pc.w < 0.5)
-                                        return f;
-                                        
-                                    //if(pc.w < 1.5) {
-                                    return float4({{ShaderNode}}Color.rgb, f.w);
-                                    //}
-                                 }
-                                 """);
+        var subContextIndex = c.ContextIdStack.Count;
 
-    }
+        var inputNodeIndex = 0;
+        
+        // Return base distance (required for blending)
+        if (SdfField.HasInputConnections)
+        {
+            ShaderNode.InputNodes[inputNodeIndex]?.CollectEmbeddedShaderCode(c);
+            inputNodeIndex++;
+        }
 
-    public void GetPostShaderCode(CodeAssembleContext c, int inputIndex)
-    {
-        c.AppendCall($"f{c} = {ShaderNode}fMaterial(p{c}, f{c});");
+        // TODO: This should be extracted into method
+        c.AppendCall($"if(p{c}.w > 0.5 && p{c}.w < 1.5) {{");
+        c.Indent();
+
+        if (ColorField.HasInputConnections)
+        {
+            c.PushContext(subContextIndex, "albedo");
+            var subContextId = c.ToString();
+            ShaderNode.InputNodes[inputNodeIndex]?.CollectEmbeddedShaderCode(c);
+            c.PopContext();
+            c.AppendCall($"f{c}.rgb = f{subContextId}.rgb * {ShaderNode}Color.rgb;");
+            //inputNodeIndex++;
+        }
+        else
+        {
+            c.AppendCall($"f{c} = float4({ShaderNode}Color.rgb, f{c}.w);");
+        }
+        
+        c.Unindent();
+        c.AppendCall("}");
+
+        return true;
     }
+    
     
     [Input(Guid = "7c656067-ef12-4990-b094-7f8160a242d1")]
-    public readonly InputSlot<ShaderGraphNode> InputField = new();
-    
+    public readonly InputSlot<ShaderGraphNode> SdfField = new();
+
+    [Input(Guid = "93D0EE54-B2F6-41EA-BBAA-EF06BCA1F1A0")]
+    public readonly InputSlot<ShaderGraphNode> ColorField = new();
+
     [GraphParam]
     [Input(Guid = "D2A64234-C7EF-424B-822C-ADDCE0B84E69")]
     public readonly InputSlot<Vector4> Color = new();
