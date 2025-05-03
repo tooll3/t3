f720ffc345bc12734552633b333f638d86c665a5
Thomas Mann <thomas.mann@framefield.com>
2025-05-03 18:46:23 +0200

    Tweak layout user name dialog

--- Editor/Gui/Dialog/UserNameDialog.cs
+++ Editor/Gui/Dialog/UserNameDialog.cs
@@ -1,4 +1,5 @@
-﻿using ImGuiNET;
+﻿#nullable enable
+using ImGuiNET;
 using T3.Editor.Gui.Styling;
 using T3.Editor.Gui.UiHelpers;
 using GraphUtils = T3.Editor.UiModel.Helpers.GraphUtils;
@@ -8,29 +9,37 @@ namespace T3.Editor.Gui.Dialog;
 internal sealed class UserNameDialog : ModalDialog
 {
     private string _userName = string.Empty;
-        
+
     protected override void OnShowNextFrame() => _userName = UserSettings.Config.UserName;
 
     internal void Draw()
     {
+        DialogSize = new Vector2(600, 300) * T3Ui.UiScaleFactor;
+        
         if (BeginDialog("Edit username"))
         {
-            ImGui.PushFont(Fonts.FontSmall);
-            ImGui.TextUnformatted("Nickname");
-            ImGui.PopFont();
+            FormInputs.AddSectionHeader("Welcome to TiXL.");
+            ImGui.TextUnformatted("Enter your nickname to group your projects into a namespace.");
+            
+            FormInputs.AddVerticalSpace();
 
-            ImGui.SetNextItemWidth(150);
+            var isValidProjectName = GraphUtils.IsValidProjectName(_userName);
+            var warning = isValidProjectName
+                              ? null
+                              : "Must be valid";
 
-            if (ImGui.IsWindowAppearing())
-                ImGui.SetKeyboardFocusHere();
+            FormInputs.AddStringInput("Username",
+                                      ref _userName!,
+                                      "Nickname",
+                                      warning,
+                                      "Your nickname should be short and not contain spaces or special characters.",
+                                      UserSettings.UndefinedUserName,
+                                      true);
 
-            ImGui.InputText("##name", ref _userName, 32);
+            FormInputs.AddVerticalSpace();
+            FormInputs.ApplyIndent();
 
-            CustomComponents
-               .HelpText("Tooll will use this to group your projects into a namespace.\n\nIt should be short and not contain spaces or special characters.");
-            ImGui.Spacing();
-
-            if (CustomComponents.DisablableButton("Okay", GraphUtils.IsValidProjectName(_userName)))
+            if (CustomComponents.DisablableButton("Okay", isValidProjectName))
             {
                 try
                 {
