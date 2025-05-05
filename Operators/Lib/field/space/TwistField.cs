66d84584616cdb674e9bb7698689814357fc3145
nikky <git@nikky.moe>
2025-05-06 01:05:46 +0200

    fixed issue that rotated the field

--- Operators/Lib/field/space/TwistField.cs
+++ Operators/Lib/field/space/TwistField.cs
@@ -5,7 +5,7 @@ namespace Lib.field.space;
 
 [Guid("D7C6E980-6766-4F83-A32C-677E3713E8A6")]
 internal sealed class TwistField : Instance<TwistField>
-                                , IGraphNodeOp
+                                 , IGraphNodeOp
 {
     [Output(Guid = "0125A816-2885-4E83-97F9-357E24F8AFC3")]
     public readonly Slot<ShaderGraphNode> Result = new();
@@ -20,7 +20,7 @@ internal sealed class TwistField : Instance<TwistField>
     private void Update(EvaluationContext context)
     {
         ShaderNode.Update(context);
-        
+
         var axis = Axis.GetEnumValue<AxisTypes>(context);
         var templateChanged = axis != _axis;
         if (!templateChanged)
@@ -34,28 +34,59 @@ internal sealed class TwistField : Instance<TwistField>
 
     public void GetPreShaderCode(CodeAssembleContext c, int inputIndex)
     {
-        c.Globals["Twist"] = """
-                            void opTwist(inout float3 p, float k) {
-                                float c = cos(k*p.y);
-                                float s = sin(k*p.y);
-                                float2x2  m = float2x2(c,-s,s,c);
-                                p = float3(mul(m,p.xz), p.y);
-                            }
-                            """;
-        
-        var a = _axisCodes0[(int)_axis];
-        c.AppendCall($"opTwist({c}p.{a}, {ShaderNode}Amount);");
+        switch (_axis)
+        {
+            case AxisTypes.X:
+                c.Globals["TwistX"] = """
+                                      void opTwistX(inout float4 p, float k) {
+                                          float c = cos(k * p.x);
+                                          float s = sin(k * p.x);
+                                          float2x2  m = float2x2(c,-s,s,c);
+                                          p.yzx = float3(mul(m,p.yz), p.x);
+                                      }
+                                      """;
+                break;
+            case AxisTypes.Y:
+                c.Globals["TwistY"] = """
+                                      void opTwistY(inout float4 p, float k) {
+                                          float c = cos(k * p.y);
+                                          float s = sin(k * p.y);
+                                          float2x2  m = float2x2(c,-s,s,c);
+                                          p.xzy = float3(mul(m,p.xz), p.y);
+                                      }
+                                      """;
+                break;
+            case AxisTypes.Z:
+                c.Globals["TwistZ"] = """
+                                      void opTwistZ(inout float4 p, float k) {
+                                          float c = cos(k * p.z);
+                                          float s = sin(k * p.z);
+                                          float2x2  m = float2x2(c,-s,s,c);
+                                          p.xyz = float3(mul(m,p.xy), p.z);
+                                      }
+                                      """;
+                break;
+        }
+
+        var twistOp = _axis switch
+                          {
+                              AxisTypes.X => "opTwistX",
+                              AxisTypes.Y => "opTwistY",
+                              AxisTypes.Z => "opTwistZ",
+                              _           => throw new ArgumentOutOfRangeException()
+                          };
+        c.AppendCall($"{twistOp}(p{c}, {ShaderNode}Amount);");
     }
 
     public void GetPostShaderCode(CodeAssembleContext c, int inputIndex)
     {
     }
-    
+
     private readonly string[] _axisCodes0 =
         [
             "yzx",
-            "xyz",
             "xzy",
+            "xyz",
         ];
 
     private AxisTypes _axis;
@@ -65,14 +96,14 @@ internal sealed class TwistField : Instance<TwistField>
         X,
         Y,
         Z,
-    }    
-    
+    }
+
     [Input(Guid = "84DD59CE-4726-4604-AFEE-92FFDEC0E3F5")]
     public readonly InputSlot<ShaderGraphNode> InputField = new();
 
     [Input(Guid = "195F4642-52F7-4C95-9127-07F6175549F5", MappedType = typeof(AxisTypes))]
     public readonly InputSlot<int> Axis = new();
-    
+
     [GraphParam]
     [Input(Guid = "4935F569-8D23-495D-BB87-9A39E5B3981E")]
     public readonly InputSlot<float> Amount = new();
