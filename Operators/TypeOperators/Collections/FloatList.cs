57a7282d7a06bf7dfa1b993c88a16ebde8eae66c
Thomas Mann <thomas.mann@framefield.com>
2025-05-14 00:48:05 +0200

    Add [FloatList] and float list editor in parameter window

--- Operators/TypeOperators/Collections/FloatList.cs
+++ Operators/TypeOperators/Collections/FloatList.cs
@@ -2,10 +2,10 @@ using T3.Core.Utils;
 
 namespace Types.Collections;
 
-[Guid("7ba0451c-9b8d-46a7-bfac-9dcb15e00023")]
+[Guid("db22a341-09f6-4953-b252-61b343159650")]
 public sealed class FloatList : Instance<FloatList>
 {
-    [Output(Guid = "918a871a-1ed6-4d40-a73e-05dbb3399b38")]
+    [Output(Guid = "73bd6ce3-92ac-44af-b334-3f66fe31b160")]
     public readonly Slot<List<float>> Result = new(new List<float>(20));
 
     public FloatList()
@@ -15,34 +15,10 @@ public sealed class FloatList : Instance<FloatList>
 
     private void Update(EvaluationContext context)
     {
-        var defaultValue = DefaultValue.GetValue(context);
-            
-        var reset = ResetTrigger.GetValue(context);
-            
-        var length = ListLenght.GetValue(context).Clamp(0, 100000);
-        if (length != _list.Count || reset)
-        {
-            _list.Clear();
-            _list.Capacity = length;
-            for (var i = 0; i < length; i++)
-            {
-                _list.Add(defaultValue);
-            }
-        }
-            
-        Result.Value = _list;
-
+        var list = List.GetValue(context);
+        Result.Value = [..list ?? []];
     }
-        
-    private readonly List<float> _list = new(20);
-        
-    [Input(Guid = "2B51B0BE-25D1-4B7F-832D-8BC5498E5620")]
-    public readonly InputSlot<bool> ResetTrigger = new();
-        
-    [Input(Guid = "5f2b5280-a157-4499-b3fa-60939a9e3ddd")]
-    public readonly InputSlot<float> DefaultValue = new();
-        
-    [Input(Guid = "A81D55FE-60FB-453F-A6C9-F08932066213")]
-    public readonly InputSlot<int> ListLenght = new();
-
+    
+    [Input(Guid = "7D1BA868-209F-4419-962B-F13C36A7D19D")]
+    public readonly InputSlot<List<float>> List = new();
 }
